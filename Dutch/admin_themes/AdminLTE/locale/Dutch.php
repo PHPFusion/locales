75d51d9da079be08c090de683156e166bf15acb2
douweyntema <douwe@schoonheidssalonanneke.nl>
2019-02-10 22:36:36 +0100

    Added new V8.0.20 themes and fixed typo

---
+++ Dutch/admin_themes/AdminLTE/locale/Dutch.php
@@ -0,0 +1,30 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: AdminLTE/locale/Dutch.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['ALT_001'] = 'Je hebt';
+$locale['ALT_002'] = 'Er zijn geen berichtne';
+$locale['ALT_003'] = 'Nieuw Bericht';
+$locale['ALT_004'] = 'Lid sinds';
+$locale['ALT_005'] = 'Zoeken...';
+$locale['ALT_006'] = 'Gemaakt door';
+$locale['ALT_007'] = 'Beheerder Wachtwoord';
+$locale['ALT_008'] = 'Layout Opties';
+$locale['ALT_009'] = 'Vaste layout';
+$locale['ALT_010'] = 'kleine Sidebar';
+$locale['ALT_011'] = 'Skins';
+$locale['ALT_012'] = 'Donker';
+$locale['ALT_013'] = 'Licht';
---
+++ Dutch/themes/Ddraig/locale/Dutch.php
@@ -0,0 +1,33 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: Dutch.php
+| Author: JoiNNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+//footer
+$locale['latest_news'] = "Laatste Nieuws";
+$locale['no_news'] = "Geen nieuws";
+$locale['latest_weblinks'] = "Laatste Weblinks";
+$locale['no_links'] = "Geen links";
+$locale['scroll_top'] = "Scroll naar boven";
+
+$locale['tcp_warning'] = "<div class=\'admin-message tcp-warn\'>\n
+<span id=\'tcp-warn\'></span><strong>Waarschuwing:</strong> het Themna Beheerder paneel is nog niet ge&iuml;nstalleerd.\n
+<br />Om te infuseren, ga naar infusions op<a href=\'".ADMIN."infusions.php".(isset($aidlink) ? $aidlink : "")."\'>klik hier </a>.\n
+</div>";
+
+//forums
+$locale['sticky'] = "Sticky";
+$locale['locked'] = "Vastgezet";
+$locale['deleted_user'] = "Verwijderde gebruiker";
