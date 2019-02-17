8a37feb13295ff4f2cd41f0099ec8aceb19c12d7
mietek11 <47700118+mietek11@users.noreply.github.com>
2019-02-17 16:57:00 +0100

    Update user_groups.php

--- Polish/locale/Polish/admin/user_groups.php
+++ Polish/locale/Polish/admin/user_groups.php
@@ -1,23 +1,23 @@
 <?php
 // Edit/Save/Delete User Group Messages
-$locale['GRP_400'] = "User Group updated";
-$locale['GRP_401'] = "User Group added";
-$locale['GRP_403'] = "Not Selected Użytkownik";
-$locale['GRP_404'] = "No Group ";
-$locale['GRP_405'] = "Cannot delete User Group";
-$locale['GRP_406'] = "There are users assigned to this group";
-$locale['GRP_407'] = "User Group deleted";
-$locale['GRP_408'] = "No Selected User or Group.";
-$locale['GRP_409'] = " Add a user group.";
+$locale['GRP_400'] = "Zaktualizowano Grupę Użytkowników";
+$locale['GRP_401'] = "Dodano Grupę Użytkowników";
+$locale['GRP_403'] = "Nie wybrano Użytkownika";
+$locale['GRP_404'] = "Bez Grupy ";
+$locale['GRP_405'] = "Nie Można usunąć Grupy Użytkowników";
+$locale['GRP_406'] = "Użytkownicy zostali przydzieleni do tej Grupy";
+$locale['GRP_407'] = "Uzytkownik";
+$locale['GRP_408'] = "Nie wybrano Grupy lub Użytkownika.";
+$locale['GRP_409'] = " Dodaj Użytkownika do Grupy.";
 
 $locale['GRP_410'] = "User \"%s\" added to group \"%s\"";
 $locale['GRP_411'] = "User \"%s\" removed from group \"%s\"";
 $locale['GRP_412'] = "\"%s\" users removed from group \"%s\"";
 
 // Edit/Delete Options
-$locale['GRP_420'] = "User Groups";
-$locale['GRP_421'] = "Edit Group";
-$locale['GRP_422'] = "Delete Group";
+$locale['GRP_420'] = "Grupy Użytkowników";
+$locale['GRP_421'] = "Edytuj Grupę";
+$locale['GRP_422'] = "Usuń Grupę";
 $locale['GRP_423'] = "User Management";
 $locale['GRP_424'] = "Displaying %d User Group.";
 $locale['GRP_425'] = "Delete this user group?";
@@ -33,9 +33,9 @@ $locale['GRP_435'] = "ID";
 $locale['GRP_436'] = "Ikona";
 $locale['GRP_437'] = "Action";
 $locale['GRP_438'] = "Użytkownicy";
-$locale['GRP_439'] = "Group Icon";
+$locale['GRP_439'] = "Ikona Grupy";
 // Search/Add Group Users
-$locale['GRP_440'] = "Add users to Group";
+$locale['GRP_440'] = "Dodaj Użytkownika Do Grupy";
 $locale['GRP_441'] = "Search for users to add to this group using the form below.";
 $locale['GRP_442'] = "You can search for multiple users by separating each entry Enter.";
 $locale['GRP_446'] = "Nazwa użytkownika";
@@ -51,4 +51,4 @@ $locale['GRP_461'] = "Remove selected users";
 $locale['GRP_462'] = "Remove all users";
 $locale['GRP_463'] = "There are no users in this group.";
 // Error Messages
-$locale['GRP_464'] = "Please enter a Group name";
+$locale['GRP_464'] = "Please enter a Group nameWsYst";
