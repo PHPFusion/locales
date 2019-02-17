834ceb2267356c47823313725b8c699576d8019d
mietek11 <47700118+mietek11@users.noreply.github.com>
2019-02-17 18:24:35 +0100

    Update Polish.php

--- Polish/themes/Magazine/locale/Polish.php
+++ Polish/themes/Magazine/locale/Polish.php
@@ -16,13 +16,13 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 $locale['MG_description'] = 'Official theme for PHP-Fusion Andromeda 9.0.3';
-$locale['created_by']  = 'Created by';
-$locale['MG_001'] = 'Hi, ';
+$locale['created_by']  = 'Stworzone przez';
+$locale['MG_001'] = 'Cześć, ';
 // Templates
-$locale['MG_101'] = 'Read More';
-$locale['MG_102'] = 'Popular News';
+$locale['MG_101'] = 'Czytaj więcej';
+$locale['MG_102'] = 'Popularne Aktualności';
 // Widget
-$locale['MG_201'] = 'Theme Settings has been updated.';
+$locale['MG_201'] = 'Ustawienia skórki zostały zaktualizowane.';
 $locale['MG_202'] = 'GitHub URL';
 $locale['MG_203'] = 'Facebook URL';
 $locale['MG_204'] = 'Twitter URL';
