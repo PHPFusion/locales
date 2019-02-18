54033558825ceef2d9b1d8d13677108319cb3083
RobiNN <RobiNN1@users.noreply.github.com>
2019-02-18 18:23:44 +0100

    Update Polish.php

--- Polish/themes/FusionTheme/themefactory/lib/installer/locale/Polish.php
+++ Polish/themes/FusionTheme/themefactory/lib/installer/locale/Polish.php
@@ -1,26 +1,26 @@
 <?php
-$locale['homeSetup_0200'] = 'Do you wish to install the default Nebula Theme Page?';
-$locale['homeSetup_0201'] = 'Yes, Install the Page';
-$locale['homeSetup_0202'] = 'No, thank you';
+$locale['homeSetup_0200'] = 'Czy chcesz zainstalować domyślną skórkę Nebula Theme Page?';
+$locale['homeSetup_0201'] = 'Tak, zainstaluj';
+$locale['homeSetup_0202'] = 'Nie, dziękuję';
 
 /*
 * Home setup
 */
-$locale['homeSetup_0100'] = 'Home';
-$locale['homeSetup_0101'] = 'Welcome to your PHP-Fusion 9 Website';
-$locale['homeSetup_0102'] = '[b]Congratulations on your first install[/b]';
-$locale['homeSetup_0103'] = 'The easiest way to develop a [i]pro grade[/i] website. [b]Start Now[/b]';
+$locale['homeSetup_0100'] = 'Start';
+$locale['homeSetup_0101'] = 'WWitamy na twojej stronie PHP-Fusion 9 ';
+$locale['homeSetup_0102'] = '[b]Gratulujemy pierwszej instalacji[/b]';
+$locale['homeSetup_0103'] = 'Najprostszy sposób na rozwój [i]pro grade[/i] Witryny. [b]Zacznij Teraz[/b]';
 $locale['homeSetup_0104'] = "Carousel";
-$locale['homeSetup_0105'] = "Feature Box";
+$locale['homeSetup_0105'] = "Pole wyboru";
 $locale['homeSetup_0106'] = "Panel";
 $locale['homeSetup_0107'] = "Block";
-$locale['homeSetup_0112'] = "Themes";
-$locale['homeSetup_0113'] = "Our themes delivers awesome design and powerful features for your website. Pixel perfected to demonstrate your website versatility features.";
-$locale['homeSetup_0114'] = "Why you'll love PHP-Fusion 9";
-$locale['homeSetup_0115'] = "With over a million lines of code rewrites that modernize content management system in a way never like before, PHP-Fusion 9 remains lightweight fast, smarter and is more beautiful than ever.";
-$locale['homeSetup_0116'] = "Worldwide Developers Forum";
+$locale['homeSetup_0112'] = "Skórki";
+$locale['homeSetup_0113'] = "Nasze motywy zapewniają niesamowity design i zaawansowane funkcje Twojej witryny.. Pixel perfected to demonstrate your website versatility features.";
+$locale['homeSetup_0114'] = "Czemu polubisz PHP-Fusion 9";
+$locale['homeSetup_0115'] = "Dzięki ponad milionowi linii przepisywania kodu, które modernizują system zarządzania treścią w sposób, który nigdy nie był taki jak poprzednio, PHP-Fusion 9 pozostaje lekki szybki, inteligentny i jest piękniejszy niż kiedykolwiek.";
+$locale['homeSetup_0116'] = "Ogólnoświatowe forum dla programistów";
 $locale['homeSetup_0117'] = "[h4]PHP-Fusion Developers Network[/h4]";
-$locale['homeSetup_0118'] = "[p]Get assisted in code development, Finding developers or Funding ideas and road-maps that in return to be pledged openly, it is all happening.
-Join in a world wide open source collaborative efforts today.[/p]";
-$locale['homeSetup_0119'] = "Amazingly Easy for Everyone";
-$locale['homeSetup_0120'] = "All is required is to tinker around to feel the basic needs. Nothing can ever go wrong with an all round CMS system.";
+$locale['homeSetup_0118'] = "[p] Pomóż w opracowywaniu kodu, znajdowaniu programistów lub finansowaniu pomysłów i map drogowych, które w zamian zostaną otwarte, wszystko się dzieje.
+Przyłącz się dziś do wspólnych wysiłków na rzecz otwartego oprogramowania na całym świecie. [/P]";
+$locale['homeSetup_0119'] = "Niesamowita i prosta dla wszystkich";
+$locale['homeSetup_0120'] = "Wszystko, czego potrzeba, to majsterkować, aby spełnić swoje wymagania. Nic nie może pójść źle z wszechstronnym systemem CMS.";
