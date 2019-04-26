5052a3346365e28a575873aa4764180c5f26636b
robinn1 <kelcakrobo@gmail.com>
2019-04-26 17:43:12 +0200

    New locales

---
+++ Czech/themes/admin_themes/Ares/locale/Czech.php
@@ -0,0 +1,22 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: Ares/locale/Czech.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['ares_001'] = 'Stránky administrace';
+$locale['ares_002'] = 'Vytvořil';
+$locale['ares_003'] = 'Informační panel';
+$locale['ares_004'] = 'Rychlé spuštění';
+$locale['ares_005'] = 'Sekce';
