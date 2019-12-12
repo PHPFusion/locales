38c4aee3a8f47356f44872f20ae27e89cc7180f3
RobiNN <kelcakrobo@gmail.com>
2019-12-12 19:09:04 +0100

    Update user_phpfusion.php

--- Arabic/locale/Arabic/user_fields/user_phpfusion.php
+++ Arabic/locale/Arabic/user_fields/user_phpfusion.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_phpfusion'] = "الفيوشن";
+$locale['uf_phpfusion'] = "PHP-Fusion";
 $locale['uf_phpfusion_id'] = "اسم المستخدم";
 $locale['uf_phpfusion_desc'] = "حساب دعم الموقع";
 $locale['uf_phpfusion_error'] = "لا يمكن تركه اسم المستخدم فارغاً.";
