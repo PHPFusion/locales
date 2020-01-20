ef7eb93e703308c675d35853a5920f1392d73aac
RobiNN <kelcakrobo@gmail.com>
2020-01-20 18:20:53 +0100

    Update Russian

--- Russian/locale/Russian/user_fields/user_instagram.php
+++ Russian/locale/Russian/user_fields/user_instagram.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_instagram'] = "Instagram";
-$locale['uf_instagram_desc'] = "Instagram Profile Page";
-$locale['uf_instagram_error'] = "Instagram Username can not be left empty.";
-$locale['uf_instagram_placeholder'] = "Instagram Username";
+$locale['uf_instagram_desc'] = "Страница Instagram";
+$locale['uf_instagram_error'] = "Имя пользователя Instagram не может быть пустым.";
+$locale['uf_instagram_placeholder'] = "Имя пользователя Instagram";
