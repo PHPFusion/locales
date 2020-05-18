2461e287d8c6ee9e9ea8e0cb3bf9c825ef411bc5
RobiNN <kelcakrobo@gmail.com>
2020-05-18 17:11:24 +0200

    Update Hungarian

--- Hungarian/locale/Hungarian/global.php
+++ Hungarian/locale/Hungarian/global.php
@@ -251,6 +251,8 @@ $locale['global_180'] = "Adminisztrátori főmenü";
 $locale['global_181'] = "Főoldal";
 $locale['global_182'] = "Megjegyzés: az admin jelszó nincs megadva, vagy hibás.";
 $locale['global_183'] = "A bejelentkezési session lejárt. Kérjük, jelentkezz be újra.";
+// Do not add anything within global_183-190. To be reserved for code verification development.
+
 // Miscellaneous
 $locale['global_190'] = "Az oldal karbantartás alatt áll";
 $locale['global_191'] = "IP címed a feketelistán szerepel";
