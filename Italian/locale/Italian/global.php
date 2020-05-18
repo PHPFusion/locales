699324f27e4cd4fb27d0dfd284da31e332351185
RobiNN <kelcakrobo@gmail.com>
2020-05-18 17:18:31 +0200

    Update Italian

--- Italian/locale/Italian/global.php
+++ Italian/locale/Italian/global.php
@@ -251,6 +251,9 @@ $locale['global_176'] = "Privacy Policy";
 $locale['global_180'] = "Admin Home";
 $locale['global_181'] = "Return to Site";
 $locale['global_182'] = "Password amministratore non inserita correttamente.";
+$locale['global_183'] = "Your login session has expired. Please login again.";
+// Do not add anything within global_183-190. To be reserved for code verification development.
+
 // Miscellaneous
 $locale['global_190'] = "Manutenzione modalità attivata";
 $locale['global_191'] = "il tuo indirizzo IP è attualmente nella lista nera.";
