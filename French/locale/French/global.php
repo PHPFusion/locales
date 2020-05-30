dcfc8a65d45b78e04be58b398b3a0140036e52c8
RobiNN <kelcakrobo@gmail.com>
2020-05-30 23:37:24 +0200

    Update French

--- French/locale/French/global.php
+++ French/locale/French/global.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL, "fr_FR.UTF-8"); // Linux Server (Windows may differ)
+$locale['setlocale'] = "fr_FR"; // https://www.php.net/manual/en/function.setlocale.php
 $locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['region'] = "FR"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
 $locale['xml_lang'] = "fr";
