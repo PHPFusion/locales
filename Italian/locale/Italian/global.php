4b6c50ffe9047d5f5fa80eb94f214452fc1d2c8b
RobiNN <kelcakrobo@gmail.com>
2020-05-30 23:43:01 +0200

    Update Italian

--- Italian/locale/Italian/global.php
+++ Italian/locale/Italian/global.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL, "it_IT.UTF-8", "it_IT", "it", "IT"); // Linux Server (Windows may differ)
+$locale['setlocale'] = "it_IT"; // https://www.php.net/manual/en/function.setlocale.php
 $locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['region'] = "IT"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
 $locale['xml_lang'] = "it";
