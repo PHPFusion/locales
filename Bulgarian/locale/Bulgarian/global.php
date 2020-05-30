261079ef4aa83493f1a628e7efb4dd3e93d494a0
RobiNN <kelcakrobo@gmail.com>
2020-05-30 23:31:27 +0200

    Update Bulgarian

--- Bulgarian/locale/Bulgarian/global.php
+++ Bulgarian/locale/Bulgarian/global.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL, "bg_BG.UTF-8"); // Linux Server (Windows may differ)
+$locale['setlocale'] = "bg_BG"; // https://www.php.net/manual/en/function.setlocale.php
 $locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['region'] = "BG"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
 $locale['xml_lang'] = "bg";
