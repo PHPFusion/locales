323daf35f6f55ef62c11e22908d6e10744a338ef
RobiNN <kelcakrobo@gmail.com>
2020-05-30 23:33:54 +0200

    Update Danish

--- Danish/locale/Danish/global.php
+++ Danish/locale/Danish/global.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL, "da", "dan", "da_DK", "da_DK.DK.UTF-8", "danish"); // Linux Server (Windows may differ)
+$locale['setlocale'] = "da_DK"; // https://www.php.net/manual/en/function.setlocale.php
 $locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['region'] = "DK"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
 $locale['xml_lang'] = "da";
