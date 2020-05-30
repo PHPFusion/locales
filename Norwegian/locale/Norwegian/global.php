0a61926afd96c8375b53d340337d25d6206421be
RobiNN <kelcakrobo@gmail.com>
2020-05-30 23:49:08 +0200

    Update Norwegian

--- Norwegian/locale/Norwegian/global.php
+++ Norwegian/locale/Norwegian/global.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL, "no_NO.UTF-8"); // Linux Server (Windows may differ)
+$locale['setlocale'] = "no_NO"; // https://www.php.net/manual/en/function.setlocale.php
 $locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['region'] = "NO"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
 $locale['xml_lang'] = "no";
