96793009de0a3d75c6bd5c3b00bb332e243ed277
RobiNN <kelcakrobo@gmail.com>
2020-08-19 23:43:18 +0200

    Update files

--- Slovak/includes/dynamics/assets/password/lang/sk.js
+++ Slovak/includes/dynamics/assets/password/lang/sk.js
@@ -1,19 +1,3 @@
-/*-------------------------------------------------------+
-| PHP-Fusion Content Management System
-| Copyright (C) PHP-Fusion Inc
-| https://www.php-fusion.co.uk/
-+--------------------------------------------------------+
-| Filename: password/lang/sk.js
-| Author: RobiNN
-+--------------------------------------------------------+
-| This program is released as free software under the
-| Affero GPL license. You can redistribute it and/or
-| modify it under the terms of this license which you
-| can read by viewing the included agpl.txt or online
-| at www.gnu.org/licenses/agpl.html. Removal of this
-| copyright header is strictly prohibited without
-| written permission from the original author(s).
-+--------------------------------------------------------*/
 var locale = new Array();
 locale['wordLength'] = 'Vaše heslo je príliš krátke';
 locale['wordNotEmail'] = 'Nepoužívajte svoj e-mail ako svoje heslo';
