0844d68338bfe37da9b39301af54d04b4b7c7353
RobiNN <kelcakrobo@gmail.com>
2020-11-18 21:23:27 +0100

    Update Italian

--- Italian/locale/Italian/admin/sitelinks.php
+++ Italian/locale/Italian/admin/sitelinks.php
@@ -51,7 +51,7 @@ $locale['SL_0053'] = "Opzioni";
 
 $locale['SL_0062'] = "Nessun collegamenti di sito sono stati definiti.";
 $locale['SL_0063'] = "Use BBCode in Link Name?";
-
+$locale['SL_0064'] = "Enabling this will reformat link name with BBcodes";
 
 $locale['SL_0070'] = "Icona";
 $locale['SL_0071'] = "Nuova finestra?";
