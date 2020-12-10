011d2cca5828551fcd80a1c54396826310e5c334
RobiNN <kelcakrobo@gmail.com>
2020-12-11 00:09:58 +0100

    Update Spanish

--- Spanish/locale/Spanish/global.php
+++ Spanish/locale/Spanish/global.php
@@ -1,16 +1,16 @@
 <?php
 $locale['setlocale'] = "es_ES"; // https://www.php.net/manual/en/function.setlocale.php
-$locale['charset'] = "utf-8"; // ISO 639-1 - http://www.loc.gov/standards/iso639-2/php/code_list.php
-$locale['region'] = "ES"; // ISO-3166 (2 Alpha numeric) - https://www.iso.org/obp/ui/#search
-$locale['xml_lang'] = "es";
-$locale['short_lang_name'] = "es";
+$locale['charset'] = "utf-8"; // https://www.iana.org/assignments/character-sets/character-sets.xhtml
+$locale['region'] = "ES"; // ISO-3166 (Alpha-2 code) https://www.iso.org/obp/ui/#search
+$locale['xml_lang'] = "es"; // ISO 639-1 Code http://www.loc.gov/standards/iso639-2/php/code_list.php
+$locale['short_lang_name'] = "es"; // ISO 639-1 Code http://www.loc.gov/standards/iso639-2/php/code_list.php
 $locale['tinymce'] = "es"; // refer to path includes/jscripts/tinymce/langs/{value}.js
 $locale['select2'] = "es"; // refer to path includes/dynamics/assets/select2/select2_locale_{value}.js
 $locale['phpmailer'] = "es"; // refer to path includes/classes/PHPMailer/language/phpmailer.lang-{value}.php
 $locale['filemanager'] = "es"; // refer to path includes/elFinder/js/i18n/{value}.js
-$locale['datepicker'] = "es-es"; // // refer to path includes/dynamics/assets/datepicker/locale/{value}.js
-$locale['datepicker_js'] = "DD-M-YYYY H:mm:ss";
-$locale['datepicker_php'] = "d-m-Y H:i:s";
+$locale['datepicker'] = "es-es"; // refer to path includes/dynamics/assets/datepicker/locale/{value}.js
+$locale['datepicker_js'] = "DD-M-YYYY H:mm:ss"; // https://momentjs.com/docs/#/displaying/format/
+$locale['datepicker_php'] = "d-m-Y H:i:s"; // https://www.php.net/manual/en/datetime.format.php#refsect1-datetime.format-parameters
 $locale['password_strength'] = "es"; // refer to path includes/dynamics/assets/password/lang/{value}.js
 $locale['date_day'] = "%d %b";
 $locale['text-direction'] = "ltr";
