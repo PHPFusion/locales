faa0bc313406413d43b70398e6a5f2397350bc02
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:20:23 +0800

    New translations user_name_last.php (Romanian)

--- Romanian/locale/Romanian/user_fields/user_name_last.php
+++ Romanian/locale/Romanian/user_fields/user_name_last.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_last'] = "Numele de familie";
+$locale['uf_name_last'] = "Nume";
 $locale['uf_name_last_desc'] = "Numele de familie al utilizatorului.";
 $locale['uf_name_last_error'] = "Numele de familie al utilizatorului nu poate fi l&#259;sat gol.";
