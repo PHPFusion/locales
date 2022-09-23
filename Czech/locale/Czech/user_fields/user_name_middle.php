fb8a2b1ddd12fb0bc8f10d29bf443d649fdbb2b6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:21:12 +0800

    New translations user_name_middle.php (Czech)

--- Czech/locale/Czech/user_fields/user_name_middle.php
+++ Czech/locale/Czech/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Střední jméno";
+$locale['uf_name_middle'] = "Prostřední jméno";
 $locale['uf_name_middle_desc'] = "Skutočné užívateľské druhé jméno";
 $locale['uf_name_middle_error'] = "Druhé (střední) jméno nesmí být prázdné.";
