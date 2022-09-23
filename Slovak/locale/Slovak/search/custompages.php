3faba300df8f42ee0a03ccb9046bbfaea860b1cc
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:08:49 +0800

    New translations custompages.php (Slovak)

--- Slovak/locale/Slovak/search/custompages.php
+++ Slovak/locale/Slovak/search/custompages.php
@@ -1,7 +1,7 @@
 <?php
 $locale['c400'] = "Vlastné stránky";
 $locale['c401'] = "Vlastná stránka";
-$locale['c402'] = "vlastných stránok";
+$locale['c402'] = "Vlastné stránky";
 $locale['c403'] = "nájdené v";
 $locale['c404'] = "názov stránky";
 $locale['c405'] = "obsah stránky";
