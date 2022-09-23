f66c955455e11af79910c3a6715d1dd2c9b20b3e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:41:05 +0800

    New translations image_uploads.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/admin/image_uploads.php
+++ Lithuanian/locale/Lithuanian/admin/image_uploads.php
@@ -1,30 +1,30 @@
 <?php
-$locale['100'] = "Fusion File Manager";
-
+$locale['IMGUP_100'] = "Fusion File Manager";
 // Delete Image
-$locale['400'] = "Trinti paveiksliuką";
-$locale['401'] = "Paveiksliukas ištrintas";
-$locale['402'] = "Grįžti į paveiksliukų administraciją";
-$locale['403'] = "Grįžti į administracijos panelę";
+$locale['IMGUP_400'] = "Trinti paveiksliuką";
+$locale['IMGUP_401'] = "Paveiksliukas ištrintas";
+$locale['IMGUP_402'] = "Grįžti į paveiksliukų administraciją";
+$locale['IMGUP_403'] = "Grįžti į administracijos panelę";
 // Upload Image
-$locale['420'] = "Įkelti paveiksliuką";
-$locale['421'] = "Failo pavadinimas:";
-$locale['422'] = "Pagrindiniai paveiksliukai";
-$locale['423'] = "Straipsnių paveiksliukai";
-$locale['424'] = "Naujienų paveiksliukai";
-$locale['425'] = "Jūs galite įkelti tik gif, jpg arba png formato paveiksliukus";
-$locale['426'] = "Paveiksliukas įkeltas";
-$locale['427'] = "Naujienų kategorijų paveiksliukai";
-$locale['428'] = "Tinklaraščių paveiksliukai";
-$locale['429'] = "Tinklaraščių kategorijų paveiksliukai";
+$locale['IMGUP_420'] = "Įkelti paveiksliuką";
+$locale['IMGUP_421'] = "Failo pavadinimas:";
+$locale['IMGUP_422'] = "Pagrindiniai paveiksliukai";
+$locale['IMGUP_423'] = "Straipsnių paveiksliukai";
+$locale['IMGUP_424'] = "Naujienų nuotraukos";
+$locale['IMGUP_425'] = "Max. file size %s / Allowed file types .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_426'] = "Paveiksliukas įkeltas";
+$locale['IMGUP_427'] = "Naujienų kategorijų paveiksliukai";
+$locale['IMGUP_428'] = "Tinklaraščių paveiksliukai";
+$locale['IMGUP_429'] = "Tinklaraščių kategorijų paveiksliukai";
 // View Image
-$locale['440'] = "Peržiūrėti paveiksliuką";
-$locale['441'] = "Nežinomas failo formatas, negalima peržiūrėti paveiksliuko.";
-$locale['442'] = "Trinti šį paveiksliuką";
+$locale['IMGUP_440'] = "Peržiūrėti paveiksliuką";
+$locale['IMGUP_441'] = "Nežinomas failo formatas, negalima peržiūrėti paveiksliuko.";
+$locale['IMGUP_442'] = "Trinti šį paveiksliuką";
 // Image Management
-$locale['460'] = "Paveiksliukai";
-$locale['461'] = "Peržiūrėti";
-$locale['463'] = "Šis aplankas tuščias";
-$locale['464'] = "Atnaujinti paveikslėlių sąrašą TinyMCE redaktoriui";
+$locale['IMGUP_460'] = "Nuotraukos";
+$locale['IMGUP_461'] = "Rodyti";
+$locale['IMGUP_463'] = "Šis aplankas tuščias";
+$locale['IMGUP_464'] = "Atnaujinti paveikslėlių sąrašą TinyMCE redaktoriui";
+$locale['IMGUP_465'] = "Image list updated";
 // Popup Error/Warning Messages
-$locale['470'] = "Ištrinti šį paveiksliuką?";
+$locale['IMGUP_470'] = "Ištrinti šį paveiksliuką?";
