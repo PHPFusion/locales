5a9919e81111f7c2815e773d4a65b51cf9e04079
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:35:52 +0800

    New translations bbcodes.php (Malay)

--- Malay/locale/Malay/admin/bbcodes.php
+++ Malay/locale/Malay/admin/bbcodes.php
@@ -1,29 +1,35 @@
 <?php
 $locale['BBCA_400'] = "Kod BB";
-$locale['BBCA_400a'] = "BB Codes Admin";
-$locale['BBCA_401'] = "Test BB Codes";
+$locale['BBCA_400a'] = "Pengurusan Kod BB";
+$locale['BBCA_401'] = "Cuba Kod BB";
 // Enabled BB Codes
-$locale['BBCA_402'] = "Enabled BB Codes";
-$locale['BBCA_403'] = "Name";
-$locale['BBCA_404'] = "Icon";
-$locale['BBCA_405'] = "Description";
-$locale['BBCA_406'] = "Usage";
+$locale['BBCA_402'] = "Kod BB Aktif";
+$locale['BBCA_403'] = "Nama";
+$locale['BBCA_404'] = "Ikon";
+$locale['BBCA_405'] = "Penerangan";
+$locale['BBCA_406'] = "Penggunaan";
 $locale['BBCA_407'] = "Susunan Item";
-$locale['BBCA_408'] = "Down";
-$locale['BBCA_409'] = "Up";
+$locale['BBCA_408'] = "Bawah";
+$locale['BBCA_409'] = "Atas";
 $locale['BBCA_410'] = "Tutup";
-$locale['BBCA_411'] = "There are no BB Codes enabled.";
+$locale['BBCA_411'] = "Tiada Kod BB yang aktif.";
 // Disabled BB Codes
-$locale['BBCA_413'] = "Disabled BB Codes";
+$locale['BBCA_413'] = "Kod BB tidak aktif";
 $locale['BBCA_414'] = "Buka";
-$locale['BBCA_416'] = "There are no BB Codes disabled.";
+$locale['BBCA_416'] = "Tiada Kod BB yang tak aktif.";
 // Test BB Codes
-$locale['BBCA_417'] = "BB Codes Test Output";
-$locale['BBCA_418'] = "Disable smileys in this test";
-$locale['BBCA_418a'] = "Test Message";
-$locale['BBCA_418b'] = "Please enter a message to test BB Codes.";
+$locale['BBCA_417'] = "Hasil cubaan Kod BB";
+$locale['BBCA_418'] = "Jangan menggunakan senyuman dalam percubaan ini";
+$locale['BBCA_418a'] = "Mesej Percubaan";
+$locale['BBCA_418b'] = "Masukan mesej untuk mencuba Kod BB.";
 // Notices
-$locale['BBCA_430'] = "BB Code has been moved Up.";
-$locale['BBCA_431'] = "BB Code has been moved Down.";
-$locale['BBCA_432'] = "BB Code has been Enabled.";
-$locale['BBCA_433'] = "BB Code has been Disabled.";
+$locale['BBCA_430'] = "Kod BB telah dipindah ke atas.";
+$locale['BBCA_431'] = "Kod BB telah dipindah ke bawah.";
+$locale['BBCA_432'] = "Kod BB telah diaktifkan.";
+$locale['BBCA_433'] = "Kod BB telah ditutup.";
+$locale['BBCA_444'] = "Kod BB telah dikemaskinikan";
+$locale['BBCA_445'] = "Kod BB tidak dapat dikemaskinikan";
+
+$locale['BBCA_500'] = "Tambah Pemisah";
+$locale['BBCA_501'] = "Pemisah";
+$locale['BBCA_502'] = "Pemisah untuk kumpulan butang Kod BB";
