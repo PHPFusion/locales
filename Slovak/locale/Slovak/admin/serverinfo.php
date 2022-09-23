2f56b91f33252329a9aac21226c1867629ac6293
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:25 +0800

    New translations serverinfo.php (Slovak)

---
+++ Slovak/locale/Slovak/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Informácie o serveri";
+//General
+$locale['SRV_401'] = "Všeobecné";
+$locale['SRV_402'] = "Serverový softvér";
+$locale['SRV_403'] = "Webserver";
+$locale['SRV_404'] = "PHP verzia";
+$locale['SRV_405'] = "PHP rozhranie";
+$locale['SRV_406'] = "Verzia MySQL servera";
+$locale['SRV_406a'] = "Ovládač databázy";
+$locale['SRV_407'] = "Verzia PHPFusionu";
+$locale['SRV_408'] = "Prefix databázy";
+$locale['SRV_409'] = "Prefix cookie";
+$locale['SRV_410'] = "Prehliadač a OS";
+$locale['SRV_411'] = "Preložil: RobiNN (robinn@php-fusion.eu)"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Nastavenia PHP";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "Podpora pre";
+$locale['SRV_426'] = "Nahrávanie súborov (max. veľkosť súboru)";
+$locale['SRV_428'] = "Zobraziť chybové hlášky";
+$locale['SRV_429'] = "Zakázané funkcie";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "verzia";
+//Write permission
+$locale['SRV_440'] = "Oprávnenia zložky";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Podrobnosti";
+$locale['SRV_451'] = "Nie je možné získať údaje. Skontrolujte, či nie je funkcia phpinfo() vypnutá.";
