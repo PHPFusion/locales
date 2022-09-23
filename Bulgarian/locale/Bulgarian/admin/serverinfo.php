a7adcdbefaa59f362d2dc76be5e855708715aa22
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:53 +0800

    New translations serverinfo.php (Bulgarian)

---
+++ Bulgarian/locale/Bulgarian/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Server Info";
+//General
+$locale['SRV_401'] = "Общи";
+$locale['SRV_402'] = "Софтуер на уебсървъра";
+$locale['SRV_403'] = "Софтуер на уебсървъра";
+$locale['SRV_404'] = "PHP версия";
+$locale['SRV_405'] = "PHP интерфейс";
+$locale['SRV_406'] = "MySQL версия на сървъра";
+$locale['SRV_406a'] = "Разреши PDO?";
+$locale['SRV_407'] = "PHPFusion версия";
+$locale['SRV_408'] = "Представка на базата данни";
+$locale['SRV_409'] = "Представка на бисквитките";
+$locale['SRV_410'] = "Браузър и ОС";
+$locale['SRV_411'] = "Превод: ctokepa"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Настройки на PHP";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "Поддръжка на";
+$locale['SRV_426'] = "Качване на файлове (максимален размер на файловете)";
+$locale['SRV_428'] = "Покзване на грешки";
+$locale['SRV_429'] = "Забрана на функции";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "версия";
+//Write permission
+$locale['SRV_440'] = "Разрешения за директорията";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Подробности";
+$locale['SRV_451'] = "Невъзможност за приемане на данни. Проверете дали phpinfo() функцията е изключена.";
