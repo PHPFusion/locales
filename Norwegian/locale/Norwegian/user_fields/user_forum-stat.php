bbbfff6f78e12cc30d87c752b7f2931efa1c3703
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:14:40 +0800

    New translations user_forum-stat.php (Norwegian)

--- Norwegian/locale/Norwegian/user_fields/user_forum-stat.php
+++ Norwegian/locale/Norwegian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Forum innlegg";
+$locale['uf_forum-stat'] = "Forum Posts";
 $locale['uf_forum-stat_desc'] = "Viser antall forum innlegg.";
 $locale['uf_forum-stat_na'] = "Please install Forum infusion.";
