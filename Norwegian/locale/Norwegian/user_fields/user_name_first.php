66041bec3ae1f52b27bd1635776e1051ee3df215
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:19:30 +0800

    New translations user_name_first.php (Norwegian)

--- Norwegian/locale/Norwegian/user_fields/user_name_first.php
+++ Norwegian/locale/Norwegian/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_first'] = "Brukerens fornavn";
+$locale['uf_name_first'] = "Fornavn";
 $locale['uf_name_first_desc'] = "Brukerens faktiske fornavn.";
 $locale['uf_name_first_error'] = "Brukerens fornavn må spesifiseres.";
