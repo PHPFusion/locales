02d4d9143cd14cc67f6f5e9b3b4a64502db0e149
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:50 +0800

    New translations downloads.php (Italian)

--- Italian/infusions/downloads/locale/Italian/search/downloads.php
+++ Italian/infusions/downloads/locale/Italian/search/downloads.php
@@ -1,11 +1,11 @@
 <?php
 $locale['downloads.php'] = "Downloads";
-$locale['d400'] = "Download";
+$locale['d400'] = "Downloads";
 $locale['d401'] = "Download";
-$locale['d402'] = "Download";
+$locale['d402'] = "Downloads";
 $locale['d403'] = "[NUOVO]";
-$locale['d404'] = "Licenza:";
+$locale['d404'] = " licenza:";
 $locale['d405'] = "O/S:";
-$locale['d406'] = "Versione:";
-$locale['d407'] = "Aggiunto il:";
+$locale['d406'] = "versione:";
+$locale['d407'] = "Date Added:";
 $locale['d408'] = "Popolarita:";
