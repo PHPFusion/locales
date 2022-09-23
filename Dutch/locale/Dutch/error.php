66298eb2d573042a2bfd4f6b11cc606be3194fca
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:33:56 +0800

    New translations error.php (Dutch)

--- Dutch/locale/Dutch/error.php
+++ Dutch/locale/Dutch/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Toegang tot deze pagina niet toegestaan!";
 $locale['err403'] = "Toegang tot deze pagina verboden!";
 $locale['err404'] = "Deze pagina is niet gevonden op deze server!";
-$locale['err500'] = "Interne server fout!";
 $locale['errunk'] = "Onbekende fout";
 $locale['errmsg'] = "De link die u hebt gevolgd is misschien verlopen, of de pagina is voor u niet toegankelijk.";
 $locale['errret'] = "Terug naar main site";
