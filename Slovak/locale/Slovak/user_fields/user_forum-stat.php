f8bf94502e3d5c467c017f92a74cb504df284be0
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:14:43 +0800

    New translations user_forum-stat.php (Slovak)

--- Slovak/locale/Slovak/user_fields/user_forum-stat.php
+++ Slovak/locale/Slovak/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Príspevky vo fóre";
+$locale['uf_forum-stat'] = "Príspevky fóra";
 $locale['uf_forum-stat_desc'] = "Zobrazí počet príspevkov vo fóre";
 $locale['uf_forum-stat_na'] = "Prosím, nainštalujte fórum.";
