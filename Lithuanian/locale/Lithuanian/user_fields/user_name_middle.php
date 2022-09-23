9163ada11d7b61ad514cc62f07bc449113deb853
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:21:22 +0800

    New translations user_name_middle.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/user_fields/user_name_middle.php
+++ Lithuanian/locale/Lithuanian/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Antrasis (vidurinis) vardas";
+$locale['uf_name_middle'] = "Antras vardas";
 $locale['uf_name_middle_desc'] = "Tikrasis antrasis nario vardas.";
 $locale['uf_name_middle_error'] = "Antrojo (vidurinio) vardo laukelis negali būti tuščas.";
