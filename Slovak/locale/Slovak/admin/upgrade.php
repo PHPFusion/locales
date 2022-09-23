cd035b5b9cd0c8198df06030d8ece82814d5c943
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:50:41 +0800

    New translations upgrade.php (Slovak)

--- Slovak/locale/Slovak/admin/upgrade.php
+++ Slovak/locale/Slovak/admin/upgrade.php
@@ -1,3 +1,21 @@
 <?php
-$locale['U_0000'] = "Aktualizácia";
-$locale['U_0001'] = "Nie sú k dispozícii žiadne aktualizácie";
+$locale['U_000'] = "Aktualizácia";
+$locale['U_001'] = "Pred aktualizáciou zálohujte svoju databázu a súbory.";
+$locale['U_002'] = "Naposledy kontrolované %s.";
+$locale['U_003'] = "Skontrolovať znova";
+$locale['U_004'] = "K dispozícii je nová aktualizácia";
+$locale['U_005'] = "Nepodarilo sa skontrolovať aktualizácie!";
+$locale['U_006'] = "Nie je k dispozícii žiadna aktualizácia";
+$locale['U_007'] = "Môžete sa automaticky aktualizovať na PHPFusion %s.";
+$locale['U_008'] = "Máte najnovšiu verziu";
+$locale['U_009'] = "Sťahuje sa %s.";
+$locale['U_010'] = "Rozbaľovanie aktualizácie.";
+$locale['U_011'] = "Kopírujú sa súbory.";
+$locale['U_012'] = "Aktualizuje sa databáza.";
+$locale['U_013'] = "Aktualizujú sa preklady.";
+$locale['U_014'] = "PHPFusion bol úspešne aktualizovaný.";
+$locale['U_015'] = "Aktualizáciu sa nepodarilo nainštalovať. Skúste to znova neskôr alebo si stiahnite aktualizáciu manuálne.";
+$locale['U_016'] = "Aktualizovať preklady";
+$locale['U_017'] = "Preklady boli úspešne aktualizované.";
+$locale['U_018'] = "Sťahuje sa jazykový balík %s."; // %s will be replaced with language name
+$locale['U_019'] = "Aktuálna verzia: %s";
