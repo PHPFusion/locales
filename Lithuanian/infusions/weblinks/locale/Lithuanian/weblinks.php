9ccdfc493c9593fdf2947f6ae4a80907d607eeba
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:06:10 +0800

    New translations weblinks.php (Lithuanian)

--- Lithuanian/infusions/weblinks/locale/Lithuanian/weblinks.php
+++ Lithuanian/infusions/weblinks/locale/Lithuanian/weblinks.php
@@ -4,11 +4,11 @@ $locale['web_0000'] = "Nuorodos";
 $locale['web_0001'] = "Visos nuorodos";
 $locale['fmt_open'] = "atidaryti|atidaryti";
 // Sorting
-$locale['web_0030'] = "Naujausia";
+$locale['web_0030'] = "Paskutinysis";
 $locale['web_0031'] = "Dažniausiai atidaroma";
 $locale['web_0032'] = "Seniausias";
 // Views
-$locale['web_0040'] = "Galerijos rodymas";
+$locale['web_0040'] = "Miniatiūrų rodymas";
 $locale['web_0041'] = "Sąrašo rodymas";
 // Messages
 $locale['web_0060'] = "Nesudarytos nuorodų kategorijos";
