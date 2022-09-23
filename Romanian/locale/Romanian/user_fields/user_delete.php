52de7186a481ec465053182ce390769b7307f456
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:12:36 +0800

    New translations user_delete.php (Romanian)

--- Romanian/locale/Romanian/user_fields/user_delete.php
+++ Romanian/locale/Romanian/user_fields/user_delete.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_delete'] = "&#350;terge utilizator";
-$locale['uf_delete_desc'] = "&#350;terge utilizator";
+$locale['uf_delete'] = "&#350;terge utilizatorul";
+$locale['uf_delete_desc'] = "&#350;terge utilizatorul";
 $locale['uf_delete_del'] = "&#350;terge-m&#259;";
 $locale['uf_delete_exit'] = "Cont &#351;ters...";
