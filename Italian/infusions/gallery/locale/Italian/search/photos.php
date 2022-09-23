932a0ceb310f70f7407f479fbf03d9b8728e56d5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:56:43 +0800

    New translations photos.php (Italian)

--- Italian/infusions/gallery/locale/Italian/search/photos.php
+++ Italian/infusions/gallery/locale/Italian/search/photos.php
@@ -1,9 +1,9 @@
 <?php
 $locale['photos.php'] = "Photo Gallery";
-$locale['p400'] = "Immagini";
+$locale['p400'] = "Foto";
 $locale['p401'] = "Immagine";
-$locale['p402'] = "Immagini";
+$locale['p402'] = "Foto";
 $locale['p403'] = "[NUOVO]";
 $locale['p404'] = "Album:";
 $locale['p405'] = "Data inserimento:";
-$locale['p406'] = "Visualizzazioni:";
+$locale['p406'] = "Views:";
