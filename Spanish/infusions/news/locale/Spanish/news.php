6230982afe840a89102310ff28ace2b8cac3b64a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:01:22 +0800

    New translations news.php (Spanish)

--- Spanish/infusions/news/locale/Spanish/news.php
+++ Spanish/infusions/news/locale/Spanish/news.php
@@ -1,18 +1,19 @@
 <?php
-$locale['news_0001'] = "Seguir leyendo";
+$locale['news_0001'] = "Leer más";
 $locale['news_0002'] = "Imprimir";
 $locale['news_0004'] = "Noticias";
-$locale['news_0005'] = "No se han publicado noticias todavía";
-$locale['news_0006'] = "sin categoría";
+$locale['news_0005'] = "No hay noticias";
+$locale['news_0006'] = "Otras Noticias";
 $locale['news_0008'] = "Última actualización";
 $locale['news_0009'] = "Categorías de noticias";
 $locale['news_0010'] = "Todas las demás categorías";
 $locale['news_0011'] = "Noticias más recientes";
 $locale['news_0012'] = "Noticias más comentadas";
 $locale['news_0013'] = "Noticias mejor calificadas";
-$locale['news_0014'] = "Vista de miniatura";
+$locale['news_0014'] = "vista del pulgar";
 $locale['news_0015'] = "Vista de lista";
 $locale['news_0016'] = "No hay categorías definidas.";
-$locale['news_0017'] = "Filtrar por";
+$locale['news_0017'] = "filtrar por:";
 $locale['news_0018'] = "Mostrar todas las noticias";
 $locale['news_0019'] = "Galería de noticias";
+$locale['news_0020'] = "Most Read News";
