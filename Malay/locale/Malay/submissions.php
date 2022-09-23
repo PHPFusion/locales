709d1e88697bb07d36fb7bb509087bd18de91764
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:42:45 +0800

    New translations submissions.php (Malay)

--- Malay/locale/Malay/submissions.php
+++ Malay/locale/Malay/submissions.php
@@ -1,6 +1,6 @@
 <?php
-$locale['article_submit'] = "%s Articles";
+$locale['article_submit'] = "%s artikel";
 $locale['blog_submit'] = "%s Blog";
-$locale['download_submit'] = "%s Downloads";
-$locale['gallery_submit'] = "%s Photos";
-$locale['news_submit'] = "%s News";
+$locale['download_submit'] = "%s Muat Turun";
+$locale['gallery_submit'] = "%s Gambar";
+$locale['news_submit'] = "%s Berita";
