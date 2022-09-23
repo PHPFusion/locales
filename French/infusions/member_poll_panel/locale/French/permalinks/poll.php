f7da7e5ac06574d229b646d9f8bda836ffc7c97f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:58:43 +0800

    New translations poll.php (French)

--- French/infusions/member_poll_panel/locale/French/permalinks/poll.php
+++ French/infusions/member_poll_panel/locale/French/permalinks/poll.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_poll_title'] = "Sondage de membres";
+$locale['pl_poll_title'] = "Sondage des membres";
 $locale['pl_poll_desc'] = "Activer les permaliens pour les liens de sondage des membres dans le site.";
