355cfe1150bc268bd35aea4694e820c39ce99ee8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:35:04 +0800

    New translations blacklist.php (German)

--- German/locale/German/admin/blacklist.php
+++ German/locale/German/admin/blacklist.php
@@ -1,45 +1,44 @@
 <?php
 $locale['BLS_000'] = "Sperrliste";
 //Blacklist message
-$locale['BLS_010'] = "Ung&uuml;ltige E-Mail oder IP-Adresse.";
-$locale['BLS_011'] = "Benutzer zur Blacklist hinzufügen.";
-$locale['BLS_012'] = "Benutzer geänderte Blacklist.";
-$locale['BLS_013'] = "Benutzer von Blacklist gelöscht";
-$locale['BLS_014'] = "Wollen Sie diesen Eintrag wirklich löschen?";
-$locale['BLS_015'] = "Die Blacklist ist zur Zeit leer.";
-$locale['BLS_016'] = "E-Mail-Adresse der Blacklist ist ungültig.";
+$locale['BLS_010'] = "E-Mail- oder IP-Adresse darf nicht leer sein.";
+$locale['BLS_011'] = "Eintrag wurde hinzugefügt.";
+$locale['BLS_012'] = "Eintrag wurde geändert.";
+$locale['BLS_013'] = "Eintrag wurde entfernt.";
+$locale['BLS_014'] = "Möchten Sie diesen Eintrag wirklich löschen?";
+$locale['BLS_015'] = "Die Sperrliste ist zur Zeit leer.";
+$locale['BLS_016'] = "E-Mail-Adresse ist keine gültige E-Mail.";
 
-$locale['BLS_020'] = "Benutzer Blacklist";
-$locale['BLS_021'] = "Benutzer auf der Blacklist bearbeiten";
-$locale['BLS_022'] = "Benutzer auf der Blackliste hinzufügen";
-$locale['BLS_023'] = "Derzeit werden %d von %d gesamten Blacklist-Einträgen angezeigt.";
+$locale['BLS_020'] = "Benutzer Sperrliste";
+$locale['BLS_021'] = "Benutzer auf der Sperrliste bearbeiten";
+$locale['BLS_022'] = "Benutzer auf der Sperrliste hinzufügen";
+$locale['BLS_023'] = "Derzeit werden %d von %d gesamten Sperrliste-Einträge angezeigt.";
 
-$locale['BLS_030'] = "Blacklist Information";
-$locale['BLS_031'] = "Administrator";
+$locale['BLS_030'] = "Sperrlisten Information";
+$locale['BLS_031'] = "Admin";
 $locale['BLS_032'] = "Datum";
-$locale['BLS_033'] = "Optionen";
-$locale['BLS_034'] = "Sperrliste der IP-Adresse: [STRONG]oder[/STRONG]";
+$locale['BLS_033'] = "Einstellungen";
+$locale['BLS_034'] = "Sperrliste IP-Adresse";
 $locale['BLS_035'] = "Sperrliste E-Mail Adresse";
 $locale['BLS_036'] = "Sperrlisten-Grund";
-$locale['BLS_037'] = "Benutzer Blacklist";
+$locale['BLS_037'] = "Benutzer Sperrliste";
 $locale['BLS_038'] = "Aktualisieren";
-$locale['BLS_039'] = "Alles";
+$locale['BLS_039'] = "Alles Auswählen";
 
-$locale['BLS_MS'] = "Die Eingabe einer IP-Adresse verhindert, dass ein Benutzer, dessen IP-Adresse mit dem Eintrag übereinstimmt, diese Seite besucht.
-Du kannst eine vollständige IP eingeben, z.B. <em>23.45.67.89.</em>, oder eine partielle IP, e.. 
-23.45.67</em> oder <em>123.45</em>.
-Bitte beachten Sie: IPv6-Adressen werden auf dieser Seite in ihre volle Länge konvertiert
-z. <em>ABCD:1234:5:6:7:8:9:FF</em> wird als <em> ABCD:1234:0005:0006:0007:0008:0009:00FF</em>angezeigt.
-Gemischte IP-Adressen (beide enthalten sowohl IPv6 als auch IPv4 Teil) werden nicht auf partielle Übereinstimmung überprüft.
+$locale['BLS_MS'] = "Durch die Eingabe einer IP-Adresse wird verhindert, dass ein Benutzer, dessen IP-Adresse mit der Eingabe übereinstimmt, diese Seite besucht.
+Sie können eine vollständige IP eingeben, z. <em>123.45.67.89.</em> oder eine Teil-IP, z. <em>123.45.67</em> oder <em>123.45</em>.
+Bitte beachten Sie: IPv6-Adressen werden auf dieser Seite in ihre vollständige Länge umgewandelt,
+z.B. <em>ABCD:1234:5:6:7:8:9:FF</em> wird als <em>ABCD:1234:0005:0006:0007:0008:0009:00FF</em> angezeigt.
+Gemischte IP-Adressen (die sowohl einen IPv6- als auch einen IPv4-Teil enthalten) werden nicht auf teilweise Übereinstimmung überprüft.
 <br /><br />
-Die Eingabe einer E-Mail-Adresse wird Mitglieder daran hindern, sich unter dieser Adresse zu registrieren.
-Sie können eine vollständige E-Mail-Adresse eingeben.. <em>foo@bar.com</em>oder eine E-Mail-Domain, z.B. <em>bar.com</em>.<br /><br />
+Die Eingabe einer E-Mail-Adresse verhindert, dass sich Mitglieder mit dieser Adresse registrieren.
+Sie können eine vollständige E-Mail-Adresse eingeben, z. <em>foo@bar.com</em> oder eine E-Mail-Domain, z. <em>bar.com</em>.<br /><br />
 
-% - passt zu jeder Zeichenkette.<br /><br />
+% - stimmt mit jeder Zeichenfolge überein.<br /><br />
 
-%..%.%@domain.tld verbietet jede Adresse, die mindestens 3 Punkte enthält.<br />
- %+%@domain.tld verbietet jede Adresse, die mindestens ein Pluszeichen enthält.<br />
+%.%.%.%@domain.tld verbietet jede Adresse, die mindestens 3 Punkte enthält.<br />
+%+%@domain.tld verbietet jede Adresse, die mindestens ein Pluszeichen enthält.<br />
 %@domain.tld verbietet jede Adresse von domain.tld<br />
-%.domain.tld alle Subdomains der Domain. ld<br />
-%payday% verbietet jede Adresse, die das Wort \"Zahlungstag\" enthält, das sehr oft auf Websites auftrat.<br />
-domain.tld ist ein Alias für %@domain.tld um es mit in v7 definierten Regeln kompatibel zu machen.<br />";
+%.domain.tld bannt alle Subdomains von domain.tld<br />
+%payday% sperrt alle Adressen, die das Wort \"payday\" enthalten, das sehr oft auf Websites vorkam.<br />
+domain.tld ist ein Alias ​​von %@domain.tld, um es mit den in v7 definierten Regeln kompatibel zu machen.<br />";
