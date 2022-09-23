9631d8d6b8f965a24568c900003bfc7361b3fa6d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:26 +0800

    New translations serverinfo.php (Swedish)

---
+++ Swedish/locale/Swedish/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Server Info";
+//General
+$locale['SRV_401'] = "Allmänt";
+$locale['SRV_402'] = "Server programvara";
+$locale['SRV_403'] = "Webb-server programvara";
+$locale['SRV_404'] = "PHP version";
+$locale['SRV_405'] = "PHP gränssnitt";
+$locale['SRV_406'] = "MySQL Server version";
+$locale['SRV_406a'] = "Databas hanterare";
+$locale['SRV_407'] = "PHPFusion version";
+$locale['SRV_408'] = "Databas prefix";
+$locale['SRV_409'] = "Cookie prefix";
+$locale['SRV_410'] = "Web-webbläsare och operativsystem";
+$locale['SRV_411'] = "Översatt av:"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "PHP inställningar";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "Stöd för";
+$locale['SRV_426'] = "Uppladdning av filer (max filstorlek)";
+$locale['SRV_428'] = "Visa fel";
+$locale['SRV_429'] = "Inaktiverade funktioner";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "version";
+//Write permission
+$locale['SRV_440'] = "Mappbehörigheter";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Detaljer";
+$locale['SRV_451'] = "Kan inte hämta data. Kontrollera om phpinfo() funktionen är avstängd?";
