7912091858f6c3d1e1693b5453c708da0daa9a77
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:19:42 +0800

    New translations user_location.php (Slovak)

--- Slovak/locale/Slovak/user_fields/user_location.php
+++ Slovak/locale/Slovak/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Bydlisko";
+$locale['uf_location'] = "Poloha";
 $locale['uf_location_desc'] = "Krajina, mesto alebo dedina";
 $locale['uf_location_error'] = "Bydlisko nemôže byť prázdne.";
