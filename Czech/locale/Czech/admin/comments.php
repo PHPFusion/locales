4c778c148c1fd9e23644a357f86e5bca0e43665e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:36:41 +0800

    New translations comments.php (Czech)

--- Czech/locale/Czech/admin/comments.php
+++ Czech/locale/Czech/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Upravit komentář";
-$locale['401'] = "Komentáře";
+$locale['C_400'] = "Upravit komentář";
+$locale['C_401'] = "Komentáře";
 // Comments messages
-$locale['410'] = "Komentář byl aktualizován";
-$locale['411'] = "Komentář byl smazán";
+$locale['C_410'] = "Komentář byl aktualizován";
+$locale['C_411'] = "Komentář byl smazán";
+$locale['C_412'] = "Komentář byl odstraněn a uživatel přidán do černé listiny.";
 // Edit Comment form
-$locale['420'] = "Deaktivujte smajlíky v tomto komentáři";
-$locale['421'] = "Uložit komentář";
+$locale['C_420'] = "Deaktivujte smajlíky v tomto komentáři";
+$locale['C_421'] = "Uložit komentář";
 // Comments
-$locale['431'] = "Vymazat & zablokovat";
-$locale['432'] = "IP uživatele:";
-$locale['433'] = "Smazat tento komentář?";
-$locale['434'] = "Pro tuto položku neexistují žádné komentáře.";
-$locale['435'] = "Opravdu chcete přidat tohoto uživatele do blacklistu?";
-$locale['436'] = "Váš komentář je na černé listině.";
+$locale['C_431'] = "Vymazat & zablokovat";
+$locale['C_432'] = "IP uživatele:";
+$locale['C_433'] = "Smazat tento komentář?";
+$locale['C_434'] = "Pro tuto položku neexistují žádné komentáře.";
+$locale['C_435'] = "Opravdu chcete přidat tohoto uživatele do blacklistu?";
+$locale['C_436'] = "Komentář na černé listině.";
+// Settings
+$locale['C_440'] = "Povolit komentáře?";
+$locale['C_441'] = "Povolit hostovi posílat příspěvky?";
+$locale['C_442'] = "Povolit avatar v komentářích?";
+$locale['C_443'] = "Celkem komentářů na stránku";
+$locale['C_444'] = "Třídění komentářů";
+$locale['C_445'] = "Vzestupně";
+$locale['C_446'] = "Sestupně";
