51d224e1c0948254c886ac44977384e025a66d15
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:53:22 +0800

    New translations bcolor.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/bbcodes/bcolor.php
+++ Lithuanian/locale/Lithuanian/bbcodes/bcolor.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_bcolor_description'] = "Pažymėto teksto fono spalva";
-$locale['bb_bcolor_usage'] = "Nuspalvinti pažymėto teksto foną";
+$locale['bb_bcolor_usage'] = "Pažymėto teksto fono spalva";
 $locale['bb_bcolor_hex'] = "HEX spalvos kodas";
