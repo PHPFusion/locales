d3f76db782546d736dd1f140c7d918ab924b3e61
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:33:19 +0800

    New translations defender.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/defender.php
+++ Lithuanian/locale/Lithuanian/defender.php
@@ -84,8 +84,15 @@ $locale['df_422'] = "Nežinoma klaida (Užklausa)";
 $locale['df_423'] = "File was not uploaded properly. Please try again";
 $locale['df_424'] = "Bandomi įkelti failai užima per daug vietos. Prašome bandyti dar kartą su mažiau failų.";
 
-// ReCaptcha2
-$locale['grecaptcha'] = "Google Recaptcha";
+$locale['error_rate'] = "Prašom įvesti dažnį.";
+$locale['error_type'] = "Prašom nurodyti papildinio tipą.";
+$locale['error_width'] = "Prašom įvesti plotį.";
+$locale['error_height'] = "Prašom įvesti aukštį.";
+$locale['error_value'] = "Prašom įvesti reikšmę.";
+
+// ReCaptcha
+$locale['grecaptcha'] = "Google Recaptcha v2";
+$locale['grecaptcha3'] = "Google Recaptcha v3";
 $locale['grecaptcha_desc'] = "Google Recaptcha laukelių modulis";
 $locale['grecaptcha_placeholder_1'] = "TINKLAPIO RAKTAS (SITE KEY) iš Google";
 $locale['grecaptcha_placeholder_2'] = "SLAPTAS RAKTAS (SECRET-KEY) iš Google";
@@ -98,4 +105,5 @@ $locale['grecaptcha_0102b'] = "Tamsus";
 $locale['grecaptcha_0103'] = "Patikros tipas";
 $locale['grecaptcha_0103a'] = "Tekstas";
 $locale['grecaptcha_0103b'] = "Garsas";
+$locale['grecaptcha_0104'] = "Score";
 $locale['grecaptcha_warn'] = "Prašome įvesti viešą ir privatų reCAPTCHA raktą";
