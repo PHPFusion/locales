b36c9b0ed07ecb8b606baf0ac8b3c12c4e4df4bb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:45:56 +0800

    New translations blog.php (Chinese Simplified)

---
+++ Chinese Simplified/infusions/blog/locale/Chinese Simplified/search/blog.php
@@ -0,0 +1,9 @@
+<?php
+$locale['blog.php'] = "Blog";
+$locale['b400'] = "Blog";
+$locale['b401'] = "博客标题";
+$locale['b402'] = "博客项目";
+$locale['b403'] = "found in";
+$locale['b404'] = "题目";
+$locale['b405'] = "博客文本";
+$locale['b406'] = "扩展博客文本";
