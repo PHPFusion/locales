c66fecf435e1c4a6cd48b5dfb26201a9ba9c6129
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:39:37 +0800

    New translations print.php (Slovak)

--- Slovak/locale/Slovak/print.php
+++ Slovak/locale/Slovak/print.php
@@ -1,12 +1,12 @@
 <?php
 //news, articles, blog
-$locale['400'] = "Pridal ";
+$locale['400'] = "Pridal/a ";
 $locale['401'] = " dňa ";
 $locale['402'] = "Podrobné novinky";
 $locale['403'] = "Podrobné blogy";
 //forums
 $locale['500'] = "Názov témy:";
-$locale['501'] = "Pridal ";
+$locale['501'] = "Pridal/a ";
 $locale['502'] = " dňa ";
 $locale['503'] = "Upravil ";
 //License Agreement
