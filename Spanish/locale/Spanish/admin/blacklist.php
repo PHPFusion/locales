af46604e052f9410ac394b5b9f47c32fdbc05e11
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:35:57 +0800

    New translations blacklist.php (Spanish)

--- Spanish/locale/Spanish/admin/blacklist.php
+++ Spanish/locale/Spanish/admin/blacklist.php
@@ -1,28 +1,28 @@
 <?php
-$locale['BLS_000'] = "Lista negra"; //406
+$locale['BLS_000'] = "Expulsiones";
 //Blacklist message
 $locale['BLS_010'] = "Correo electrónico o dirección IP inválidos.";
 $locale['BLS_011'] = "Agregar usuarios a la lista negra.";
 $locale['BLS_012'] = "Lista negra modificada por el usuario.";
-$locale['BLS_013'] = "Usuario eliminado de la lista negra"; //401
+$locale['BLS_013'] = "Usuario eliminado de la lista negra";
 $locale['BLS_014'] = "¿Está seguro de que desea eliminar esta entrada?";
-$locale['BLS_015'] = "La lista negra está actualmente vacía."; //465
-$locale['BLS_016'] = "La dirección de correo electrónico de la lista negra no es un correo electrónico válido."; //405
+$locale['BLS_015'] = "La lista negra está actualmente vacía.";
+$locale['BLS_016'] = "La dirección de correo electrónico de la lista negra no es un correo electrónico válido.";
 
-$locale['BLS_020'] = "Usuario de la lista negra"; //420
-$locale['BLS_021'] = "Editar usuario en la lista negra"; //421
+$locale['BLS_020'] = "Usuario de la lista negra";
+$locale['BLS_021'] = "Editar usuario en la lista negra";
 $locale['BLS_022'] = "Agregar usuario en la lista negra";
 $locale['BLS_023'] = "Actualmente muestra %d de %d entradas de lista negra.";
 
-$locale['BLS_030'] = "Información en la lista negra"; //461
-$locale['BLS_031'] = "Administrador"; //467
-$locale['BLS_032'] = "Fecha";  //468
-$locale['BLS_033'] = "Opciones"; //462
-$locale['BLS_034'] = "Dirección IP de la lista negra: <strong> o </ strong>"; //441
-$locale['BLS_035'] = "Dirección de correo electrónico de la lista negra"; //442
-$locale['BLS_036'] = "Motivo de la lista negra"; //443
-$locale['BLS_037'] = "Usuario de la lista negra";  //444
-$locale['BLS_038'] = "Actualizar";
+$locale['BLS_030'] = "Información en la lista negra";
+$locale['BLS_031'] = "Administrador";
+$locale['BLS_032'] = "Fecha";
+$locale['BLS_033'] = "Opciones";
+$locale['BLS_034'] = "Dirección IP de la lista negra: <strong> o </ strong>";
+$locale['BLS_035'] = "Dirección de correo electrónico de la lista negra";
+$locale['BLS_036'] = "Motivo de la lista negra";
+$locale['BLS_037'] = "Usuario de la lista negra";
+$locale['BLS_038'] = "Actualización";
 $locale['BLS_039'] = "Select All";
 
 $locale['BLS_MS'] = "Al ingresar una dirección IP evitará que un usuario cuya dirección IP coincida con la entrada visite este sitio.
@@ -41,4 +41,4 @@ Puede ingresar una dirección de correo electrónico completa, p. <em>foo@bar.co
 %@domain.tld prohíbe cualquier dirección de domain.tld<br />
 %.domain.tld prohíbe todos los subdominios de domain.tld<br />
 %payday% prohíbe cualquier dirección que contenga la palabra \"payday\" que a menudo estaba en los sitios.<br />
-domain.tld es un alias de %@domain.tld para hacerlo compatible con las reglas definidas en v7.<br />"; //440
+domain.tld es un alias de %@domain.tld para hacerlo compatible con las reglas definidas en v7.<br />";
