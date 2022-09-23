d270200115da9e0b8b0bf3a523e900e31ac1b049
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:26:21 +0800

    New translations user_theme.php (Czech)

--- Czech/locale/Czech/user_fields/user_theme.php
+++ Czech/locale/Czech/user_fields/user_theme.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_theme'] = "Vzhled";
+$locale['uf_theme'] = "Téma";
 $locale['uf_theme_desc'] = "Povolí uživatelovi vybrat vlastní vzhled";
 $locale['uf_theme_error'] = "Vzhled  nesmí být prázdny.";
