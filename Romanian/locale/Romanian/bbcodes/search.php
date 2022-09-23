598b2c13b6ec9d321e1163a33cfc8a62d78c8dd7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:01:16 +0800

    New translations search.php (Romanian)

--- Romanian/locale/Romanian/bbcodes/search.php
+++ Romanian/locale/Romanian/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Caut&#259; textul dat pe saitul curent";
 $locale['bb_search_usage'] = "Textul de c&#259;utat";
 $locale['bb_search_prefix'] = "Caut&#259; ";
-$locale['bb_search_suffix'] = " pe ";
+$locale['bb_search_suffix'] = " la ";
 $locale['bb_search_where'] = "Unde";
