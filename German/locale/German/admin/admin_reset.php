6304bb1df8f6f13cdc7460bbef0a6c03aa330d41
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:33:19 +0800

    New translations admin_reset.php (German)

--- German/locale/German/admin/admin_reset.php
+++ German/locale/German/admin/admin_reset.php
@@ -2,36 +2,35 @@
 $locale['apw_title'] = "Administratoren-Passwörter Zurücksetzen";
 $locale['apw_desc'] = "Ändert die Login- & Admin Passwörter der Administratoren";
 $locale['apw_admin1'] = "Administrator zurücksetzen";
-$locale['apw_400'] = "Zurücksetzen bei:";
+$locale['apw_400'] = "Administrator(en) zum Zurücksetzen:";
 $locale['apw_401'] = "Alle Administratoren";
 $locale['apw_402'] = "nur Seitenadministratoren";
 $locale['apw_403'] = "nur Administratoren";
-$locale['apw_404'] = "Begr&uuml;ndung der Zur&uuml;cksetzung:";
-$locale['apw_405'] = "Login- &amp; Adminpasswort zur&uuml;cksetzen";
-$locale['apw_406'] = "Passw&ouml;rter zur&uuml;cksetzen";
-$locale['apw_407'] = "Passwort Zurücksetzung auf ";
-$locale['apw_408'] = "Hallo [USER_NAME],
-
-";
-$locale['apw_409'] = "Hallo [USER_NAME],
-
-";
-$locale['apw_410'] = "Ergebnisse";
-$locale['apw_411'] = "<strong>Hinweis:</strong> Du hast kein Adminpasswort angegeben oder es ist ung&uuml;ltig.";
-$locale['apw_412'] = "Adminpasswort:";
-$locale['apw_415'] = "Zur&uuml;cksetzungs Log";
+$locale['apw_404'] = "Begründung für das Zurücksetzen:";
+$locale['apw_405'] = "Login- und Admin-Kennwort zurücksetzen";
+$locale['apw_406'] = "Passwörter zurücksetzen";
+$locale['apw_407'] = "Passwort zurücksetzen auf ";
+$locale['apw_408'] = "Hallo [USER_NAME],<br/>Ihr Admin-Passwort wurde unter [SITEURL] zurückgesetzt:<br/>
+Admin-Passwort: [NEW_ADMIN_PASS]<br/>Der Grund für dieses Zurücksetzen ist: [RESET_MESSAGE]<br/>Mit freundlichen Grüßen<br/>[ADMIN]";
+$locale['apw_409'] = "Hallo [USER_NAME],<br/>Ihr Anmelde- und Administratorkennwort wurde unter [SITEURL] zurückgesetzt:<br/>
+Login-Passwort: [NEW_PASS]<br/>Admin-Passwort: [NEW_ADMIN_PASS]<br/>Der Grund für dieses Zurücksetzen ist: [RESET_MESSAGE]<br/>
+Mit freundlichen Grüßen<br/>[ADMIN]";
+$locale['apw_410'] = "Ergebnisse zurücksetzen";
+$locale['apw_411'] = "Admin-Passwort aktualisieren.";
+$locale['apw_412'] = "Admin Passwort:";
+$locale['apw_415'] = "Protokoll zurücksetzen";
 $locale['apw_416'] = "Log ID";
-$locale['apw_417'] = "Datum";
-$locale['apw_418'] = "Verantwortlicher";
-$locale['apw_419'] = "Zur&uuml;ckgesetzt bei";
+$locale['apw_417'] = "Zeit zurücksetzen";
+$locale['apw_418'] = "Verantwortlich";
+$locale['apw_419'] = "Zurücksetzen";
 $locale['apw_420'] = "Erfolgreich";
 $locale['apw_421'] = "Grund";
 $locale['apw_422'] = "von";
-$locale['apw_423'] = "Kein Grund angegeben.";
+$locale['apw_423'] = "Ohne Grund.";
 $locale['apw_424'] = "Admins zurücksetzen:";
-$locale['apw_425'] = "Admins fehlgeschlagen:";
-$locale['apw_426'] = "Es wurde kein Password ver&auml;ndertNo password is modified.";
-$locale['apw_427'] = "Option";
-$locale['apw_428'] = "Soll der Log wirklich gel&ouml;scht werden?";
-$locale['apw_429'] = "Log gel&ouml;scht";
-$locale['apw_430'] = "Alles";
+$locale['apw_425'] = "Administratoren fehlgeschlagen:";
+$locale['apw_426'] = "Es wird kein Passwort geändert.";
+$locale['apw_427'] = "Einstellungen";
+$locale['apw_428'] = "Möchten Sie dieses Protokoll wirklich löschen?";
+$locale['apw_429'] = "Protokoll gelöscht";
+$locale['apw_430'] = "Alle auswählen";
