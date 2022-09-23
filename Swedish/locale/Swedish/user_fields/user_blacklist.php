a11b1f55970104f3f1694fe0b3b3a990655cd72e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:11:33 +0800

    New translations user_blacklist.php (Swedish)

--- Swedish/locale/Swedish/user_fields/user_blacklist.php
+++ Swedish/locale/Swedish/user_fields/user_blacklist.php
@@ -3,12 +3,3 @@ $locale['uf_blacklist'] = "Svartlista";
 $locale['uf_blacklist_desc'] = "Iintegritetspolicy för Svartlistning";
 $locale['uf_blacklist_error'] = "Fel i datatyp";
 $locale['uf_blacklist_message'] = "När du blockerar någon, kan den personen inte längre se dig inklusive saker du lägger upp och kontaktar dig.";
-
-$locale['uf_blacklist_000'] = "Blockerade och ignorerade användare";
-$locale['uf_blacklist_001'] = "Häv blockering";
-$locale['uf_blacklist_002'] = "Ignorera";
-$locale['uf_blacklist_003'] = "Det finns inga svartlistade användare.";
-$locale['uf_blacklist_004'] = "Användaren %s har tagits bort från din svartlista.";
-$locale['uf_blacklist_005'] = "Användaren svartlistades inte.";
-$locale['uf_blacklist_006'] = "Hittade ingen användare.";
-$locale['uf_blacklist_007'] = "Ej tillgänglig.";
