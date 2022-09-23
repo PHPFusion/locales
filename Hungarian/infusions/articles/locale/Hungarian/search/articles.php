6b09047aacb3c632202f9d4a17b54ba27a2e1324
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:44:07 +0800

    New translations articles.php (Hungarian)

--- Hungarian/infusions/articles/locale/Hungarian/search/articles.php
+++ Hungarian/infusions/articles/locale/Hungarian/search/articles.php
@@ -1,8 +1,8 @@
 <?php
 $locale['articles.php'] = "Cikkek";
 $locale['a400'] = "Cikkek";
-$locale['a401'] = "Cikket";
-$locale['a402'] = "Cikket";
-$locale['a403'] = "találat a";
+$locale['a401'] = "Teljes Cikk";
+$locale['a402'] = "Cikkek";
+$locale['a403'] = "találtam a(z)";
 $locale['a404'] = "Cikk címében";
 $locale['a405'] = "Cikk tartalmában";
