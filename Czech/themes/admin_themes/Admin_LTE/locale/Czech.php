4469c73bbd3f0c780fc60802f134716e6ac09cb1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:08:10 +0800

    New translations English.php (Czech)

---
+++ Czech/themes/admin_themes/Admin_LTE/locale/Czech.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'Máte';
+$locale['ALT_002'] = 'Neexistují žádné zprávy';
+$locale['ALT_003'] = 'Nová zpráva';
+$locale['ALT_004'] = 'Členem od';
+$locale['ALT_005'] = 'Hledat...';
+$locale['ALT_006'] = 'Vytvořil';
+$locale['ALT_007'] = 'Admin heslo';
+$locale['ALT_008'] = 'Možnosti rozložení';
+$locale['ALT_009'] = 'Fixní uspořádání';
+$locale['ALT_010'] = 'Malý boční panel';
+$locale['ALT_011'] = 'Vzhledy';
+$locale['ALT_012'] = 'Tmavá';
+$locale['ALT_013'] = 'Světlá';
