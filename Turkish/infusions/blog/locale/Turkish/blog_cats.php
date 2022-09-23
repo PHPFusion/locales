0c044b54f1d6186b49b4c2975595f1462f74ae84
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:26 +0800

    New translations blog_cats.php (Turkish)

--- Turkish/infusions/blog/locale/Turkish/blog_cats.php
+++ Turkish/infusions/blog/locale/Turkish/blog_cats.php
@@ -4,6 +4,6 @@ $locale['401'] = "Kategori:";
 $locale['402'] = "Öğe Sayısı:";
 $locale['403'] = "Kategorisiz Bloglar";
 $locale['404'] = "Bu Kategoride Hiç Blog Gönderisi Yok.";
-$locale['405'] = "Daha Fazla";
+$locale['405'] = "Daha fazlası";
 $locale['406'] = "Tüm Kategorileri Göster";
 $locale['407'] = "Tanımlanmış Blog Kategorisi Yok";
