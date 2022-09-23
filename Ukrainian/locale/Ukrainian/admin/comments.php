361bb86892b27da4a9386c8c7342f2a8f48fb6a7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:47 +0800

    New translations comments.php (Ukrainian)

--- Ukrainian/locale/Ukrainian/admin/comments.php
+++ Ukrainian/locale/Ukrainian/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Редагувати коментар";
-$locale['401'] = "Коментарі";
+$locale['C_400'] = "Редагувати коментар";
+$locale['C_401'] = "Коментарі";
 // Comments messages
-$locale['410'] = "Коментарі змінено";
-$locale['411'] = "Коментарі видалено";
+$locale['C_410'] = "Коментарі змінено";
+$locale['C_411'] = "Коментарі видалено";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
 // Edit Comment form
-$locale['420'] = "Вимкнути усміхайлики в коментарях";
-$locale['421'] = "Зберегти";
+$locale['C_420'] = "Вимкнути усміхайлики в коментарях";
+$locale['C_421'] = "Зберегти";
 // Comments
-$locale['431'] = "Видалити";
-$locale['432'] = "Адреса IP:";
-$locale['433'] = "Видалити коментар?";
-$locale['434'] = "Коментарі відсутні";
-$locale['435'] = "Do you really want to add this user to a blacklist?";
-$locale['436'] = "Your comment are blacklisted.";
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "Адреса IP:";
+$locale['C_433'] = "Видалити коментар?";
+$locale['C_434'] = "Коментарі відсутні";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Система коментарів";
+$locale['C_441'] = "Повідомлення в чаті від гостей";
+$locale['C_442'] = "Аватари в коментарях";
+$locale['C_443'] = "Коментарів на одній сторінці";
+$locale['C_444'] = "Сортування коментарів";
+$locale['C_445'] = "зростанням";
+$locale['C_446'] = "спаданням";
