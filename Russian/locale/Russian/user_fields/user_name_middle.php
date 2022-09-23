b83abfdfc083d3d878ff751d02a188ac3824f670
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:21:04 +0800

    New translations user_name_middle.php (Russian)

--- Russian/locale/Russian/user_fields/user_name_middle.php
+++ Russian/locale/Russian/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Отчество (второе имя)";
+$locale['uf_name_middle'] = "Отчество";
 $locale['uf_name_middle_desc'] = "Настоящее отчество (второе имя) пользователя.";
 $locale['uf_name_middle_error'] = "Отчество (второе имя) является обязательным для указания полем.";
