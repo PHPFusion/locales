2137df06709e626fa9e3da9503c83af4122acc00
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:41:15 +0800

    New translations image_uploads.php (Turkish)

--- Turkish/locale/Turkish/admin/image_uploads.php
+++ Turkish/locale/Turkish/admin/image_uploads.php
@@ -1,30 +1,30 @@
 <?php
-$locale['100'] = "Fusion Dosya Yöneticisi";
-
+$locale['IMGUP_100'] = "Fusion Dosya Yönetimi";
 // Delete Image
-$locale['400'] = "Resim Sil";
-$locale['401'] = "Resim Silindi";
-$locale['402'] = "Görseller Yöneticisine Dön";
-$locale['403'] = "Site Yönetimine Dön";
+$locale['IMGUP_400'] = "Resim Sil";
+$locale['IMGUP_401'] = "Resim Silindi";
+$locale['IMGUP_402'] = "Görseller Yöneticisine Dön";
+$locale['IMGUP_403'] = "Yönetim Sayfasına Dön";
 // Upload Image
-$locale['420'] = "Resim Yükle";
-$locale['421'] = "Dosya İsmi:";
-$locale['422'] = "Ana Görüntüler";
-$locale['423'] = "Makale Görüntüleri";
-$locale['424'] = "Haber Görselleri";
-$locale['425'] = "Maks. Dosya boyutu %s / İzin verilen dosya türleri .jpg,.JPG,.png,.PNG,.JPEG,.gif,.GIF,.bmp,.BMP,.svg,.SVG,.tiff,.TIFF";
-$locale['426'] = "Görüntü Yüklendi";
-$locale['427'] = "Haber Kategorileri Görselleri";
-$locale['428'] = "Blog Görselleri";
-$locale['429'] = "Blog Kategorileri Görüntüleri";
+$locale['IMGUP_420'] = "Resim Yükle";
+$locale['IMGUP_421'] = "Dosya İsmi:";
+$locale['IMGUP_422'] = "Ana Görüntüler";
+$locale['IMGUP_423'] = "Makale Görüntüleri";
+$locale['IMGUP_424'] = "Haber Resimleri";
+$locale['IMGUP_425'] = "Max. file size %s / Allowed file types .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_426'] = "Görüntü Yüklendi";
+$locale['IMGUP_427'] = "Haber Kategorileri Görselleri";
+$locale['IMGUP_428'] = "Blog Görselleri";
+$locale['IMGUP_429'] = "Blog Kategorileri Görüntüleri";
 // View Image
-$locale['440'] = "Resmi Görüntüle";
-$locale['441'] = "Bilinmeyen dosya biçimi, resim görüntülenemiyor.";
-$locale['442'] = "Bu resmi sil";
+$locale['IMGUP_440'] = "Resmi Görüntüle";
+$locale['IMGUP_441'] = "Bilinmeyen dosya biçimi, resim görüntülenemiyor.";
+$locale['IMGUP_442'] = "Bu resmi sil";
 // Image Management
-$locale['460'] = "Görüntüler";
-$locale['461'] = "Göster";
-$locale['463'] = "Bu klasör boş";
-$locale['464'] = "TinyMCE editörü için görüntü listesini güncelle";
+$locale['IMGUP_460'] = "Görüntüler";
+$locale['IMGUP_461'] = "Görüntüle";
+$locale['IMGUP_463'] = "Bu klasör boş";
+$locale['IMGUP_464'] = "TinyMCE editörü için görüntü listesini güncelle";
+$locale['IMGUP_465'] = "Image list updated";
 // Popup Error/Warning Messages
-$locale['470'] = "Bu resim silinsin mi?";
+$locale['IMGUP_470'] = "Bu resim silinsin mi?";
