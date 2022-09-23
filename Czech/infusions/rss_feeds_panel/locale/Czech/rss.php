7befa75b4e088ec63fd2e5f91e0688c576116fe0
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:02:44 +0800

    New translations rss.php (Czech)

--- Czech/infusions/rss_feeds_panel/locale/Czech/rss.php
+++ Czech/infusions/rss_feeds_panel/locale/Czech/rss.php
@@ -2,9 +2,9 @@
 $locale['rss_title'] = "RSS kanály";
 $locale['rss_articles'] = "Články";
 $locale['rss_blog'] = "Blog";
-$locale['rss_downloads'] = "Soubory ke stažení";
+$locale['rss_downloads'] = "Ke stažení";
 $locale['rss_forums'] = "Fórum";
-$locale['rss_news'] = "Novinky";
+$locale['rss_news'] = "Zprávy";
 $locale['rss_weblinks'] = "Webové odkazy";
 $locale['rss_in'] = " v ";
 $locale['rss_nodata'] = "Žádné RSS údaje nejsou k dispozici";
