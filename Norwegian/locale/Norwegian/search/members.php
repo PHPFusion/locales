e111aacc78336751a9bf76a6ca5aecf5de35c24f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:09:17 +0800

    New translations members.php (Norwegian)

--- Norwegian/locale/Norwegian/search/members.php
+++ Norwegian/locale/Norwegian/search/members.php
@@ -1,5 +1,5 @@
 <?php
 $locale['m400'] = "Brukere";
-$locale['m401'] = "Bruker";
+$locale['m401'] = "Medlem";
 $locale['m402'] = "Brukere";
 $locale['m403'] = "Profilen er kun synlig for registrerte brukere!";
