1721350cb7d3e36bd77599db3c878f411f9e1bff
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:25 +0800

    New translations error.php (Romanian)

--- Romanian/locale/Romanian/error.php
+++ Romanian/locale/Romanian/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Nu ai access pt vizualiza aceast&#259; pagin&#259;!";
 $locale['err403'] = "Acces refuzat la aceast&#259; pagin&#259;!";
 $locale['err404'] = "Aceast&#259; pagin&#259; nu a fost gasit&#259; pe server!";
-$locale['err500'] = "Eroare intern&#259; server!";
 $locale['errunk'] = "Eroare necunoscut&#259;";
 $locale['errmsg'] = "Urm&#259;toarea leg&#259;tur&#259; poate a expirat, sau poate nu este accesibil&#259; pentru tine.";
 $locale['errret'] = "Revino la site-ul principal";
