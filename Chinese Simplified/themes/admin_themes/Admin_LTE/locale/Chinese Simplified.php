5bdf9b3b650dd309c4767594a05c1bad7815fddf
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:07:46 +0800

    New translations English.php (Chinese Simplified)

---
+++ Chinese Simplified/themes/admin_themes/Admin_LTE/locale/Chinese Simplified.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = '你有';
+$locale['ALT_002'] = 'There are no messages';
+$locale['ALT_003'] = 'New Message';
+$locale['ALT_004'] = 'Member since';
+$locale['ALT_005'] = '搜索...';
+$locale['ALT_006'] = 'Created by';
+$locale['ALT_007'] = '管理员密码';
+$locale['ALT_008'] = '布局选项';
+$locale['ALT_009'] = '固定布局';
+$locale['ALT_010'] = '侧边栏';
+$locale['ALT_011'] = '外观';
+$locale['ALT_012'] = 'Dark';
+$locale['ALT_013'] = 'Light';
