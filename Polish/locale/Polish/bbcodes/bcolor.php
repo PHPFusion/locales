036e2e060e555a68cd190f49c5b95c3f51f2c9f4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:53:24 +0800

    New translations bcolor.php (Polish)

--- Polish/locale/Polish/bbcodes/bcolor.php
+++ Polish/locale/Polish/bbcodes/bcolor.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_bcolor_description'] = "Zmienia kolor tła zaznaczonego tekstu";
-$locale['bb_bcolor_usage'] = "Tekst ze zmienionym kolorem tla";
+$locale['bb_bcolor_usage'] = "Zmienia kolor tła zaznaczonego tekstu";
 $locale['bb_bcolor_hex'] = "Heksadecymalna (szesnastkowa) wartość koloru tła";
