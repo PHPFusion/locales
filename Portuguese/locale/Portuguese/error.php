8014d552fbebceb5a8232286d353bf8468deca89
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:33:59 +0800

    New translations error.php (Portuguese)

--- Portuguese/locale/Portuguese/error.php
+++ Portuguese/locale/Portuguese/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Acesso não-autorizado a esta página";
 $locale['err403'] = "Acesso proibido a esta página";
 $locale['err404'] = "Desculpe, esta página não está disponível neste servidor";
-$locale['err500'] = "Erro Interno do Servidor";
 $locale['errunk'] = "Erro Desconhecido";
 $locale['errmsg'] = "O link que você seguiu pode ter expirado, ou a página pode não estar acessível para si.";
 $locale['errret'] = "Retornar à página principal";
