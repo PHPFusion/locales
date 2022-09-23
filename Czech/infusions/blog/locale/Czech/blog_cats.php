16ddf6c9007ad96bf56da9e4f37f73ead69b77a7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:47:25 +0800

    New translations blog_cats.php (Czech)

--- Czech/infusions/blog/locale/Czech/blog_cats.php
+++ Czech/infusions/blog/locale/Czech/blog_cats.php
@@ -4,6 +4,6 @@ $locale['401'] = "Kategorie:";
 $locale['402'] = "Počet položek:";
 $locale['403'] = "Nekategorizované blogy";
 $locale['404'] = "V této kategorii nejsou žádné blogy.";
-$locale['405'] = "Číst více";
+$locale['405'] = "Více";
 $locale['406'] = "Zobrazit všechny kategorie";
 $locale['407'] = "Nebyly definovány žádné kategorie blogu";
