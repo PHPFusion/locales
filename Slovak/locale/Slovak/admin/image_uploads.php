444357c4011e539678b1499b2d03584b9110bfe6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:41:12 +0800

    New translations image_uploads.php (Slovak)

--- Slovak/locale/Slovak/admin/image_uploads.php
+++ Slovak/locale/Slovak/admin/image_uploads.php
@@ -1,30 +1,30 @@
 <?php
-$locale['100'] = "Fusion správca súborov";
-
+$locale['IMGUP_100'] = "Fusion správca súborov";
 // Delete Image
-$locale['400'] = "Vymazať obrázok";
-$locale['401'] = "Obrázok vymazaný";
-$locale['402'] = "Návrat do administrácie obrázkov";
-$locale['403'] = "Návrat do administrátorského indexu";
+$locale['IMGUP_400'] = "Vymazať obrázok";
+$locale['IMGUP_401'] = "Obrázok vymazaný";
+$locale['IMGUP_402'] = "Návrat do administrácie obrázkov";
+$locale['IMGUP_403'] = "Návrat do administrátorského indexu";
 // Upload Image
-$locale['420'] = "Nahrať obrázok";
-$locale['421'] = "Názov obrázka:";
-$locale['422'] = "Hlavné obrázky";
-$locale['423'] = "Obrázky článkov";
-$locale['424'] = "Obrázky noviniek";
-$locale['425'] = "Max. veľkosť súboru %s / Povolené typy súborov .jpg,.JPG,.png,.PNG,.JPEG,.gif,.GIF,.bmp,.BMP,.svg,.SVG,.tiff,.TIFF";
-$locale['426'] = "Obrázok bol nahratý";
-$locale['427'] = "Obrázky kategórií noviniek";
-$locale['428'] = "Obrázky blogov";
-$locale['429'] = "Obrázky kategórií blogov";
+$locale['IMGUP_420'] = "Nahrať obrázok";
+$locale['IMGUP_421'] = "Názov súboru:";
+$locale['IMGUP_422'] = "Hlavné obrázky";
+$locale['IMGUP_423'] = "Obrázky článkov";
+$locale['IMGUP_424'] = "Obrázky novinky";
+$locale['IMGUP_425'] = "Max. file size %s / Allowed file types .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_426'] = "Obrázok bol nahratý";
+$locale['IMGUP_427'] = "Obrázky kategórií noviniek";
+$locale['IMGUP_428'] = "Obrázky blogov";
+$locale['IMGUP_429'] = "Obrázky kategórií blogov";
 // View Image
-$locale['440'] = "Náhľad";
-$locale['441'] = "Neznámy formát súboru, nemôžem zobraziť obrázok.";
-$locale['442'] = "Vymazať tento obrázok";
+$locale['IMGUP_440'] = "Náhľad";
+$locale['IMGUP_441'] = "Neznámy formát súboru, nemôžem zobraziť obrázok.";
+$locale['IMGUP_442'] = "Vymazať tento obrázok";
 // Image Management
-$locale['460'] = "Obrázky";
-$locale['461'] = "Náhľad";
-$locale['463'] = "Táto zložka je prázdna";
-$locale['464'] = "Obnoviť zoznam obrázkov pre TinyMCE editor";
+$locale['IMGUP_460'] = "Obrázky";
+$locale['IMGUP_461'] = "Zobraziť";
+$locale['IMGUP_463'] = "Táto zložka je prázdna";
+$locale['IMGUP_464'] = "Obnoviť zoznam obrázkov pre TinyMCE editor";
+$locale['IMGUP_465'] = "Image list updated";
 // Popup Error/Warning Messages
-$locale['470'] = "Vymazať tento obrázok?";
+$locale['IMGUP_470'] = "Vymazať tento obrázok?";
