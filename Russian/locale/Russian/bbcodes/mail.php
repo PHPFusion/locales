bf27085738e8c173574f9e3d488bff142a993e5f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:59:24 +0800

    New translations mail.php (Russian)

--- Russian/locale/Russian/bbcodes/mail.php
+++ Russian/locale/Russian/bbcodes/mail.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_mail'] = "Электропочта";
-$locale['bb_mail_description'] = "Отображение адреса Е-мейла активной ссылкой";
+$locale['bb_mail_description'] = "Делает адрес электронной почты кликабельным";
 $locale['bb_mail_usage'] = "Адрес для отображения ссылкой";
