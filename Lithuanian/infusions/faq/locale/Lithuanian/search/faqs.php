e8289c55b98d8aca390b71dd0c66c8d45cb832e9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:51:18 +0800

    New translations faqs.php (Lithuanian)

--- Lithuanian/infusions/faq/locale/Lithuanian/search/faqs.php
+++ Lithuanian/infusions/faq/locale/Lithuanian/search/faqs.php
@@ -1,8 +1,8 @@
 <?php
 $locale['faqs.php'] = "DUK";
-$locale['fq400'] = "DUK";
+$locale['fq400'] = "D.U.K.";
 $locale['fq401'] = "DUK";
-$locale['fq402'] = "DUK";
+$locale['fq402'] = "D.U.K.";
 $locale['fq403'] = "rasta";
 $locale['fq404'] = "DUK klausimas";
 $locale['fq405'] = "DUK atsakymas";
