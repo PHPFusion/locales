e0b15603cd68e97bd438da89a5e878aeafdd8fb9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:41:09 +0800

    New translations image_uploads.php (Polish)

--- Polish/locale/Polish/admin/image_uploads.php
+++ Polish/locale/Polish/admin/image_uploads.php
@@ -1,30 +1,30 @@
 <?php
-$locale['100'] = "Menedżer plików Fusion";
-
+$locale['IMGUP_100'] = "Menedżer plików Fusion";
 // Delete Image
-$locale['400'] = "Usuń Obraz";
-$locale['401'] = "Obraz usunięty";
-$locale['402'] = "Powrót do obrazów administratora";
-$locale['403'] = "Powrót do strony głównej administratora";
+$locale['IMGUP_400'] = "Usuń Obraz";
+$locale['IMGUP_401'] = "Obraz usunięty";
+$locale['IMGUP_402'] = "Powrót do obrazów administratora";
+$locale['IMGUP_403'] = "Powrót do strony głównej administratora";
 // Upload Image
-$locale['420'] = "Prześlij obraz";
-$locale['421'] = "Nazwa pliku:";
-$locale['422'] = "Obrazy główne";
-$locale['423'] = "Obrazy artykułów";
-$locale['424'] = "Obrazy aktualności";
-$locale['425'] = "Maksymalny rozmiar pliku %s / Dozwolone typy plików .jpg,.JPG,.png,.PNG,.JPEG,.gif,.GIF,.bmp,.BMP,.svg,.SVG,.tiff,.TIFF";
-$locale['426'] = "Obraz przesłano";
-$locale['427'] = "Obrazy kategorii aktualności";
-$locale['428'] = "Obrazy wpisów";
-$locale['429'] = "Obrazy kategorii wpisów";
+$locale['IMGUP_420'] = "Prześlij obraz";
+$locale['IMGUP_421'] = "Nazwa pliku:";
+$locale['IMGUP_422'] = "Obrazy główne";
+$locale['IMGUP_423'] = "Obrazy artykułów";
+$locale['IMGUP_424'] = "Zdjęcia aktualności";
+$locale['IMGUP_425'] = "Maksymalny rozmiar pliku %s / Dozwolone typy plików .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_426'] = "Obraz przesłano";
+$locale['IMGUP_427'] = "Obrazy kategorii aktualności";
+$locale['IMGUP_428'] = "Obrazy wpisów";
+$locale['IMGUP_429'] = "Obrazy kategorii wpisów";
 // View Image
-$locale['440'] = "Wyświetl obraz";
-$locale['441'] = "Nieznany format pliku, nie można wyświetlić obrazu.";
-$locale['442'] = "Usuń ten obraz";
+$locale['IMGUP_440'] = "Wyświetl obraz";
+$locale['IMGUP_441'] = "Nieznany format pliku, nie można wyświetlić obrazu.";
+$locale['IMGUP_442'] = "Usuń ten obraz";
 // Image Management
-$locale['460'] = "Obrazy";
-$locale['461'] = "Podgląd";
-$locale['463'] = "Ten katalog jest pusty";
-$locale['464'] = "Zaktualizuj listę obrazów dla edytora TinyMCE";
+$locale['IMGUP_460'] = "Obrazy";
+$locale['IMGUP_461'] = "Zobacz";
+$locale['IMGUP_463'] = "Ten katalog jest pusty";
+$locale['IMGUP_464'] = "Zaktualizuj listę obrazów dla edytora TinyMCE";
+$locale['IMGUP_465'] = "Lista obrazów zaktualizowana";
 // Popup Error/Warning Messages
-$locale['470'] = "Usunąć ten obraz?";
+$locale['IMGUP_470'] = "Usunąć ten obraz?";
