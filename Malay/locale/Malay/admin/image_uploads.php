ed3f47b0f20ec36738789b417042834393741d82
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:41:23 +0800

    New translations image_uploads.php (Malay)

--- Malay/locale/Malay/admin/image_uploads.php
+++ Malay/locale/Malay/admin/image_uploads.php
@@ -1,30 +1,30 @@
 <?php
-$locale['100'] = "Fusion File Manager";
-
+$locale['IMGUP_100'] = "Pengurus Fail Fusion";
 // Delete Image
-$locale['400'] = "Padam Imej";
-$locale['401'] = "Imej dipadam";
-$locale['402'] = "Kembali ke Admin Imej";
-$locale['403'] = "Kembali ke Indeks Admin";
+$locale['IMGUP_400'] = "Padam Imej";
+$locale['IMGUP_401'] = "Imej dipadam";
+$locale['IMGUP_402'] = "Kembali ke Admin Imej";
+$locale['IMGUP_403'] = "Kembali ke Indeks Admin";
 // Upload Image
-$locale['420'] = "Muat Naik Imej";
-$locale['421'] = "Nama Fail:";
-$locale['422'] = "Imej Utama";
-$locale['423'] = "Imej Artikel";
-$locale['424'] = "Imej Berita";
-$locale['425'] = "Anda hanya boleh muat naik imej jenis format gif, jpg atau png";
-$locale['426'] = "Imej telah dimuatnaik";
-$locale['427'] = "Imej Kategori Berita";
-$locale['428'] = "Imej Blog";
-$locale['429'] = "Imej Kategori Blog";
+$locale['IMGUP_420'] = "Muat Naik Imej";
+$locale['IMGUP_421'] = "Nama Fail:";
+$locale['IMGUP_422'] = "Imej Utama";
+$locale['IMGUP_423'] = "Imej Artikel";
+$locale['IMGUP_424'] = "Imej Berita";
+$locale['IMGUP_425'] = "Saiz fail makes %s / Jenis fail dibenarkan . jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_426'] = "Imej telah dimuatnaik";
+$locale['IMGUP_427'] = "Imej Kategori Berita";
+$locale['IMGUP_428'] = "Imej Blog";
+$locale['IMGUP_429'] = "Imej Kategori Blog";
 // View Image
-$locale['440'] = "Papar Imej";
-$locale['441'] = "Jenis fail imej tidak dapat dibaca, paparan dibatalkan";
-$locale['442'] = "Padam Imej ini";
+$locale['IMGUP_440'] = "Papar Imej";
+$locale['IMGUP_441'] = "Jenis fail imej tidak dapat dibaca, paparan dibatalkan";
+$locale['IMGUP_442'] = "Padam Imej ini";
 // Image Management
-$locale['460'] = "Imej";
-$locale['461'] = "Papar";
-$locale['463'] = "Folder ini kosong";
-$locale['464'] = "Kemaskinikan senarai imej untuk penyunting TinyMCE";
+$locale['IMGUP_460'] = "Gambar";
+$locale['IMGUP_461'] = "Tinjauan";
+$locale['IMGUP_463'] = "Folder ini kosong";
+$locale['IMGUP_464'] = "Kemaskinikan senarai imej untuk penyunting TinyMCE";
+$locale['IMGUP_465'] = "Senarai imej telah dikemaskini";
 // Popup Error/Warning Messages
-$locale['470'] = "Padam imej ini?";
+$locale['IMGUP_470'] = "Padam imej ini?";
