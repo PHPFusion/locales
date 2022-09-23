74772ff13ea150919eba28087fbeb5b9fcf68653
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:55:37 +0800

    New translations code.php (Norwegian)

--- Norwegian/locale/Norwegian/bbcodes/code.php
+++ Norwegian/locale/Norwegian/bbcodes/code.php
@@ -1,5 +1,5 @@
 <?php
 $locale['bb_code_description'] = "Viser teksten som kode";
 $locale['bb_code_usage'] = "Noen koder";
-$locale['bb_code_code'] = "Kode";
+$locale['bb_code_code'] = "Code";
 $locale['bb_code_save'] = "Last ned kildekode";
