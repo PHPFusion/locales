6077a14eedb0b576bbd6c93e55a5e2fe512b27cd
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:59:35 +0800

    New translations news.php (Czech)

--- Czech/infusions/news/locale/Czech/permalinks/news.php
+++ Czech/infusions/news/locale/Czech/permalinks/news.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_news_title'] = "Novinky";
+$locale['pl_news_title'] = "Zprávy";
 $locale['pl_news_desc'] = "Povolit trvalé odkazy pro novinky.";
 $locale['pl_tags_001'] = "ID novinky.";
 $locale['pl_tags_002'] = "Název nebo předmět novinky.";
