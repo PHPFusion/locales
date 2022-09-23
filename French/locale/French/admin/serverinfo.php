bb0e7ee90d13443bd0079f6ddcb28f8fccd6c7d6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:48 +0800

    New translations serverinfo.php (French)

---
+++ French/locale/French/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Server Info";
+//General
+$locale['SRV_401'] = "Général";
+$locale['SRV_402'] = "Logiciel serveur";
+$locale['SRV_403'] = "Logiciel de serveur Web";
+$locale['SRV_404'] = "Version de PHP";
+$locale['SRV_405'] = "Interface PHP";
+$locale['SRV_406'] = "Version du serveur MySQL";
+$locale['SRV_406a'] = "Pilote de base de données";
+$locale['SRV_407'] = "Version PHPFusion";
+$locale['SRV_408'] = "Préfixe de la base de données";
+$locale['SRV_409'] = "Préfixe des cookies";
+$locale['SRV_410'] = "Navigateur Web et système d'exploitation";
+$locale['SRV_411'] = "Traduit par: lass1976, syrig"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Configuration PHP";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "L'assistance pour";
+$locale['SRV_426'] = "Envoi de fichiers (taille max)";
+$locale['SRV_428'] = "Afficher les erreurs";
+$locale['SRV_429'] = "Fonctions désactivées";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "version";
+//Write permission
+$locale['SRV_440'] = "Permissions du dossier";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Informations";
+$locale['SRV_451'] = "Impossible de récupérer les données. Vérifier si la fonction phpinfo() est désactivée ?";
