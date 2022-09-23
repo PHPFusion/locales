3170474ab0f57bc99b878e339d28b7f8203a0ffa
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:29 +0800

    New translations serverinfo.php (Ukrainian)

---
+++ Ukrainian/locale/Ukrainian/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Server Info";
+//General
+$locale['SRV_401'] = "Загальне";
+$locale['SRV_402'] = "Сервер";
+$locale['SRV_403'] = "Веб-сервер";
+$locale['SRV_404'] = "Версія PHP";
+$locale['SRV_405'] = "Інтерфейс PHP";
+$locale['SRV_406'] = "Версія MySQL";
+$locale['SRV_406a'] = "Драйвер бази даних";
+$locale['SRV_407'] = "Версія PHPFusion";
+$locale['SRV_408'] = "Префікс бази даних PHPFusion";
+$locale['SRV_409'] = "Префікс файлів cookie PHPFusion";
+$locale['SRV_410'] = "Переглядач та ОС";
+$locale['SRV_411'] = "Перекладав: kot2007"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Параметри PHP";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "Підтримка RegisterGlobals";
+$locale['SRV_425'] = "Підтримка";
+$locale['SRV_426'] = "Завантаження файлів (макс. розмір)";
+$locale['SRV_428'] = "Відображення помилок";
+$locale['SRV_429'] = "Заборонені функції";
+$locale['SRV_430'] = "пусто";
+$locale['SRV_431'] = "версія";
+//Write permission
+$locale['SRV_440'] = "Права доступу";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Подробиці";
+$locale['SRV_451'] = "Неможливо отримати дані. Перевірте чи не вимкнена функція phpinfo()?";
