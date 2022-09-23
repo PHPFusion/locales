267478288ee8bfd50000d253ce8e0c618457dc30
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:07:52 +0800

    New translations English.php (Slovak)

---
+++ Slovak/themes/admin_themes/Admin_LTE/locale/Slovak.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'Máte';
+$locale['ALT_002'] = 'Neexistujú žiadne správy';
+$locale['ALT_003'] = 'Napísať novú správu';
+$locale['ALT_004'] = 'Členom od';
+$locale['ALT_005'] = 'Hľadať...';
+$locale['ALT_006'] = 'Vytvoril';
+$locale['ALT_007'] = 'Admin heslo';
+$locale['ALT_008'] = 'Možnosti rozloženia';
+$locale['ALT_009'] = 'Fixné usporiadanie';
+$locale['ALT_010'] = 'Malý bočný panel';
+$locale['ALT_011'] = 'Vzhľady';
+$locale['ALT_012'] = 'Tmavá';
+$locale['ALT_013'] = 'Svetlá';
