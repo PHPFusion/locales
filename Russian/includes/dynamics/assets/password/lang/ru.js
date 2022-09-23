8ad72098bd0b30fefeafc31d2b72fe823059cb08
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:33:38 +0800

    New translations en.js (Russian)

--- Russian/includes/dynamics/assets/password/lang/ru.js
+++ Russian/includes/dynamics/assets/password/lang/ru.js
@@ -1,17 +1,17 @@
 var pwstrength_locale = {
-    wordMinLength: 'Слишком короткий пароль',
-    wordMaxLength: 'Ваш пароль слишком длинный',
-    wordInvalidChar: 'Ваш пароль содержит недопустимый символ',
-    wordNotEmail: 'Не используйте e-mail в качестве пароля',
-    wordSimilarToUsername: 'Пароль не должен содержать логин',
-    wordTwoCharacterClasses: 'Используйте разные классы символов',
-    wordRepetitions: 'Слишком много повторений',
-    wordSequences: 'Пароль содержит последовательности',
-    errorList: 'Ошибки:',
-    veryWeak: 'Очень слабый',
-    weak: 'Слабый',
-    normal: 'Нормальный',
-    medium: 'Средний',
-    strong: 'Сильный',
-    veryStrong: 'Очень сильный'
-}
+    wordMinLength: 'Your password is too short',
+    wordMaxLength: 'Your password is too long',
+    wordInvalidChar: 'Your password contains an invalid character',
+    wordNotEmail: 'Do not use your email as your password',
+    wordSimilarToUsername: 'Your password cannot contain your username',
+    wordTwoCharacterClasses: 'Use different character classes',
+    wordRepetitions: 'Too many repetitions',
+    wordSequences: 'Your password contains sequences',
+    errorList: 'Errors:',
+    veryWeak: 'Very Weak',
+    weak: 'Weak',
+    normal: 'Обычный',
+    medium: 'Medium',
+    strong: 'Strong',
+    veryStrong: 'Very Strong'
+};
