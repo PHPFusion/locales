8c33c5a0df80e8a1746018eee9c141bd6ccb982e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:30 +0800

    New translations downloads.php (Arabic)

--- Arabic/infusions/downloads/locale/Arabic/permalinks/downloads.php
+++ Arabic/infusions/downloads/locale/Arabic/permalinks/downloads.php
@@ -1,3 +1,3 @@
 <?php
 $locale['pl_downloads_title'] = "ملفات التحميل";
-$locale['pl_downloads_desc'] = "فعّل الروابط الثابتة للتحميلات على هذا الموقع.";
+$locale['pl_downloads_desc'] = "تفعيل الروابط الثابتة للتنزيلات على الموقع.";
