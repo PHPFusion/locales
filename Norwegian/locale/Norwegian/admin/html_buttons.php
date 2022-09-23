83f6b86a90a71eff5acd895e1213566ecb612548
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:40:29 +0800

    New translations html_buttons.php (Norwegian)

--- Norwegian/locale/Norwegian/admin/html_buttons.php
+++ Norwegian/locale/Norwegian/admin/html_buttons.php
@@ -12,7 +12,7 @@ $locale['html_008'] = "Juster tekst til høyre";
 $locale['html_009'] = "Blokkjuster";
 $locale['html_010'] = "Sett inn Link";
 $locale['html_011'] = "Sett inn Bilde";
-$locale['html_012'] = "Midtstill";
+$locale['html_012'] = "Center";
 $locale['html_013'] = "Liten";
 $locale['html_014'] = "Liten 2";
 $locale['html_015'] = "Alt";
@@ -23,7 +23,7 @@ $locale['html_019'] = "Overskrifter";
 $locale['html_020'] = "Line Break";
 
 $locale['texts'] = "Tekst";
-$locale['preview'] = "Forhåndsvisning";
+$locale['preview'] = "Preview";
 $locale['word_count'] = "Karakter antall";
 $locale['error_preview'] = "Forhåndsvis feil";
 $locale['error_preview_text'] = "Forhåndsvisningen kan midlertidig ikke genereres. Vennligst last inn siden på nytt og prøv igjen.";
