89f7e2ae2cfb82a980f158e5632d07e78f7a9375
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:06:03 +0800

    New translations weblinks.php (Slovak)

--- Slovak/infusions/weblinks/locale/Slovak/search/weblinks.php
+++ Slovak/infusions/weblinks/locale/Slovak/search/weblinks.php
@@ -2,7 +2,7 @@
 $locale['weblinks.php'] = "Odkazy";
 $locale['w400'] = "Odkazy";
 $locale['w401'] = "Odkaz";
-$locale['w402'] = "webových odkazov";
+$locale['w402'] = "Odkazy";
 $locale['w403'] = "[NOVÉ]";
 $locale['w404'] = "Pridané:";
 $locale['w405'] = "Prezreté:";
