afb26011a72c13eb18bbc16d775f1649731e25d8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:56:42 +0800

    New translations photos.php (Hungarian)

--- Hungarian/infusions/gallery/locale/Hungarian/search/photos.php
+++ Hungarian/infusions/gallery/locale/Hungarian/search/photos.php
@@ -1,9 +1,9 @@
 <?php
-$locale['photos.php'] = "Galéria";
+$locale['photos.php'] = "Képgaléria";
 $locale['p400'] = "Képek";
 $locale['p401'] = "Kép";
-$locale['p402'] = "Képet";
+$locale['p402'] = "Képek";
 $locale['p403'] = "[ÚJ]";
 $locale['p404'] = "Album: ";
 $locale['p405'] = "Dátum:";
-$locale['p406'] = "Megtekintve:";
+$locale['p406'] = "Megtekintve: ";
