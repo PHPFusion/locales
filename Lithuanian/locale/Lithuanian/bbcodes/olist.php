e8b73f332fcac277ef98c6be29fff58be3e1f39a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:00:18 +0800

    New translations olist.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/bbcodes/olist.php
+++ Lithuanian/locale/Lithuanian/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Sukuria numeruotą sąrašą";
 $locale['bb_olist_usage'] = "Tekstas 1 :enter: Tekstas 2 :enter: etc";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
