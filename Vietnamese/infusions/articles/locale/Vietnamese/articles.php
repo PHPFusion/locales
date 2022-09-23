8e2e136e8e1794b499bc087a937322f58c413b25
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:45:28 +0800

    New translations articles.php (Vietnamese)

---
+++ Vietnamese/infusions/articles/locale/Vietnamese/articles.php
@@ -0,0 +1,16 @@
+<?php
+$locale['article_0000'] = "Articles";
+$locale['article_0001'] = "Tất cả bài viết";
+$locale['article_0002'] = "Hiện danh mục";
+$locale['article_0003'] = "Các Danh mục";
+$locale['article_0004'] = "Cập nhật lần cuối";
+$locale['article_0005'] = "Danh mục:";
+$locale['article_0030'] = "Gần đây nhất";
+$locale['article_0031'] = "Được bình luận nhiều nhất";
+$locale['article_0032'] = "Highest Rating";
+$locale['article_0040'] = "Gallery view";
+$locale['article_0041'] = "List view";
+$locale['article_0060'] = "No Article Categories defined.";
+$locale['article_0061'] = "No Articles defined.";
+$locale['article_0062'] = "There are no Articles in this Category.";
+$locale['article_0063'] = "Most Read";
