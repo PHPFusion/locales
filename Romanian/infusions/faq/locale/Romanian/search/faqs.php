4c2088ada114680b15944167dad4542e88bd402d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:51:41 +0800

    New translations faqs.php (Romanian)

--- Romanian/infusions/faq/locale/Romanian/search/faqs.php
+++ Romanian/infusions/faq/locale/Romanian/search/faqs.php
@@ -1,7 +1,7 @@
 <?php
 $locale['faqs.php'] = "FAQ";
 $locale['fq400'] = "&#206;ntreb&#259;ri frecvente";
-$locale['fq401'] = "&#206;ntrebare frecvent&#259;";
+$locale['fq401'] = "FAQ";
 $locale['fq402'] = "&#206;ntreb&#259;ri frecvente";
 $locale['fq403'] = "g&#259;sit(e) &#238;n";
 $locale['fq404'] = "&#206;trebare";
