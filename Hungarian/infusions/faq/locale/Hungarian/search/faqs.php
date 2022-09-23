b91a3c84d24ea2b4fe3812785da2db7d9f24a222
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:51:14 +0800

    New translations faqs.php (Hungarian)

--- Hungarian/infusions/faq/locale/Hungarian/search/faqs.php
+++ Hungarian/infusions/faq/locale/Hungarian/search/faqs.php
@@ -2,7 +2,7 @@
 $locale['faqs.php'] = "GyIK";
 $locale['fq400'] = "GyIK";
 $locale['fq401'] = "GyIK";
-$locale['fq402'] = "GyIK-et";
-$locale['fq403'] = "találat a";
+$locale['fq402'] = "GyIK";
+$locale['fq403'] = "találtam a(z)";
 $locale['fq404'] = "GyIK kérdésében";
 $locale['fq405'] = "GyIK válaszában";
