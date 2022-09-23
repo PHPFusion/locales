98fc80592281107b5c346d37fe705a2ab71bcc3a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:49:00 +0800

    New translations downloads.php (Hungarian)

--- Hungarian/infusions/downloads/locale/Hungarian/search/downloads.php
+++ Hungarian/infusions/downloads/locale/Hungarian/search/downloads.php
@@ -2,10 +2,10 @@
 $locale['downloads.php'] = "Letöltések";
 $locale['d400'] = "Letöltések";
 $locale['d401'] = "Letöltés";
-$locale['d402'] = "Letöltést";
+$locale['d402'] = "Letöltések";
 $locale['d403'] = "[ÚJ]";
-$locale['d404'] = "Licenc:";
+$locale['d404'] = "Licensz:";
 $locale['d405'] = "O/S:";
 $locale['d406'] = "Verzió:";
-$locale['d407'] = "Dátum:";
+$locale['d407'] = "Hozzáadás dátuma:";
 $locale['d408'] = "Letöltve:";
