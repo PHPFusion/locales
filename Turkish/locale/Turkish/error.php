0391ad4d2f02c32342b0b17e294fb7bb948b7d79
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:04 +0800

    New translations error.php (Turkish)

--- Turkish/locale/Turkish/error.php
+++ Turkish/locale/Turkish/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Bu sayfaya yetkisiz erişim";
 $locale['err403'] = "Bu sayfaya erişim yasaklandı";
 $locale['err404'] = "Üzgünüz, bu sayfa bu sunucuda mevcut değil";
-$locale['err500'] = "Dahili sunucu hatası";
 $locale['errunk'] = "Bilinmeyen hata";
 $locale['errmsg'] = "İzlediğiniz bağlantının süresi dolmuş olabilir veya sayfaya sizin erişiminiz olmayabilir.";
 $locale['errret'] = "Ana siteye geri dön";
