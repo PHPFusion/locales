1895d2aeefe3e7ce754244b5d66262c36b1095dd
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:26:49 +0800

    New translations user_theme.php (Slovak)

--- Slovak/locale/Slovak/user_fields/user_theme.php
+++ Slovak/locale/Slovak/user_fields/user_theme.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_theme'] = "Vzhľad";
+$locale['uf_theme'] = "Téma";
 $locale['uf_theme_desc'] = "Povolí užívateľovi vybrať vlastný vzhľad";
 $locale['uf_theme_error'] = "Vzhľad nemôže byť prázdny.";
