6a2be058bedbb35254d9607d58a940d9e62a8e11
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:53:13 +0800

    New translations forums.php (Polish)

--- Polish/infusions/forum/locale/Polish/permalinks/forums.php
+++ Polish/infusions/forum/locale/Polish/permalinks/forums.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_forums_title'] = "Forum";
+$locale['pl_forums_title'] = "Fora";
 $locale['pl_forums_desc'] = "Aktywuje bezpośrednie odnośniki dla różnych linków na forum.";
 $locale['pl_tags_001'] = "Identyfikator forum dla konkretnego forum.";
 $locale['pl_tags_002'] = "Tytuł konkretnego forum.";
