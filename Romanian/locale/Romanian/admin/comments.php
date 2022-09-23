8b100b57706cd6975c2bc59c5358ead1942f1220
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:36:34 +0800

    New translations comments.php (Romanian)

--- Romanian/locale/Romanian/admin/comments.php
+++ Romanian/locale/Romanian/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Editare comentariu";
-$locale['401'] = "Comentarii";
+$locale['C_400'] = "Editare comentariu";
+$locale['C_401'] = "Comentarii";
 // Comments messages
-$locale['410'] = "Comentariu actualizat";
-$locale['411'] = "Comentariu &#351;ters";
+$locale['C_410'] = "Comentariu actualizat";
+$locale['C_411'] = "Comentariu &#351;ters";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
 // Edit Comment form
-$locale['420'] = "Dezactiveaz&#259; figurinele &#238;n acest comentariu";
-$locale['421'] = "Salveaz&#259; comentariul";
+$locale['C_420'] = "Dezactiveaz&#259; figurinele &#238;n acest comentariu";
+$locale['C_421'] = "Salveaz&#259; comentariul";
 // Comments
-$locale['431'] = "&#350;terge";
-$locale['432'] = "IP utilizator:";
-$locale['433'] = "Sterg acest comentariu?";
-$locale['434'] = "Nu exist&#259; comentarii postate.";
-$locale['435'] = "Do you really want to add this user to a blacklist?";
-$locale['436'] = "Your comment are blacklisted.";
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "IP utilizator:";
+$locale['C_433'] = "Sterg acest comentariu?";
+$locale['C_434'] = "Nu exist&#259; comentarii postate.";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Activez sistemul de comentarii?";
+$locale['C_441'] = "Permi&#355;i vizitatorilor s&#259; posteze?";
+$locale['C_442'] = "Avatare &#238;n comentarii?";
+$locale['C_443'] = "Comentarii pe pagin&#259;";
+$locale['C_444'] = "Sortare comentarii";
+$locale['C_445'] = "Cresc&#259;tor";
+$locale['C_446'] = "Descresc&#259;tor";
