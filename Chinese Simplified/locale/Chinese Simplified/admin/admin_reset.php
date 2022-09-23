4585eaee89a1f4ea20f189ce2ed1ce0323112cd6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:35:21 +0800

    New translations admin_reset.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/admin/admin_reset.php
@@ -0,0 +1,36 @@
+<?php
+$locale['apw_title'] = "管理员密码重置";
+$locale['apw_desc'] = "更改管理员登录/管理员密码";
+$locale['apw_admin1'] = "管理员重置";
+$locale['apw_400'] = "管理员重置";
+$locale['apw_401'] = "所有管理员";
+$locale['apw_402'] = "只有超级管理员";
+$locale['apw_403'] = "所有管理员";
+$locale['apw_404'] = "复位的理由";
+$locale['apw_405'] = "重置登录和管理员密码";
+$locale['apw_406'] = "重置密码";
+$locale['apw_407'] = "密码重置在 ";
+$locale['apw_408'] = "您好[USER_NAME]，您的管理员密码已在[SITEURL]重置：<br/>
+管理员密码：[NEW_ADMIN_PASS] <br/>重置的原因是：[RESET_MESSAGE] <br/>谢谢，<br/> [管理员]";
+$locale['apw_409'] = "您好[USER_NAME]，您的登录名和管理员密码已在[SITEURL]重置：<br/>
+登录密码：[NEW_PASS] <br/>管理员密码：[NEW_ADMIN_PASS] <br/>重置的原因是：[RESET_MESSAGE] <br/>
+谢谢，<br/>[ADMIN]";
+$locale['apw_410'] = "重置结果";
+$locale['apw_411'] = "更新管理员密码。";
+$locale['apw_412'] = "Admin Password:";
+$locale['apw_415'] = "重置日志";
+$locale['apw_416'] = "日志 ID";
+$locale['apw_417'] = "重置时间";
+$locale['apw_418'] = "负责";
+$locale['apw_419'] = "重 置";
+$locale['apw_420'] = "成功";
+$locale['apw_421'] = "Reason";
+$locale['apw_422'] = "的";
+$locale['apw_423'] = "没有说明理由。";
+$locale['apw_424'] = "管理员重置";
+$locale['apw_425'] = "管理员失败";
+$locale['apw_426'] = "没有密码被修改";
+$locale['apw_427'] = "设置";
+$locale['apw_428'] = "是否确实要删除此日志？";
+$locale['apw_429'] = "日志已被删除";
+$locale['apw_430'] = "全部选择";
