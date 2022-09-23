e66a1644d9c79c448fe6a4a77bd8504192a2015d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:11:28 +0800

    New translations user_blacklist.php (Polish)

--- Polish/locale/Polish/user_fields/user_blacklist.php
+++ Polish/locale/Polish/user_fields/user_blacklist.php
@@ -3,12 +3,3 @@ $locale['uf_blacklist'] = "Czarna lista";
 $locale['uf_blacklist_desc'] = "Czarna lista użytkowników";
 $locale['uf_blacklist_error'] = "Błąd w rodzaju danych";
 $locale['uf_blacklist_message'] = "Gdy kogoś zablokujesz, ta osoba ta nie będzie Cię więcej widziała, włączając w to Twoje posty oraz kontakt z Tobą.";
-
-$locale['uf_blacklist_000'] = "Zablokowani i ignorowani użytkownicy";
-$locale['uf_blacklist_001'] = "Odblokuj";
-$locale['uf_blacklist_002'] = "Ignoruj";
-$locale['uf_blacklist_003'] = "Brak użytkowników na czarnej liście.";
-$locale['uf_blacklist_004'] = "Użytkownik %s został usunięty z czarnej listy.";
-$locale['uf_blacklist_005'] = "Użytkownik nie został umieszczony na czarnej liście.";
-$locale['uf_blacklist_006'] = "Nie znaleziono użytkownika.";
-$locale['uf_blacklist_007'] = "Nie dostępne.";
