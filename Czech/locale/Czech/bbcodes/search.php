9cf943e436562317d7b7db46dd4ea3f6c826f1fd
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:01:47 +0800

    New translations search.php (Czech)

--- Czech/locale/Czech/bbcodes/search.php
+++ Czech/locale/Czech/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Vyhledání daného textu na aktuální stránce";
 $locale['bb_search_usage'] = "Text, který chcete vyhledávat";
 $locale['bb_search_prefix'] = "Hledat ";
-$locale['bb_search_suffix'] = " na ";
+$locale['bb_search_suffix'] = " dne ";
 $locale['bb_search_where'] = "Kde";
