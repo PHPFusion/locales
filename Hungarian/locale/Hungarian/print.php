3c5bd0e9992884d230079a3e1b08d2b1a1b6ab6a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:38:54 +0800

    New translations print.php (Hungarian)

--- Hungarian/locale/Hungarian/print.php
+++ Hungarian/locale/Hungarian/print.php
@@ -1,16 +1,16 @@
 <?php
 //news, articles, blog
-$locale['400'] = "Írta: ";
+$locale['400'] = "Beküldte ";
 $locale['401'] = " - Dátum: ";
 $locale['402'] = "Teljes hír";
-$locale['403'] = "Teljes Blog";
+$locale['403'] = "Teljes blog";
 //forums
 $locale['500'] = "Fórumtéma:";
-$locale['501'] = "Írta: ";
+$locale['501'] = "Beküldte ";
 $locale['502'] = " - Dátum: ";
 $locale['503'] = "Szerkesztette: ";
 //License Agreement
-$locale['600'] = "Szabályzat";
+$locale['600'] = "Felhasználási feltételek";
 $locale['601'] = "Legutolsó módosítás: ";
 //Privacy Policy
-$locale['700'] = "Adatvédelmi irányelvek";
+$locale['700'] = "Adatvédelmi nyilatkozat";
