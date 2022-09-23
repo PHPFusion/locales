87726b9917a3d8772281ab8d89aed4de942ed07c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:18:49 +0800

    New translations user_location.php (Hungarian)

--- Hungarian/locale/Hungarian/user_fields/user_location.php
+++ Hungarian/locale/Hungarian/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Lakóhely";
+$locale['uf_location'] = "Helyszín";
 $locale['uf_location_desc'] = "Ország, vagy város";
 $locale['uf_location_error'] = "Meg kell adnod a lakóhelyedet.";
