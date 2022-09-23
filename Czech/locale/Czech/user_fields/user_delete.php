5f14ef60a78ffaaa00702dc171f56e1dd63d395a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:13:03 +0800

    New translations user_delete.php (Czech)

--- Czech/locale/Czech/user_fields/user_delete.php
+++ Czech/locale/Czech/user_fields/user_delete.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_delete'] = "Odstranit uživatele";
-$locale['uf_delete_desc'] = "Odstraní uživatele";
+$locale['uf_delete'] = "Smazat uživatele";
+$locale['uf_delete_desc'] = "Smazat uživatele";
 $locale['uf_delete_del'] = "Vymaž ma";
 $locale['uf_delete_exit'] = "Účet byl smazán...";
