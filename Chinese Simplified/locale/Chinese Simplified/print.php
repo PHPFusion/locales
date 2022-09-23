826fca6e8f0fe86b4700a3189284ce55373dc23d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:39:41 +0800

    New translations print.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/print.php
@@ -0,0 +1,16 @@
+<?php
+//news, articles, blog
+$locale['400'] = "发表于";
+$locale['401'] = " 在 ";
+$locale['402'] = "Extended News";
+$locale['403'] = "Extended Blog";
+//forums
+$locale['500'] = "主题:";
+$locale['501'] = "发表于";
+$locale['502'] = " 在 ";
+$locale['503'] = "编辑者";
+//License Agreement
+$locale['600'] = "协议条款";
+$locale['601'] = "最后更新日期";
+//Privacy Policy
+$locale['700'] = "隐私政策";
