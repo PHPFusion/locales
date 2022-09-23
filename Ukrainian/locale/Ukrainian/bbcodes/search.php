f23ec4776cb82a67818e9db023dfbe93dfe03a2f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:01:30 +0800

    New translations search.php (Ukrainian)

--- Ukrainian/locale/Ukrainian/bbcodes/search.php
+++ Ukrainian/locale/Ukrainian/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Вбудувати пошук на сайті";
 $locale['bb_search_usage'] = "Шукати текст";
 $locale['bb_search_prefix'] = "Шукати на сайті [";
-$locale['bb_search_suffix'] = "] в розділі: ";
+$locale['bb_search_suffix'] = " ";
 $locale['bb_search_where'] = "місце пошуку";
