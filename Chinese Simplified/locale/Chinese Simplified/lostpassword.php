537f69513a45fce4151eed566158ce0b7928f61d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:37:11 +0800

    New translations lostpassword.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/lostpassword.php
@@ -0,0 +1,29 @@
+<?php
+$locale['400'] = "Lost Password";
+$locale['401'] = "A confirmation email has been sent to your email address.";
+$locale['402'] = "Your new password has been sent to your email address.";
+$locale['403'] = "Return to Main Site";
+$locale['404'] = "The email address you specified could not be found.";
+$locale['405'] = "The email address you specified is invalid.";
+$locale['406'] = "Please try again.";
+$locale['407'] = "Please enter the email address your account is registered with below.
+A new password will be created and sent to this address.";
+$locale['408'] = "Send Password";
+$locale['409'] = "New password for ";
+$locale['410'] = "Hello [USER_NAME],
+You have or someone has requested a new password to access your [SITENAME] account.
+
+To change your password please click the following link:
+[NEW_PASS_LINK]
+
+Regards,
+[SITEUSERNAME]";
+$locale['411'] = "Hello [USER_NAME],
+Your new password to access your [SITENAME] account is:
+
+[NEW_PASS]
+
+Regards,
+[SITEUSERNAME]";
+$locale['412'] = "There was an error, please try again.";
+$locale['413'] = "Please enter your email address:";
