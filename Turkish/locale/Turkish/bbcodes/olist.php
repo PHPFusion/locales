1b930f6c04a9e32c6aeb3e0436c27e04223b94e5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:00:28 +0800

    New translations olist.php (Turkish)

--- Turkish/locale/Turkish/bbcodes/olist.php
+++ Turkish/locale/Turkish/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Seçili Türde Sıralı Liste Oluşturur";
 $locale['bb_olist_usage'] = "Metin 1 :enter: Metin 2 :enter: vs.";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
