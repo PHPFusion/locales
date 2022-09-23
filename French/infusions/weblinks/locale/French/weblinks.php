d2ddb1300d3209135029eb62ef5634b33d6d76bb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:05:32 +0800

    New translations weblinks.php (French)

--- French/infusions/weblinks/locale/French/weblinks.php
+++ French/infusions/weblinks/locale/French/weblinks.php
@@ -4,9 +4,9 @@ $locale['web_0000'] = "Liens Internet";
 $locale['web_0001'] = "Tous les liens web";
 $locale['fmt_open'] = "ouvrir|ouvrir";
 // Sorting
-$locale['web_0030'] = "Le plus récent";
+$locale['web_0030'] = "Le(s) plus récent(s)";
 $locale['web_0031'] = "Top Ouvert";
-$locale['web_0032'] = "Le plus ancien";
+$locale['web_0032'] = "Ancien";
 // Views
 $locale['web_0040'] = "Vue miniature";
 $locale['web_0041'] = "Affichage Liste";
