4ab2adbefb32479337527d70305190cc861ac57b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:32 +0800

    New translations error.php (Czech)

--- Czech/locale/Czech/error.php
+++ Czech/locale/Czech/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Neoprávněný přístup na tuto stránku";
 $locale['err403'] = "Přístup na tuto stránku je zakázán";
 $locale['err404'] = "Je nám líto, ale tato stránka není k dispozici na tomto serveru";
-$locale['err500'] = "Interní chyba serveru";
 $locale['errunk'] = "Neznámá chyba";
 $locale['errmsg'] = "Odkaz, který jste obdrželi, možná vypršel, nebo stránka není přístupná.";
 $locale['errret'] = "Návrat na hlavní stránku";
