a0bf7daedb6535ab0702e002532c5bc85e840353
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:01 +0800

    New translations error.php (Slovak)

--- Slovak/locale/Slovak/error.php
+++ Slovak/locale/Slovak/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Neoprávnený prístup na túto stránku";
 $locale['err403'] = "Prístup na túto stránku je zakázaný";
 $locale['err404'] = "Ospravedlňujeme sa, ale táto stránka nie je dostupná na tomto serveri";
-$locale['err500'] = "Interná chyba servera";
 $locale['errunk'] = "Neznáma chyba";
 $locale['errmsg'] = "Odkaz, ktorý ste obdržali, možno vypršal, alebo stránka nie je prístupná.";
 $locale['errret'] = "Návrat na hlavnú stránku";
