b9a7a3d3ba6ad7117f060e53dce91b4b337bd00b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:23 +0800

    New translations defender.php (Malay)

--- Malay/locale/Malay/defender.php
+++ Malay/locale/Malay/defender.php
@@ -3,7 +3,7 @@ $locale['validate'] = "Sila meyemak dan mengesahkan isi borang.";
 $locale['error_request'] = "Kesalahan didapati semasa memproseskan permintaan anda";
 $locale['regex_error'] = "Sila meyemak dan mengesahkan isi borang - %s";
 // Text fields errors
-$locale['error_input_username'] = "Please enter a valid username";
+$locale['error_input_username'] = "Sila masukkan nama pengguna yang sah";
 $locale['error_input_default'] = "Sila isikan isi borang ini";
 $locale['error_input_password'] = "Sila isikan kata laluan yang sah";
 $locale['error_input_email'] = "Silah isikan  alamat emel yang sah";
@@ -16,16 +16,16 @@ $locale['df_error_text'] = "Sila betulkan kesilapan di %s dan memerlukan permerh
 $locale['error_invalid_theme'] = "Sila pilih tema situs yang sah";
 $locale['error_input_checkbox'] = "Silah tandakan kotak semak ini";
 $locale['error_input_file'] = "Sila pilih fail yang sah";
-$locale['error_secure_file'] = "The uploaded file has been rejected due to possibility of malicious payload.";
-$locale['error_input_login'] = "Incorrect username or password";
+$locale['error_secure_file'] = "Fail yang dimuatnaikkan telah ditolak sebab kemungkinan muatan jahat.";
+$locale['error_input_login'] = "Nama pengguna atau kata laluan salah";
 // Admin Login
 $locale['cookie_title'] = "Sesi Tamat Masa";
 $locale['cookie_description'] = "Tiada pengesahan sah / atau sesi anda mungkin telah tamat masa. Sila log masuk sekali lagi.";
 $locale['cookie_expired'] = "Cookie laman admin telah tamat. Sila log masuk sekali lagi.";
 $locale['password_invalid'] = "Kata laluan Salah atau Tidak Sah";
 $locale['password_invalid_description'] = "Kata Laluan tidak betul. Sila cuba sekali lagi.";
-$locale['prefix_error'] = "Contact prefix requires attention.";
-$locale['contact_error'] = "Contact number requires attention.";
+$locale['prefix_error'] = "Nombor awalan hubungan memerlukan perhatian.";
+$locale['contact_error'] = "Nombor hubungan memerlukan perhatian.";
 $locale['cookie_error'] = "Kesalahan Cookie";
 $locale['cookie_error_description'] = "Anda perlu untuk mengguna Cookie pelayar log masuk panel admin.";
 $locale['validate_title'] = "Bolehkah anda menyemak sesuatu..";
@@ -57,14 +57,14 @@ $locale['token_error_5'] = "Tarikh dan Masa Token tidak Sah.";
 $locale['token_error_6'] = "Post terlampau cepat.";
 $locale['token_error_7'] = "Hash Token tidak Sah.";
 $locale['token_error_8'] = "Format Token tidak Sah.";
-$locale['token_error_9'] = "Cannot find any token for this form.";
-$locale['token_error_10'] = "Cannot find any token for storage: ";
+$locale['token_error_9'] = "Token untuk borang tidak dapat dikesan.";
+$locale['token_error_10'] = "Token simpanan tidak dapat dikesan: ";
 
-$locale['df_300'] = "Click to add files";
-$locale['df_301'] = "Click to add photo";
-$locale['df_302'] = "Click to add video";
-$locale['df_303'] = "Click to add audio";
-$locale['df_304'] = "Selected file Remove";
+$locale['df_300'] = "Klik untuk tambah fail";
+$locale['df_301'] = "Klik untuk tambah gambar";
+$locale['df_302'] = "Klik untuk tambah video";
+$locale['df_303'] = "Klik untuk tambah audio";
+$locale['df_304'] = "Padam fail pilihan";
 
 $locale['df_400'] = "%s mempunyai kata huruf yang tidak dibenarkan.";
 $locale['df_401'] = "%s bukan alamat emel yang sah.";
@@ -72,7 +72,7 @@ $locale['df_402'] = "%s bukan kata laluan yang sah.";
 $locale['df_403'] = "%s bukan nombor yang sah.";
 $locale['df_404'] = "%s bukan tarikh yang sah.";
 $locale['df_405'] = "%s bukan alamat URL yang sah.";
-$locale["df_406"] = "%s has an unknown type set.";
+$locale["df_406"] = "Jenis %s tidak dapat dikenali.";
 $locale['df_415'] = "Nama fail tidak Sah.";
 $locale['df_416'] = "Saiz muatan naik melampaui saiz dibenarkan. Sila cuba dengan saiz fail yang kurang daripada %s.";
 $locale['df_417'] = "Jenis fail muatan naik tidak dibenarkan. Imej yang dibenarkan adalah jenis extension - %s";
@@ -84,8 +84,15 @@ $locale['df_422'] = "Kasalahan Query";
 $locale['df_423'] = "Imej tidak dapat dimuat naikkan oleh Server dengan betul. Sila cuba sekali lagi.";
 $locale['df_424'] = "Muatan naik ini melampaui batasan yang dibenarkan. Sila cuba sekali lagi dan kurangkan fail yang dipilih.";
 
-// ReCaptcha2
+$locale['error_rate'] = "Sila isikan Penilaian.";
+$locale['error_type'] = "Sila isikan Jenis Extension";
+$locale['error_width'] = "Sila isikan Lebar";
+$locale['error_height'] = "Sila isikan Tinggi";
+$locale['error_value'] = "Sila isikan nilai yang berkenaan";
+
+// ReCaptcha
 $locale['grecaptcha'] = "Google Recaptcha";
+$locale['grecaptcha3'] = "Google Recaptcha v3";
 $locale['grecaptcha_desc'] = "Modul Isi Borang Google Recaptcha";
 $locale['grecaptcha_placeholder_1'] = "SITE KEY daripada Google";
 $locale['grecaptcha_placeholder_2'] = "SECRET-KEY daripada Google";
@@ -98,4 +105,5 @@ $locale['grecaptcha_0102b'] = "Gelap";
 $locale['grecaptcha_0103'] = "Jenis Pengesahan";
 $locale['grecaptcha_0103a'] = "Teks";
 $locale['grecaptcha_0103b'] = "Audio";
+$locale['grecaptcha_0104'] = "Skor";
 $locale['grecaptcha_warn'] = "Sila isikan kekunci awam dan kekunci swasta reCAPTCHA";
