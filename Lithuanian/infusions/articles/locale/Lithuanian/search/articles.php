388d3dbb0290e3c1752e637c9cf2ac24816cd0d5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:44:11 +0800

    New translations articles.php (Lithuanian)

--- Lithuanian/infusions/articles/locale/Lithuanian/search/articles.php
+++ Lithuanian/infusions/articles/locale/Lithuanian/search/articles.php
@@ -3,6 +3,6 @@ $locale['articles.php'] = "Straipsniai";
 $locale['a400'] = "Straipsniai";
 $locale['a401'] = "Straipsnis";
 $locale['a402'] = "Straipsniai";
-$locale['a403'] = "rasta ";
+$locale['a403'] = "rasta";
 $locale['a404'] = "straipsnio antraštė";
 $locale['a405'] = "straipsnio tekstas";
