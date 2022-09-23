50b4abce1493284926dadcb2cedabf04b71388c8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:53:50 +0800

    New translations forum.php (Dutch)

--- Dutch/infusions/forum/locale/Dutch/search/forum.php
+++ Dutch/infusions/forum/locale/Dutch/search/forum.php
@@ -1,9 +1,9 @@
 <?php
 $locale['forum.php'] = "Forum Beheer";
-$locale['f400'] = "Forum Posts";
+$locale['f400'] = "Forum berichten";
 $locale['f401'] = "alle forums";
 $locale['f402'] = "Forum berichten";
-$locale['f403'] = "Forum Posts";
+$locale['f403'] = "Forum berichten";
 $locale['f404'] = "Sticky:";
 $locale['f405'] = "Aankondiging:";
 $locale['f406'] = "gevonden in";
