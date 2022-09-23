1c2737e90e7a56a4d867448252c7c8b0a1b31c9c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:24 +0800

    New translations blog_cats.php (Slovak)

--- Slovak/infusions/blog/locale/Slovak/blog_cats.php
+++ Slovak/infusions/blog/locale/Slovak/blog_cats.php
@@ -6,4 +6,4 @@ $locale['403'] = "Nekategorizované blogy";
 $locale['404'] = "V tejto kategórii nie sú žiadne blogy.";
 $locale['405'] = "Viac";
 $locale['406'] = "Zobraziť všetky kategórie";
-$locale['407'] = "Nie sú pridané žiadne kategórie blogov.";
+$locale['407'] = "Nie sú pridané žiadne kategórie blogov";
