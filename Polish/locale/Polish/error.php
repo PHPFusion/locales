9d426acdd0881148c0c5ee368a1a5508c332bea6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:33:58 +0800

    New translations error.php (Polish)

--- Polish/locale/Polish/error.php
+++ Polish/locale/Polish/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Nieautoryzowany dostęp do tej strony";
 $locale['err403'] = "Dostęp do tej strony zabroniony";
 $locale['err404'] = "Przepraszamy, ta strona nie jest dostępna na tym serwerze";
-$locale['err500'] = "Wewnętrzny błąd serwera";
 $locale['errunk'] = "Nieznany błąd";
 $locale['errmsg'] = "Link, którym podążałeś, mógł wygasnąć lub możesz nie mieć dostępu do strony.";
 $locale['errret'] = "Powrót do strony głównej";
