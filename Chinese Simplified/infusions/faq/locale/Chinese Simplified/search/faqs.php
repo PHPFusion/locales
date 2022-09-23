d19c6a66ef87e25e9955976d1ed1e1112be2003b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:51:56 +0800

    New translations faqs.php (Chinese Simplified)

---
+++ Chinese Simplified/infusions/faq/locale/Chinese Simplified/search/faqs.php
@@ -0,0 +1,8 @@
+<?php
+$locale['faqs.php'] = "常见问答（FAQ）";
+$locale['fq400'] = "FAQs";
+$locale['fq401'] = "常见问答（FAQ）";
+$locale['fq402'] = "FAQs";
+$locale['fq403'] = "found in";
+$locale['fq404'] = "FAQ question";
+$locale['fq405'] = "FAQ answer";
