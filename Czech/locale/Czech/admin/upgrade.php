097c325dd817d7887a333380e8a3b4098639b3b1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:51:10 +0800

    New translations upgrade.php (Czech)

--- Czech/locale/Czech/admin/upgrade.php
+++ Czech/locale/Czech/admin/upgrade.php
@@ -1,3 +1,21 @@
 <?php
-$locale['U_0000'] = "Aktualizace";
-$locale['U_0001'] = "Žádné aktualizace nejsou k dispozici";
+$locale['U_000'] = "Aktualizace";
+$locale['U_001'] = "Před aktualizací prosím zálohujte databázi a soubory.";
+$locale['U_002'] = "Naposledy zkontrolováno %s.";
+$locale['U_003'] = "Znovu zkontrolovat";
+$locale['U_004'] = "Nová aktualizace je k dispozici";
+$locale['U_005'] = "Nepodařilo se zkontrolovat aktualizace!";
+$locale['U_006'] = "Nejsou dostupné žádné aktualizace";
+$locale['U_007'] = "Můžete automaticky aktualizovat na PHPFusion %s.";
+$locale['U_008'] = "Máte nejnovější verzi";
+$locale['U_009'] = "Stahování %s.";
+$locale['U_010'] = "Rozbalování aktualizace.";
+$locale['U_011'] = "Kopírování souborů.";
+$locale['U_012'] = "Aktualizace databáze.";
+$locale['U_013'] = "Aktualizace překladů.";
+$locale['U_014'] = "PHPFusion byl úspěšně aktualizován.";
+$locale['U_015'] = "Aktualizace nemohla být nainstalována. Zkuste to prosím později nebo si ji stáhněte ručně.";
+$locale['U_016'] = "Aktualizovat překlady";
+$locale['U_017'] = "Překlady byly úspěšně aktualizovány.";
+$locale['U_018'] = "Stahování jazykového balíčku %s."; // %s will be replaced with language name
+$locale['U_019'] = "Aktuální verze: %s";
