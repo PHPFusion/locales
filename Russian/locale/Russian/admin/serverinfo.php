b9257c4ddd02040e51c595c95dc6b790c6936f95
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:24 +0800

    New translations serverinfo.php (Russian)

---
+++ Russian/locale/Russian/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Информация сервера";
+//General
+$locale['SRV_401'] = "Общие";
+$locale['SRV_402'] = "Серверное ПО";
+$locale['SRV_403'] = "Программное обеспечение веб-сервера";
+$locale['SRV_404'] = "Версия PHP";
+$locale['SRV_405'] = "Интерфейс PHP";
+$locale['SRV_406'] = "Версия сервера MySQL";
+$locale['SRV_406a'] = "Драйвер БД";
+$locale['SRV_407'] = "Версия PHPFusion";
+$locale['SRV_408'] = "Префикс БД";
+$locale['SRV_409'] = "Префикс Cookie";
+$locale['SRV_410'] = "Веб-браузер и ОС";
+$locale['SRV_411'] = "Авторы перевода: Rizado, ovasclub"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Настройки PHP";
+$locale['SRV_423'] = "Безопасный режим";
+$locale['SRV_424'] = "Регистрация глобальных переменных";
+$locale['SRV_425'] = "Поддержка";
+$locale['SRV_426'] = "Загрузка файлов (максимальный размер)";
+$locale['SRV_428'] = "Отображать ошибки";
+$locale['SRV_429'] = "Отключенные функции";
+$locale['SRV_430'] = "нет данных";
+$locale['SRV_431'] = "версия";
+//Write permission
+$locale['SRV_440'] = "Разрешения на папки";
+$locale['SRV_441'] = "Доступен для записи";
+$locale['SRV_442'] = "Недоступен для записи";
+//PHPinfo
+$locale['SRV_450'] = "Подробности";
+$locale['SRV_451'] = "Невозможно получить данные. Проверьте, не отключена ли функция phpinfo().";
