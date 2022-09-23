12e4fec323bd5fbafb7b67f1a15b9302f5ac89a4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:48 +0800

    New translations comments.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/admin/comments.php
@@ -0,0 +1,26 @@
+<?php
+// Comments titles
+$locale['C_400'] = "编辑评论";
+$locale['C_401'] = "Comments";
+// Comments messages
+$locale['C_410'] = "评论已更新";
+$locale['C_411'] = "评论已删除";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
+// Edit Comment form
+$locale['C_420'] = "在这个评论中禁用微笑";
+$locale['C_421'] = "保存评论";
+// Comments
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "用户 IP：";
+$locale['C_433'] = "删除此评论？";
+$locale['C_434'] = "There are no comments for this item.";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Enable comment system?";
+$locale['C_441'] = "Allow Guests to post?";
+$locale['C_442'] = "Enable avatar comments?";
+$locale['C_443'] = "Comments per page";
+$locale['C_444'] = "Comments Sorting";
+$locale['C_445'] = "Ascending";
+$locale['C_446'] = "Descending";
