bb6d87d32aaf1631fbc922a3e724b5393b7bf5c3
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:38 +0800

    New translations comments.php (Dutch)

--- Dutch/locale/Dutch/admin/comments.php
+++ Dutch/locale/Dutch/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Reactie wijzigen";
-$locale['401'] = "Reacties";
+$locale['C_400'] = "Reactie wijzigen";
+$locale['C_401'] = "Reacties";
 // Comments messages
-$locale['410'] = "Reactie gewijzigd";
-$locale['411'] = "Reactie verwijderd";
+$locale['C_410'] = "Reactie gewijzigd";
+$locale['C_411'] = "Reactie verwijderd";
+$locale['C_412'] = "De reactie is verwijderd en de gebruiker is toegevoegd aan de zwarte lijst.";
 // Edit Comment form
-$locale['420'] = "Smileys in deze reactie uitschakelen";
-$locale['421'] = "Reactie opslaan";
+$locale['C_420'] = "Smileys in deze reactie uitschakelen";
+$locale['C_421'] = "Reactie opslaan";
 // Comments
-$locale['431'] = "Verwijderen en blokkeren";
-$locale['432'] = "IP van gebruiker:";
-$locale['433'] = "Deze reactie verwijderen?";
-$locale['434'] = "Er zijn nog geen reacties op dit item.";
-$locale['435'] = "Wilt u deze gebruiker echt toevoegen aan de zwarte lijst?";
-$locale['436'] = "Uw reactie is op de zwarte lijst geplaatst.";
+$locale['C_431'] = "Verwijderen en blokkeren";
+$locale['C_432'] = "IP van gebruiker:";
+$locale['C_433'] = "Deze reactie verwijderen?";
+$locale['C_434'] = "Er zijn nog geen reacties op dit item.";
+$locale['C_435'] = "Wilt u deze gebruiker echt toevoegen aan de zwarte lijst?";
+$locale['C_436'] = "Opmerking op geblokkeerd.";
+// Settings
+$locale['C_440'] = "Reacties toestaan?";
+$locale['C_441'] = "Gasten toestaan berichten te plaatsen?";
+$locale['C_442'] = "Avatar commentaren aanzetten?";
+$locale['C_443'] = "Reacties per pagina";
+$locale['C_444'] = "Reacties sorteren";
+$locale['C_445'] = "Oplopend";
+$locale['C_446'] = "Aflopend";
