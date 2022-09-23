dc8de9a9f39942c17e93ec81d0b24ca3425574f2
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:11 +0800

    New translations blacklist.php (Turkish)

--- Turkish/locale/Turkish/admin/blacklist.php
+++ Turkish/locale/Turkish/admin/blacklist.php
@@ -1,29 +1,29 @@
 <?php
-$locale['BLS_000'] = "Kara Liste"; //406
+$locale['BLS_000'] = "Kara Liste";
 //Blacklist message
 $locale['BLS_010'] = "Geçersiz E-Posta Veya IP Adresi.";
 $locale['BLS_011'] = "Kara Listeye Kullanıcılar Ekleme.";
 $locale['BLS_012'] = "Değiştirilmiş Kullanıcı Kara Listesi.";
-$locale['BLS_013'] = "Kullanıcı Kara Listeden Silindi"; //401
+$locale['BLS_013'] = "Kullanıcı Kara Listeden Silindi";
 $locale['BLS_014'] = "Bu Girişi Silmek İstediğinizden Emin Misiniz?";
-$locale['BLS_015'] = "Kara Liste Şu An Boş."; //465
-$locale['BLS_016'] = "Kara Liste E-Posta Adresi Geçerli Bir E-Posta Değil."; //405
+$locale['BLS_015'] = "Kara Liste Şu An Boş.";
+$locale['BLS_016'] = "Kara Liste E-Posta Adresi Geçerli Bir E-Posta Değil.";
 
-$locale['BLS_020'] = "Kara Liste Kullanıcısı"; //420
-$locale['BLS_021'] = "Kara Listeye Alınmış Kullanıcıyı Düzenle"; //421
+$locale['BLS_020'] = "Kara Liste Kullanıcısı";
+$locale['BLS_021'] = "Kara Listeye Alınmış Kullanıcıyı Düzenle";
 $locale['BLS_022'] = "Kara Listeye Kullanıcı Ekle";
 $locale['BLS_023'] = "Şu Anda %d - %d Toplam Kara Liste Girişleri Görüntüleniyor.";
 
-$locale['BLS_030'] = "Kara Liste Bilgisi"; //461
-$locale['BLS_031'] = "Yönetici"; //467
-$locale['BLS_032'] = "Tarih";  //468
-$locale['BLS_033'] = "Seçenekler"; //462
-$locale['BLS_034'] = "Kara Liste IP Adresi: [STRONG]or[/STRONG]"; //441
-$locale['BLS_035'] = "Kara Liste E-Posta Adresi"; //442
-$locale['BLS_036'] = "Kara Listeye Neden Olan Sebep"; //443
-$locale['BLS_037'] = "Kara Liste Kullanıcısı";  //444
-$locale['BLS_038'] = "Güncelle";
-$locale['BLS_039'] = "Tümünü Seç";
+$locale['BLS_030'] = "Kara Liste Bilgisi";
+$locale['BLS_031'] = "Yönetici";
+$locale['BLS_032'] = "Tarih";
+$locale['BLS_033'] = "Seçenekler";
+$locale['BLS_034'] = "Kara Liste IP Adresi: [STRONG]or[/STRONG]";
+$locale['BLS_035'] = "Kara Liste E-Posta Adresi";
+$locale['BLS_036'] = "Kara Listeye Neden Olan Sebep";
+$locale['BLS_037'] = "Kara Liste Kullanıcısı";
+$locale['BLS_038'] = "Güncelleştir";
+$locale['BLS_039'] = "Tümünü seç";
 
 $locale['BLS_MS'] = "IP Adresi Girildiğinde, IP Adresi Aynı Olan Kullanıcının Bu Siteyi Ziyaret Etmesi Engellenecektir.
 Tam bir IP Girebilirsiniz, ör., <em>123.45.67.89.</em>, veya kısmi bir IP, ör. <em>123.45.67</em> veya <em>123.45</em>.
@@ -41,4 +41,4 @@ Tam Bir E-Posta Adresi Girebilirsiniz, ör. <em>foo@bar.com</em>, Veya Bir E-Pos
 %@domain.tld Adresinden Herhangi Bir Adresi Yasaklar domain.tld<br />
 %.domain.tld Tüm Alt Alanlarını Yasaklar domain.tld<br />
 %payday% Sitelerde Çok Sık Bulunan \"payday\" Kelimesini İçeren Herhangi Bir Adresi Yasaklar.<br />
-domain.tld v7'de Tanımlanan Kurallarla Uyumlu Hale Getirmek Için %@domain.tld'nin Bir Takma Adıdır.<br />"; //440
+domain.tld v7'de Tanımlanan Kurallarla Uyumlu Hale Getirmek Için %@domain.tld'nin Bir Takma Adıdır.<br />";
