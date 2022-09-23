10d973ca303f9f885e6d86238375252c55b6c5de
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:55:26 +0800

    New translations color.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/bbcodes/color.php
+++ Lithuanian/locale/Lithuanian/bbcodes/color.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_color_description'] = "Nuspalvina pasirinktą tekstą";
-$locale['bb_color_usage'] = "Nuspalvinti pasirinktą tekstą";
+$locale['bb_color_usage'] = "Nuspalvina pasirinktą tekstą";
 $locale['bb_color_hex'] = "HEX spalvos kodas su #";
