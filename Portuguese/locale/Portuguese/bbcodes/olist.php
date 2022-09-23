1fcfd5900836b867f50143640d4e0c10d1ddf182
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:00:23 +0800

    New translations olist.php (Portuguese)

--- Portuguese/locale/Portuguese/bbcodes/olist.php
+++ Portuguese/locale/Portuguese/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Cria uma lista ordenada para o tipo especificado";
 $locale['bb_olist_usage'] = "Texto 1 :enter: Texto 2 :enter: etc";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
