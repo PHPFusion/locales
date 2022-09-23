44133dab3d61e07fce4b0ac3d0a8ad229a801cf5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:21 +0800

    New translations blog_cats.php (Norwegian)

--- Norwegian/infusions/blog/locale/Norwegian/blog_cats.php
+++ Norwegian/infusions/blog/locale/Norwegian/blog_cats.php
@@ -4,6 +4,6 @@ $locale['401'] = "Kategori:";
 $locale['402'] = "Antall Elementer:";
 $locale['403'] = "Ikke-Kategoriserte Blogger";
 $locale['404'] = "Det er ingen Blogg elementer i denne kategorien.";
-$locale['405'] = "Mer";
+$locale['405'] = "More";
 $locale['406'] = "Vis Alle Kategorier";
 $locale['407'] = "Ingen Blogg Kategori er definert";
