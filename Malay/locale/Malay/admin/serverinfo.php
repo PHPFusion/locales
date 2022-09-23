0dfb2f178c451be0a4b494fb2a4ee26b84c57826
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:36 +0800

    New translations serverinfo.php (Malay)

---
+++ Malay/locale/Malay/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Maklumat Server";
+//General
+$locale['SRV_401'] = "Am";
+$locale['SRV_402'] = "Perisian Server";
+$locale['SRV_403'] = "Perisian Webserver";
+$locale['SRV_404'] = "Versi PHP";
+$locale['SRV_405'] = "Permantauan PHP";
+$locale['SRV_406'] = "Versi MySQL Server";
+$locale['SRV_406a'] = "Mengguna PDO?";
+$locale['SRV_407'] = "Versi PHPFusion";
+$locale['SRV_408'] = "Prefix Jadual";
+$locale['SRV_409'] = "Prefix Cookie";
+$locale['SRV_410'] = "Pelayar dan Perisian";
+$locale['SRV_411'] = "Terjemah oleh: Frederick MC Chan"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "Pengaturan PHP";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "Sokongan untuk";
+$locale['SRV_426'] = "Muat Naik Fail (Saiz fail maksimum)";
+$locale['SRV_428'] = "Papar kesalahan";
+$locale['SRV_429'] = "Fungsi tidak aktif";
+$locale['SRV_430'] = "tiada berkenaan";
+$locale['SRV_431'] = "versi";
+//Write permission
+$locale['SRV_440'] = "Hak Folder";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Butir-butir";
+$locale['SRV_451'] = "Maklumat dan butiran tidak dapat didapati. Sila buat pengesahan fungsi phpinfo() didapati dan aktif?";
