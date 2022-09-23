dc584d1fef44776f637d52d8148e99482fe6b02d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:45:10 +0800

    New translations blog.php (Romanian)

--- Romanian/infusions/blog/locale/Romanian/permalinks/blog.php
+++ Romanian/infusions/blog/locale/Romanian/permalinks/blog.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_blog_title'] = "Postare pe blog";
+$locale['pl_blog_title'] = "Blog";
 $locale['pl_blog_desc'] = "Activeaz&#259; leg&#259;turile permanente pentru post&#259;rile pe blog.";
 $locale['pl_tags_001'] = "ID unei post&#259;rii";
 $locale['pl_tags_002'] = "Titlul sau subiectul unei postar&#259;rii";
