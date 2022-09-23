1c45491b2f63d27c0ef6760e7b99715329223397
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:44:18 +0800

    New translations articles.php (Romanian)

--- Romanian/infusions/articles/locale/Romanian/articles.php
+++ Romanian/infusions/articles/locale/Romanian/articles.php
@@ -1,7 +1,7 @@
 <?php
 $locale['article_0000'] = "Articole";
 $locale['article_0001'] = "Toate articolele";
-$locale['article_0002'] = "Arat&#259; categoriile";
+$locale['article_0002'] = "Arată categoriile";
 $locale['article_0003'] = "Categorii";
 $locale['article_0004'] = "Actualizat ultima dat&#259;";
 $locale['article_0005'] = "Categorie:";
@@ -13,3 +13,4 @@ $locale['article_0041'] = "Vizualizare ca list&#259;";
 $locale['article_0060'] = "Nu exist&#259; categorii de articole definite.";
 $locale['article_0061'] = "Nu exist&#259; articole definite.";
 $locale['article_0062'] = "Nu a fost ad&#259;ugat niciun articol &#238;n aceast&#259; categorie.";
+$locale['article_0063'] = "Cele mai citite";
