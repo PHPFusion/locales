68c7e0853621f62e96ed711c91e055a0f8037810
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:59:44 +0800

    New translations olist.php (German)

--- German/locale/German/bbcodes/olist.php
+++ German/locale/German/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Erstellt eine sortierte Liste vom gew&auml;hlten Typ.";
 $locale['bb_olist_usage'] = "Text #1 :Eingabetaste: Text #2 :Eingabetaste: usw.";
+$locale['bb_olist_0100'] = "Nummerierte Liste";
+$locale['bb_olist_0101'] = "Alphabete Kleinbuchstaben";
+$locale['bb_olist_0102'] = "Alphabete Großbuchstaben";
+$locale['bb_olist_0103'] = "Römische Kleinbuchstaben";
+$locale['bb_olist_0104'] = "Römische Großbuchstaben";
