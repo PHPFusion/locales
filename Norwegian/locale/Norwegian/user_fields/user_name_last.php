0b7b230d276730fe2fa8ccda29f6dbbb25d29703
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:20:34 +0800

    New translations user_name_last.php (Norwegian)

--- Norwegian/locale/Norwegian/user_fields/user_name_last.php
+++ Norwegian/locale/Norwegian/user_fields/user_name_last.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_last'] = "Brukers etternavn";
+$locale['uf_name_last'] = "Etternavn";
 $locale['uf_name_last_desc'] = "Brukers faktiske etternavn.";
 $locale['uf_name_last_error'] = "Brukers faktiske etternavn må skrives inn.";
