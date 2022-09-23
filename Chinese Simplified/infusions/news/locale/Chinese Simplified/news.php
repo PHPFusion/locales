5013843d29cdf5318ad0a91df05132f307d4ad4b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:01:11 +0800

    New translations news.php (Chinese Simplified)

---
+++ Chinese Simplified/infusions/news/locale/Chinese Simplified/news.php
@@ -0,0 +1,19 @@
+<?php
+$locale['news_0001'] = "继续阅读";
+$locale['news_0002'] = "Print";
+$locale['news_0004'] = "新闻";
+$locale['news_0005'] = "尚未发布任何新闻";
+$locale['news_0006'] = "Uncategorised";
+$locale['news_0008'] = "上次更新";
+$locale['news_0009'] = "新建分类";
+$locale['news_0010'] = "所有类别";
+$locale['news_0011'] = "Most Recent News";
+$locale['news_0012'] = "Most Commented News";
+$locale['news_0013'] = "Top Rated News";
+$locale['news_0014'] = "Thumb view";
+$locale['news_0015'] = "List view";
+$locale['news_0016'] = "No categories defined.";
+$locale['news_0017'] = "过滤";
+$locale['news_0018'] = "Show all news";
+$locale['news_0019'] = "News Gallery";
+$locale['news_0020'] = "Most Read News";
