c815db3cca54b19b693e46d549e873b13ee58a72
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:01:40 +0800

    New translations search.php (Hungarian)

--- Hungarian/locale/Hungarian/bbcodes/search.php
+++ Hungarian/locale/Hungarian/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Keresés egy megadott szövegre az oldalon";
 $locale['bb_search_usage'] = "Szöveg";
 $locale['bb_search_prefix'] = "Keresés: ";
-$locale['bb_search_suffix'] = " - ";
+$locale['bb_search_suffix'] = " - Dátum: ";
 $locale['bb_search_where'] = "Típus";
