dc158b5ace7efce593052f92a70736836c537cb6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:53:29 +0800

    New translations bcolor.php (Turkish)

--- Turkish/locale/Turkish/bbcodes/bcolor.php
+++ Turkish/locale/Turkish/bbcodes/bcolor.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_bcolor_description'] = "Seçili metne renkli arkaplan ekler";
-$locale['bb_bcolor_usage'] = "arkaplanı renklendirecek metin";
+$locale['bb_bcolor_usage'] = "Seçili metne renkli arkaplan ekler";
 $locale['bb_bcolor_hex'] = "Arkaplan rengi HEX kodu";
