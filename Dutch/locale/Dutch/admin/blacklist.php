8e5aec0583a5ca620df834504a750291e250e7e3
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:03 +0800

    New translations blacklist.php (Dutch)

--- Dutch/locale/Dutch/admin/blacklist.php
+++ Dutch/locale/Dutch/admin/blacklist.php
@@ -1,27 +1,27 @@
 <?php
-$locale['BLS_000'] = "Zwarte lijst"; //406
+$locale['BLS_000'] = "Zwarte lijst";
 //Blacklist message
 $locale['BLS_010'] = "Ongeldig E-mailadres of IP-adres.";
 $locale['BLS_011'] = "Gebruikers op zwarte lijst plaatsen.";
 $locale['BLS_012'] = "Gebruiker aangepaste zwarte lijst.";
-$locale['BLS_013'] = "Gebruiker van Zwarte Lijst verwijderd"; //401
+$locale['BLS_013'] = "Gebruiker van Zwarte Lijst verwijderd";
 $locale['BLS_014'] = "Weet je zeker dat je deze vermelding wilt verwijderen?";
-$locale['BLS_015'] = "De zwarte lijst is momenteel leeg."; //465
-$locale['BLS_016'] = "Zwarte lijst Email adres is niet geldig."; //405
+$locale['BLS_015'] = "De zwarte lijst is momenteel leeg.";
+$locale['BLS_016'] = "Zwarte lijst Email adres is niet geldig.";
 
-$locale['BLS_020'] = "Gebruiker op zwarte lijst plaatsen"; //420
-$locale['BLS_021'] = "Gebruiker op zwarte lijst wijzigen"; //421
+$locale['BLS_020'] = "Gebruiker op zwarte lijst plaatsen";
+$locale['BLS_021'] = "Gebruiker op zwarte lijst wijzigen";
 $locale['BLS_022'] = "Voeg geblokkeerde gebruiker toe";
 $locale['BLS_023'] = "Momenteel weergegeven %d van %d totale zwarte lijst items.";
 
-$locale['BLS_030'] = "Reden van plaatsing op zwarte lijst"; //461
-$locale['BLS_031'] = "Beheerder"; //467
-$locale['BLS_032'] = "Datum";  //468
-$locale['BLS_033'] = "Opties"; //462
-$locale['BLS_034'] = "IP-adres op zwarte lijst: [STRONG]of[/STRONG]"; //441
-$locale['BLS_035'] = "Email adres op zwarte lijst plaatsen"; //442
-$locale['BLS_036'] = "Reden van plaatsing op zwarte lijst"; //443
-$locale['BLS_037'] = "Gebruiker op zwarte lijst plaatsen";  //444
+$locale['BLS_030'] = "Reden van plaatsing op zwarte lijst";
+$locale['BLS_031'] = "Beheerder";
+$locale['BLS_032'] = "Datum";
+$locale['BLS_033'] = "Opties";
+$locale['BLS_034'] = "IP-adres op zwarte lijst: [STRONG]of[/STRONG]";
+$locale['BLS_035'] = "Email adres op zwarte lijst plaatsen";
+$locale['BLS_036'] = "Reden van plaatsing op zwarte lijst";
+$locale['BLS_037'] = "Gebruiker op zwarte lijst plaatsen";
 $locale['BLS_038'] = "Bijwerken";
 $locale['BLS_039'] = "Selecteer alles";
 
@@ -38,4 +38,4 @@ Gemengde IP-adressen (deze bevatten zowel IPv6 en IPv4 deel) zullen niet worden 
  %+%@domain.tld blokkeerd elk adres met tenm inste 1 plus-teken.<br /> 
 %@domain.tld blokkeers elk andres van domain.tld<br /> 
 %.domain.tld blokkeerd alleb sub-domeinen van het domein domain.tld<br />
-%payday% blokkeerd elk adres wat het woord \"payday\" bevat, wat vaak voorkomy op websites.<br /> domain.tld is een alias of %@domain.tld om het comptatible te maken met de regels in v7.<br />"; //440
+%payday% blokkeerd elk adres wat het woord \"payday\" bevat, wat vaak voorkomy op websites.<br /> domain.tld is een alias of %@domain.tld om het comptatible te maken met de regels in v7.<br />";
