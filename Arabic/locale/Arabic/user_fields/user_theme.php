d93e90c2da94f4a0a2e6d2811729d4ce29f5afff
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:26:25 +0800

    New translations user_theme.php (Arabic)

--- Arabic/locale/Arabic/user_fields/user_theme.php
+++ Arabic/locale/Arabic/user_fields/user_theme.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_theme'] = "قالب";
+$locale['uf_theme'] = "الثيم";
 $locale['uf_theme_desc'] = "السماح للأعضاء بإختيار القالب";
 $locale['uf_theme_error'] = "لا يمكن أن تتركه فارغاً.";
