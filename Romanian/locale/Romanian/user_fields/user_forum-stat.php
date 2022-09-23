38c3c51149c20f4112bc2cec169c6c3b6789b9b0
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:15:06 +0800

    New translations user_forum-stat.php (Romanian)

--- Romanian/locale/Romanian/user_fields/user_forum-stat.php
+++ Romanian/locale/Romanian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Post&#259;ri &#238;n forum";
+$locale['uf_forum-stat'] = "Forum Posts";
 $locale['uf_forum-stat_desc'] = "Afi&#351;eaz&#259; num&#259;rul de post&#259;ri din forum";
 $locale['uf_forum-stat_na'] = "Please install Forum infusion.";
