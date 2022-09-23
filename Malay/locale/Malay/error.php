a82fc8c9ebb909bb89eade93077d7fa4ad61c4e3
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:12 +0800

    New translations error.php (Malay)

--- Malay/locale/Malay/error.php
+++ Malay/locale/Malay/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Tidak dibenarkan untuk melihat laman ini!";
 $locale['err403'] = "Tidak dibenarkan untuk melihat laman ini!";
 $locale['err404'] = "Halaman ini tidak terdapat dalam server ini!";
-$locale['err500'] = "Kesalahan Pendalaman Server!";
 $locale['errunk'] = "Kesalahan Pendalaman lain lain";
-$locale['errmsg'] = "The link you followed may have expired, or the page may not be accessible to you.";
+$locale['errmsg'] = "Pautan yang anda mengikuti mungkin telah tamat tempoh, ataupun akses halaman tidak dibenarkan.";
 $locale['errret'] = "Kembali ke Halaman Utama";
