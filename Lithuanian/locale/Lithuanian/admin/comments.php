16821de6aeb3b6756bbb6dc877160321422c5006
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:17 +0800

    New translations comments.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/admin/comments.php
+++ Lithuanian/locale/Lithuanian/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Redaguoti komentarą";
-$locale['401'] = "Komentarai";
+$locale['C_400'] = "Redaguoti komentarą";
+$locale['C_401'] = "Komentarai";
 // Comments messages
-$locale['410'] = "Komentaras atnaujintas";
-$locale['411'] = "Komentaras ištrintas";
+$locale['C_410'] = "Komentaras atnaujintas";
+$locale['C_411'] = "Komentaras ištrintas";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
 // Edit Comment form
-$locale['420'] = "Išjungti šypsenėles";
-$locale['421'] = "Išsaugoti komentarą";
+$locale['C_420'] = "Išjungti šypsenėles";
+$locale['C_421'] = "Išsaugoti komentarą";
 // Comments
-$locale['431'] = "Trinti";
-$locale['432'] = "Vartotojo IP:";
-$locale['433'] = "Trinti komentarą?";
-$locale['434'] = "Komentarų nėra.";
-$locale['435'] = "Do you really want to add this user to a blacklist?";
-$locale['436'] = "Your comment are blacklisted.";
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "Vartotojo IP:";
+$locale['C_433'] = "Trinti komentarą?";
+$locale['C_434'] = "Komentarų nėra.";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Įjungti komentarų sistemą?";
+$locale['C_441'] = "Leisti svečiams rašyti pranešimus?";
+$locale['C_442'] = "Įjungti avatarų rodymą komentaruose?";
+$locale['C_443'] = "Komentarų puslapyje";
+$locale['C_444'] = "Komentarų rikiavimas";
+$locale['C_445'] = "Didėjančia tvarka";
+$locale['C_446'] = "Mažėjančia tvarka";
