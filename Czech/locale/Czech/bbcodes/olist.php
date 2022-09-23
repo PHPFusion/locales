1804c5e0b996107cc62a67a81028a24a592ebb5f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:59:22 +0800

    New translations olist.php (Czech)

--- Czech/locale/Czech/bbcodes/olist.php
+++ Czech/locale/Czech/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Vytvoří uspořádaný seznam pro zadaný typ";
 $locale['bb_olist_usage'] = "Text 1 :enter: Text 2 :enter: atd";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
