6cfe4d242f1834ea45197f4f4787a20094f099cb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:06:07 +0800

    New translations weblinks.php (Chinese Simplified)

---
+++ Chinese Simplified/infusions/weblinks/locale/Chinese Simplified/search/weblinks.php
@@ -0,0 +1,8 @@
+<?php
+$locale['weblinks.php'] = "Weblinks";
+$locale['w400'] = "Web Links";
+$locale['w401'] = "Web Link";
+$locale['w402'] = "Web Links";
+$locale['w403'] = "[新]";
+$locale['w404'] = "Addition date:";
+$locale['w405'] = "Visits:";
