d71d5f9b3c3ebe505b510d71c5135b915804888b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:31:08 +0800

    New translations comments.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/comments.php
@@ -0,0 +1,14 @@
+<?php
+$locale['c100'] = "Comments";
+$locale['c101'] = "没有任何评论。";
+$locale['c102'] = "Post Comment";
+$locale['c103'] = "Update Comment";
+$locale['c104'] = "Name:";
+$locale['c105'] = "Please Login to Post a Comment.";
+$locale['c106'] = "Manage Comments";
+$locale['c107'] = " Disable smileys in this comment";
+$locale['c110'] = "删除该评论？";
+$locale['c111'] = "发表评论";
+$locale['c112'] = "回复";
+$locale['c113'] = "标题";
+$locale['c114'] = "评论已更新";
