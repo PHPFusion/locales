56032381c0719b585649e06279cf4b2620a8b947
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:12:40 +0800

    New translations user_delete.php (Ukrainian)

--- Ukrainian/locale/Ukrainian/user_fields/user_delete.php
+++ Ukrainian/locale/Ukrainian/user_fields/user_delete.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_delete'] = "Видалення користувача";
+$locale['uf_delete'] = "Видалити";
 $locale['uf_delete_desc'] = "Видалити";
 $locale['uf_delete_del'] = "Видалити мене";
 $locale['uf_delete_exit'] = "Обліковий запис успішно видалено...";
