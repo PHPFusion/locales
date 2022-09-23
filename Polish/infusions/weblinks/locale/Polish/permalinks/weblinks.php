0af7048cb602e701db549b94f39be5bb7f08beff
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:04:48 +0800

    New translations weblinks.php (Polish)

--- Polish/infusions/weblinks/locale/Polish/permalinks/weblinks.php
+++ Polish/infusions/weblinks/locale/Polish/permalinks/weblinks.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_weblinks_title'] = "Linki";
+$locale['pl_weblinks_title'] = "Katalog stron";
 $locale['pl_weblinks_desc'] = "Aktywuje bezpośrednie odnośniki dla linków na stronie.";
