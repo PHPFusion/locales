aa44e9d6d4e4cb9e5f32461a6fe691c30b68e1b3
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:33:57 +0800

    New translations error.php (Norwegian)

--- Norwegian/locale/Norwegian/error.php
+++ Norwegian/locale/Norwegian/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Uautorisert adgang!";
 $locale['err403'] = "Adgang til denne siden er forbudt!";
 $locale['err404'] = "Denne siden ble ikke funnet på denne serveren!";
-$locale['err500'] = "Intern server feil!";
 $locale['errunk'] = "Ukjent feil";
 $locale['errmsg'] = "The link you followed may have expired, or the page may not be accessible to you.";
 $locale['errret'] = "Gå tilbake til forside";
