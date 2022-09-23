118f15d48cfe9701c196c85fb5cfd06d061314d9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:57:36 +0800

    New translations photos.php (Slovak)

--- Slovak/infusions/gallery/locale/Slovak/search/photos.php
+++ Slovak/infusions/gallery/locale/Slovak/search/photos.php
@@ -2,8 +2,8 @@
 $locale['photos.php'] = "Galéria";
 $locale['p400'] = "Fotky";
 $locale['p401'] = "Fotka";
-$locale['p402'] = "fotiek";
+$locale['p402'] = "Fotky";
 $locale['p403'] = "[NOVÉ]";
 $locale['p404'] = "Album:";
 $locale['p405'] = "Pridané:";
-$locale['p406'] = "Zobrazené:";
+$locale['p406'] = "Zobrazení:";
