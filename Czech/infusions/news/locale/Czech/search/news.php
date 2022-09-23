0456ff254422839b07260bdbb93c83c1b77c3b9a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:00:15 +0800

    New translations news.php (Czech)

--- Czech/infusions/news/locale/Czech/search/news.php
+++ Czech/infusions/news/locale/Czech/search/news.php
@@ -1,6 +1,6 @@
 <?php
-$locale['news.php'] = "Novinky";
-$locale['n400'] = "Novinky";
+$locale['news.php'] = "Zprávy";
+$locale['n400'] = "Zprávy";
 $locale['n401'] = "Novinka";
 $locale['n402'] = "novinek";
 $locale['n403'] = "nalezeno v";
