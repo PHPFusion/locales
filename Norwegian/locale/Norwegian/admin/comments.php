ea4da2ec036a6a270ebe8f7085383a2ed5052838
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:37:40 +0800

    New translations comments.php (Norwegian)

--- Norwegian/locale/Norwegian/admin/comments.php
+++ Norwegian/locale/Norwegian/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Rediger kommentar";
-$locale['401'] = "Kommentarer";
+$locale['C_400'] = "Rediger kommentar";
+$locale['C_401'] = "Kommentarer";
 // Comments messages
-$locale['410'] = "Kommentar oppdatert";
-$locale['411'] = "Kommentar slettet";
+$locale['C_410'] = "Kommentar oppdatert";
+$locale['C_411'] = "Kommentar slettet";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
 // Edit Comment form
-$locale['420'] = "Slå av smilefjes i kommentaren";
-$locale['421'] = "Lagre kommentar";
+$locale['C_420'] = "Slå av smilefjes i kommentaren";
+$locale['C_421'] = "Lagre kommentar";
 // Comments
-$locale['431'] = "Slett";
-$locale['432'] = "Brukers IP:";
-$locale['433'] = "Slett denne kommentaren?";
-$locale['434'] = "Det er ikke skrevet kommentarer til dette.";
-$locale['435'] = "Do you really want to add this user to a blacklist?";
-$locale['436'] = "Your comment are blacklisted.";
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "Brukers IP: ";
+$locale['C_433'] = "Slett denne kommentaren?";
+$locale['C_434'] = "Det er ikke skrevet kommentarer til dette.";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Filnavnet ble lagt til feil.";
+$locale['C_441'] = "Tillat gjester å skrive innlegg?";
+$locale['C_442'] = "Aktiver avatar kommentarer:";
+$locale['C_443'] = "Kommentarer pr. side";
+$locale['C_444'] = "eller filnavn:";
+$locale['C_445'] = "Ascending";
+$locale['C_446'] = "Descending";
