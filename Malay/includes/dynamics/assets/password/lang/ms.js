cc3e9453f65a4cb2057a5eaa6b4a875c2fec372e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:33:50 +0800

    New translations en.js (Malay)

---
+++ Malay/includes/dynamics/assets/password/lang/ms.js
@@ -0,0 +1,17 @@
+var pwstrength_locale = {
+    wordMinLength: 'Kata laluan terlalu pendek',
+    wordMaxLength: 'Kata laluan terlalu panjang',
+    wordInvalidChar: 'Kata laluan mengandungi huruf abjad tidak sah',
+    wordNotEmail: 'Jangan menggunakan emel sebagai kata laluan',
+    wordSimilarToUsername: 'Kata laluan anda tidak boleh mengandungi nama pengguna anda',
+    wordTwoCharacterClasses: 'Gunakan huruf abjad kelas yang lain',
+    wordRepetitions: 'Terlalu banyak ulangan',
+    wordSequences: 'Kata laluan anda mengandungi urutan',
+    errorList: 'Kesalahan:',
+    veryWeak: 'Terlalu Lemah',
+    weak: 'Lemah',
+    normal: 'Biasa',
+    medium: 'Sederhana',
+    strong: 'Kuat',
+    veryStrong: 'Sangat kuat'
+};
