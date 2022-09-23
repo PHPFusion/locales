a9016b52c382f8ba4bd18c23153e5a27cf570753
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:26:32 +0800

    New translations user_timezone.php (French)

--- French/locale/French/user_fields/user_timezone.php
+++ French/locale/French/user_fields/user_timezone.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_timezone'] = "Zones horaires";
+$locale['uf_timezone'] = "Fuseau horaire";
 $locale['uf_timezone_desc'] = "Fuseau horaire défini par l'utilisateur.";
 $locale['uf_timezone_error'] = "Le fuseau horaire n'a pas été défini correctement.";
