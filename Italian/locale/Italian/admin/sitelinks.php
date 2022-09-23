eca367b8555a94b4cefc25b6845a1a9ea38c3770
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:49:23 +0800

    New translations sitelinks.php (Italian)

--- Italian/locale/Italian/admin/sitelinks.php
+++ Italian/locale/Italian/admin/sitelinks.php
@@ -1,6 +1,6 @@
 <?php
 // News titles
-$locale['SL_0001'] = "Link del sito";
+$locale['SL_0001'] = "Menù navigazione";
 $locale['SL_0010'] = "Aggiungi collegamento al sito";
 $locale['SL_0011'] = "Modifica sito";
 $locale['SL_0012'] = "Collegamenti Navigazione Sito";
@@ -10,18 +10,18 @@ $locale['SL_0016'] = "Link sito aggiornato";
 $locale['SL_0017'] = "Sito Link cancellato";
 $locale['SL_0018'] = "Site Link Settings updated";
 // Site Links form
-$locale['SL_0020'] = "Link Name:";
+$locale['SL_0020'] = "Link Name";
 $locale['SL_0020a'] = "Link Icon";
 $locale['SL_0021'] = "Link URL:";
 $locale['SL_0022'] = "visibilità:";
-$locale['SL_0023'] = "ordine:";
+$locale['SL_0023'] = "Elemento ordine";
 $locale['SL_0024'] = "Posizione:";
 $locale['SL_0025'] = "pannello di navigazione solo";
 $locale['SL_0026'] = "pannello di navigazione e sub-header";
 $locale['SL_0027'] = "Sub-intestazione solo";
 $locale['SL_0028'] = "Apri il link in una nuova finestra";
-$locale['SL_0029'] = "Link Categoria";
-$locale['SL_0030'] = "Link icona";
+$locale['SL_0029'] = "Link Category";
+$locale['SL_0030'] = "Link Icon";
 $locale['SL_0031'] = "Link Status";
 $locale['SL_0032'] = "No Parent";
 $locale['SL_0033'] = "Link Language";
@@ -32,7 +32,7 @@ $locale['SL_0037'] = "Please select the link parent";
 $locale['SL_0038'] = "There are no links available that can be the link parent";
 $locale['SL_0039'] = "Confirm Move";
 
-$locale['SL_0040'] = "Salva collegamento";
+$locale['SL_0040'] = "Save Link";
 $locale['SL_0041'] = "Link Settings";
 $locale['SL_0042'] = "Configure your site links navigation settings";
 $locale['SL_0043'] = "Links per page";
@@ -44,16 +44,16 @@ $locale['SL_0048'] = "No, do not group the links";
 $locale['SL_0049'] = "Yes, group the links as \"Show more\"";
 
 // Site Links list
-$locale['SL_0050'] = "Link nome";
-$locale['SL_0051'] = "Visibilità";
-$locale['SL_0052'] = "Ordine";
+$locale['SL_0050'] = "Link Name";
+$locale['SL_0051'] = "Visibility";
+$locale['SL_0052'] = "Elemento ordine";
 $locale['SL_0053'] = "Opzioni";
 
 $locale['SL_0062'] = "Nessun collegamenti di sito sono stati definiti.";
 $locale['SL_0063'] = "Use BBCode in Link Name?";
 $locale['SL_0064'] = "Enabling this will reformat link name with BBcodes";
 
-$locale['SL_0070'] = "Icona";
+$locale['SL_0070'] = "Icon";
 $locale['SL_0071'] = "Nuova finestra?";
 $locale['SL_0072'] = "Menu";
 $locale['SL_0073'] = "Riordinare";
