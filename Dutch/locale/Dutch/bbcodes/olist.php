241062b73458ea3c874cf7fbdee721eeb7b6eecf
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:00:20 +0800

    New translations olist.php (Dutch)

--- Dutch/locale/Dutch/bbcodes/olist.php
+++ Dutch/locale/Dutch/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Maakt geordende lijst van opgegeven type";
 $locale['bb_olist_usage'] = "Tekst 1 :enter: Tekst 2 :enter: etc";
+$locale['bb_olist_0100'] = "Genummerde lijst";
+$locale['bb_olist_0101'] = "Alfabets kleine letters";
+$locale['bb_olist_0102'] = "Alfabets hoofdletters";
+$locale['bb_olist_0103'] = "Romeinse kleine letters";
+$locale['bb_olist_0104'] = "Romeinse hoofdletters";
