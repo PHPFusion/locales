4de07a1007f1a7cc1d721de813159b77379d883b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:20:38 +0800

    New translations user_name_last.php (Portuguese)

--- Portuguese/locale/Portuguese/user_fields/user_name_last.php
+++ Portuguese/locale/Portuguese/user_fields/user_name_last.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_last'] = "Apelido";
+$locale['uf_name_last'] = "Sobrenome";
 $locale['uf_name_last_desc'] = "Apelido real do utilizador.";
 $locale['uf_name_last_error'] = "O Apelido real do utilizador não pode ser deixado em branco.";
