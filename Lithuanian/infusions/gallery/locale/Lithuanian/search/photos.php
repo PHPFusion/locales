e7984e557cf4102c4d87e13654e6a67edf7086ec
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:57:10 +0800

    New translations photos.php (Lithuanian)

--- Lithuanian/infusions/gallery/locale/Lithuanian/search/photos.php
+++ Lithuanian/infusions/gallery/locale/Lithuanian/search/photos.php
@@ -1,9 +1,9 @@
 <?php
 $locale['photos.php'] = "Photo Gallery";
-$locale['p400'] = "Foto";
+$locale['p400'] = "Nuotraukos";
 $locale['p401'] = "Foto";
-$locale['p402'] = "Foto";
-$locale['p403'] = "[Nauja]";
-$locale['p404'] = "Albumas:";
+$locale['p402'] = "Nuotraukos";
+$locale['p403'] = "[NAUJAS]";
+$locale['p404'] = "Album:";
 $locale['p405'] = "Įkėlimo data:";
-$locale['p406'] = "Peržiūros:";
+$locale['p406'] = "Views:";
