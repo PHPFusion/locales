15fb04bec142fd308390f9671f110444a90dc00e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:08:34 +0800

    New translations custompages.php (Czech)

--- Czech/locale/Czech/search/custompages.php
+++ Czech/locale/Czech/search/custompages.php
@@ -1,7 +1,7 @@
 <?php
 $locale['c400'] = "Vlastní stránky";
 $locale['c401'] = "Vlastní stránka";
-$locale['c402'] = "vlastních stránek";
+$locale['c402'] = "Vlastní stránky";
 $locale['c403'] = "nalezeno v";
 $locale['c404'] = "název stránky";
 $locale['c405'] = "obsah stránky";
