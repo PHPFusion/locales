f4a307185fdb78afaceaa0d6050ee55c8ea40624
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:48:13 +0800

    New translations downloads.php (Polish)

--- Polish/infusions/downloads/locale/Polish/permalinks/downloads.php
+++ Polish/infusions/downloads/locale/Polish/permalinks/downloads.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_downloads_title'] = "Pobieranie";
+$locale['pl_downloads_title'] = "Download";
 $locale['pl_downloads_desc'] = "Aktywuje bezpośrednie odnośniki dla pobrań na stronie.";
