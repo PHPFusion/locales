16e625364b5428d9d0db24c9ca999758eef44d7b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:36:03 +0800

    New translations blacklist.php (Czech)

--- Czech/locale/Czech/admin/blacklist.php
+++ Czech/locale/Czech/admin/blacklist.php
@@ -1,27 +1,27 @@
 <?php
-$locale['BLS_000'] = "Černá listina"; //406
+$locale['BLS_000'] = "Černá listina";
 //Blacklist message
-$locale['BLS_010'] = "Neplatná e-mailová adresa nebo IP adresa.";
-$locale['BLS_011'] = "Přidávání uživatelů do černé listiny.";
-$locale['BLS_012'] = "Uživatelská modifikovaná černá listina.";
-$locale['BLS_013'] = "Uživatel byl smazán z černé listiny"; //401
+$locale['BLS_010'] = "Email nebo IP adresa nesmí být prázdné.";
+$locale['BLS_011'] = "Záznam byl přidán.";
+$locale['BLS_012'] = "Záznam byl změněn.";
+$locale['BLS_013'] = "Záznam byl odstraněn.";
 $locale['BLS_014'] = "Opravdu chcete tuto položku smazat?";
-$locale['BLS_015'] = "Černá listina je momentálně prázdná."; //465
-$locale['BLS_016'] = "E-mailová adresa černé listiny, není platná e-mailová adresa."; //405
+$locale['BLS_015'] = "Černá listina je momentálně prázdná.";
+$locale['BLS_016'] = "Emailová adresa není platný email.";
 
-$locale['BLS_020'] = "Zablokovat"; //420
-$locale['BLS_021'] = "Upravit blokovaného uživatele"; //421
+$locale['BLS_020'] = "Zablokovat";
+$locale['BLS_021'] = "Upravit blokovaného uživatele";
 $locale['BLS_022'] = "Přidat uživatele na černou listinu";
 $locale['BLS_023'] = "Aktuální zobrazení %d z %d černé listiny.";
 
-$locale['BLS_030'] = "Info o zablokovaných"; //461
-$locale['BLS_031'] = "Admin"; //467
-$locale['BLS_032'] = "Datum";  //468
-$locale['BLS_033'] = "Možnosti"; //462
-$locale['BLS_034'] = "Blokované IP adresy: [STRONG]nebo[/STRONG]"; //441
-$locale['BLS_035'] = "Blokované e-mailové adresy"; //442
-$locale['BLS_036'] = "Důvod zablokování"; //443
-$locale['BLS_037'] = "Přidat uživatele na černou listinu";  //444
+$locale['BLS_030'] = "Info o zablokovaných";
+$locale['BLS_031'] = "Administrátor";
+$locale['BLS_032'] = "Datum";
+$locale['BLS_033'] = "Možnosti";
+$locale['BLS_034'] = "Blokované IP adresy: [STRONG]nebo[/STRONG]";
+$locale['BLS_035'] = "Blokované e-mailové adresy";
+$locale['BLS_036'] = "Důvod zablokování";
+$locale['BLS_037'] = "Přidat uživatele na černou listinu";
 $locale['BLS_038'] = "Aktualizovat";
 $locale['BLS_039'] = "Označit vše";
 
@@ -41,4 +41,4 @@ Můžete vložit celý email, <em>foo@bar.com</em>, a nebo jen doménu, <em>bar.
 %@domain.tld zakáže jakoukoli adresu od domain.tld<br/>
 %.domain.tld zakáže všechny subdomény domény.tld<br/>
 %payday% zakáže jakoukoli adresu, která obsahuje slovo \"payday\",které je velmi časté na stránkách.<br/>
-domain.tld je alias %@domain.tld, aby byl kompatibilní s pravidly definovanými v v7<br/>"; //440
+domain.tld je alias %@domain.tld, aby byl kompatibilní s pravidly definovanými v v7<br/>";
