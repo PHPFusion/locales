62108d2460647b87cb0c7a45da123d7bf28777d6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:36:35 +0800

    New translations comments.php (French)

--- French/locale/French/admin/comments.php
+++ French/locale/French/admin/comments.php
@@ -1,17 +1,26 @@
 <?php
 // Comments titles
-$locale['400'] = "Editer commentaire";
-$locale['401'] = "Commentaires";
+$locale['C_400'] = "Editer commentaire";
+$locale['C_401'] = "Commentaires";
 // Comments messages
-$locale['410'] = "Commentaire modifié";
-$locale['411'] = "Commentaire supprimé";
+$locale['C_410'] = "Commentaire modifié";
+$locale['C_411'] = "Commentaire supprimé";
+$locale['C_412'] = "The comment has been removed and user added to blacklist.";
 // Edit Comment form
-$locale['420'] = "Désactiver les smileys dans ce commentaire";
-$locale['421'] = "Enregistrer le commentaire";
+$locale['C_420'] = "Désactiver les smileys dans ce commentaire";
+$locale['C_421'] = "Enregistrer le commentaire";
 // Comments
-$locale['431'] = "Supprimer et bannir";
-$locale['432'] = "Adresse IP de l'utilisateur :";
-$locale['433'] = "Supprimer ce commentaire ?";
-$locale['434'] = "Il n'y a aucun commentaire pour cet élément.";
-$locale['435'] = "Voulez-vous vraiment ajouter cet utilisateur à une liste noire ?";
-$locale['436'] = "Votre commentaire est mis sur liste noire.";
+$locale['C_431'] = "Delete & Ban";
+$locale['C_432'] = "Adresse IP de l'utilisateur :";
+$locale['C_433'] = "Supprimer ce commentaire ?";
+$locale['C_434'] = "Il n'y a aucun commentaire pour cet élément.";
+$locale['C_435'] = "Do you really want to add this user to a blacklist?";
+$locale['C_436'] = "Blacklisted comment.";
+// Settings
+$locale['C_440'] = "Activer le système de commentaires ?";
+$locale['C_441'] = "Autoriser les invités à poster ?";
+$locale['C_442'] = "Activer les commentaires d'avatar ?";
+$locale['C_443'] = "Commentaires de base par page";
+$locale['C_444'] = "Tri des commentaires";
+$locale['C_445'] = "Croissant";
+$locale['C_446'] = "Décroissant";
