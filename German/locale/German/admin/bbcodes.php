43942f85fd80bdaf991e2830d767d2d427d96b6f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:34:53 +0800

    New translations bbcodes.php (German)

--- German/locale/German/admin/bbcodes.php
+++ German/locale/German/admin/bbcodes.php
@@ -1,5 +1,5 @@
 <?php
-$locale['BBCA_400'] = "BB-Codes Verwaltung";
+$locale['BBCA_400'] = "BB-Codes";
 $locale['BBCA_400a'] = "BB Codes Admin";
 $locale['BBCA_401'] = "BB-Codes testen";
 // Enabled BB Codes
@@ -9,8 +9,8 @@ $locale['BBCA_404'] = "Symbol";
 $locale['BBCA_405'] = "Beschreibung";
 $locale['BBCA_406'] = "Benutzung";
 $locale['BBCA_407'] = "Reihenfolge";
-$locale['BBCA_408'] = "Abw&auml;rts";
-$locale['BBCA_409'] = "Aufw&auml;rts";
+$locale['BBCA_408'] = "Runter";
+$locale['BBCA_409'] = "Nach oben";
 $locale['BBCA_410'] = "Deaktivieren";
 $locale['BBCA_411'] = "Es sind keine BB-Codes aktiviert.";
 // Disabled BB Codes
@@ -18,12 +18,12 @@ $locale['BBCA_413'] = "Deaktivierte BB-Codes";
 $locale['BBCA_414'] = "Aktivieren";
 $locale['BBCA_416'] = "Es sind keine BB-Codes deaktiviert.";
 // Test BB Codes
-$locale['BBCA_417'] = "BB-Codes testen &raquo; Ausgabe";
-$locale['BBCA_418'] = "Smileys in diesem Test deaktivieren.";
+$locale['BBCA_417'] = "BB Codes Test-Ausgabe";
+$locale['BBCA_418'] = "Deaktivieren Sie Smileys in diesem Test";
 $locale['BBCA_418a'] = "Test Nachricht";
-$locale['BBCA_418b'] = "Du musst eine Nachricht angeben, um die BB-Codes zu testen.";
+$locale['BBCA_418b'] = "Bitte geben Sie eine Nachricht ein, um BB Codes zu testen.";
 // Notices
-$locale['BBCA_430'] = "Der BB-Code wurde nach unten verschoben.";
+$locale['BBCA_430'] = "BB-Code wurde nach oben verschoben.";
 $locale['BBCA_431'] = "Der BB-Code wurde nach unten verschoben.";
 $locale['BBCA_432'] = "Der BB-Code wurde aktiviert.";
 $locale['BBCA_433'] = "Der BB-Code wurde deaktiviert.";
