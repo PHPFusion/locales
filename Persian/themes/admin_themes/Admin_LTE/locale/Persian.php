31a735b3ac7775f0218583184d98c3ee057f662d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:07:48 +0800

    New translations English.php (Persian)

---
+++ Persian/themes/admin_themes/Admin_LTE/locale/Persian.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'شما دارید';
+$locale['ALT_002'] = 'There are no messages';
+$locale['ALT_003'] = 'New Message';
+$locale['ALT_004'] = 'عضو از';
+$locale['ALT_005'] = 'جستجو...';
+$locale['ALT_006'] = 'ایجاد شده توسط';
+$locale['ALT_007'] = 'گذرواژه مدیر';
+$locale['ALT_008'] = 'گزینه های طرح بندی';
+$locale['ALT_009'] = 'طرح ثابت';
+$locale['ALT_010'] = 'نوار کناری کوچک';
+$locale['ALT_011'] = 'پوسته';
+$locale['ALT_012'] = 'Dark';
+$locale['ALT_013'] = 'Light';
