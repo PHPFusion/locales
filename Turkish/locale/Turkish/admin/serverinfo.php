4c8095b354a7ee1735e147b292127dcf999fb64d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:28 +0800

    New translations serverinfo.php (Turkish)

---
+++ Turkish/locale/Turkish/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "Sunucu Bilgisi";
+//General
+$locale['SRV_401'] = "Genel";
+$locale['SRV_402'] = "Sunucu Yazılımı";
+$locale['SRV_403'] = "Webserver Yazılımı";
+$locale['SRV_404'] = "PHP Sürümü";
+$locale['SRV_405'] = "PHP Arayüzü";
+$locale['SRV_406'] = "MySQL Sunucu Sürümü";
+$locale['SRV_406a'] = "Veritabanı Sürücüsü";
+$locale['SRV_407'] = "PHPFusion Sürümü";
+$locale['SRV_408'] = "Veritabanı Öneki";
+$locale['SRV_409'] = "Çerez Öneki";
+$locale['SRV_410'] = "Web Tarayıcısı Ve İşletim Sistemi";
+$locale['SRV_411'] = "Tercüme eden: ALTUN, TurkAndroid"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "PHP Ayarları";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "GlobalKayitlar";
+$locale['SRV_425'] = "Destek";
+$locale['SRV_426'] = "Dosya Yükleniyor (Maksimum Dosya Boyutu)";
+$locale['SRV_428'] = "Hataları Göster";
+$locale['SRV_429'] = "Devre Dışı Bırakılmış İşlevler";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "Versiyon";
+//Write permission
+$locale['SRV_440'] = "Klasör İzinleri";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Ayrıntılar";
+$locale['SRV_451'] = "Veri Alınamadı, Phpinfo() İşlevinin Kapalı Olup Olmadığını Kontrol Edin?";
