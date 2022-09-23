ed444ade788c5c1c612a67ff23675e7ffafc6dcf
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:35:45 +0800

    New translations bbcodes.php (Turkish)

--- Turkish/locale/Turkish/admin/bbcodes.php
+++ Turkish/locale/Turkish/admin/bbcodes.php
@@ -8,14 +8,14 @@ $locale['BBCA_403'] = "Adı";
 $locale['BBCA_404'] = "İkon";
 $locale['BBCA_405'] = "Açıklama";
 $locale['BBCA_406'] = "Kullanım";
-$locale['BBCA_407'] = "Sıralama";
+$locale['BBCA_407'] = "Düzen";
 $locale['BBCA_408'] = "Aşağı";
 $locale['BBCA_409'] = "Yukarı";
-$locale['BBCA_410'] = "Kapalı";
+$locale['BBCA_410'] = "Pasifleştir";
 $locale['BBCA_411'] = "Etkin BB Kodu Yok.";
 // Disabled BB Codes
 $locale['BBCA_413'] = "Kapalı BB Kodları";
-$locale['BBCA_414'] = "Aktif Et";
+$locale['BBCA_414'] = "Etkinleştir";
 $locale['BBCA_416'] = "Kapatılmış BB Kodları Yok.";
 // Test BB Codes
 $locale['BBCA_417'] = "BB Kodları Test Çıktısı";
@@ -27,3 +27,9 @@ $locale['BBCA_430'] = "BB Kodu Yukarı Taşındı.";
 $locale['BBCA_431'] = "BB Kodu Aşağı Taşındı.";
 $locale['BBCA_432'] = "BB Kodu Etkinleştirildi.";
 $locale['BBCA_433'] = "BB Kodu Devre Dışı Bırakıldı.";
+$locale['BBCA_444'] = "BB Kodu güncellendi";
+$locale['BBCA_445'] = "BB Kodu güncelleme hatası";
+
+$locale['BBCA_500'] = "Ayırıcı ekle";
+$locale['BBCA_501'] = "Ayırıcı";
+$locale['BBCA_502'] = "BBcode düğmelerini gruplamak için ayırıcı";
