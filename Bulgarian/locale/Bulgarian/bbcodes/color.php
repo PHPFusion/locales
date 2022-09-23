9b5c71f8aa87d57914cbb409b1c5971152b0e577
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:55:25 +0800

    New translations color.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/bbcodes/color.php
+++ Bulgarian/locale/Bulgarian/bbcodes/color.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_color_description'] = "Оцвевята избрания текст";
-$locale['bb_color_usage'] = "Цвят на избрания текст";
+$locale['bb_color_usage'] = "Оцвевята избрания текст";
 $locale['bb_color_hex'] = "HEX стойност на цвета на текста";
