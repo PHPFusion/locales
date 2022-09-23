23f452838227611c457703e84ac3a0310082620e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:04:50 +0800

    New translations ulist.php (Russian)

--- Russian/locale/Russian/bbcodes/ulist.php
+++ Russian/locale/Russian/bbcodes/ulist.php
@@ -1,6 +1,6 @@
 <?php
 $locale['bb_ulist_description'] = "Создание списка с маркерами";
-$locale['bb_ulist_usage'] = "Строка 1 :enter: Строка 2 :enter: и т.д.";
+$locale['bb_ulist_usage'] = "Элемент 1 :enter: Элемент 2 :enter: и т.д.";
 $locale['bb_ulist_1'] = "диск";
 $locale['bb_ulist_2'] = "круг";
 $locale['bb_ulist_3'] = "квадрат";
