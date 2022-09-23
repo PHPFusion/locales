61f7c2e25f5cf8ebbc459fc13687318cc47347f7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 23:32:36 +0800

    New translations en.js (Arabic)

--- Arabic/includes/dynamics/assets/password/lang/ar.js
+++ Arabic/includes/dynamics/assets/password/lang/ar.js
@@ -1,17 +1,17 @@
 var pwstrength_locale = {
-    wordMinLength: "كلمة المرور قصيرة جداً",
-    wordMaxLength: "كلمة المرور طويلة جدا",
-    wordInvalidChar: "تحتوي كلمة المرور على رموز غير صالحة",
-    wordNotEmail: "لا تستخدم بريدك الإلكتروني ككلمة مرور",
-    wordSimilarToUsername: "لا يمكن ان تحتوي كلمة المرور على إسم المستخدم",
-    wordTwoCharacterClasses: "إستخدم فئات أحرف مختلفة",
-    wordRepetitions: "تكرارات كثيرة",
-    wordSequences: "تحتوي كلمة المرور على أنماط متتابعة",
-    errorList: "الأخطاء:",
-    veryWeak: "ضعيفة جداً",
-    weak: "ضعيفة",
-    normal: "عادية",
-    medium: "متوسطة",
-    strong: "قوية",
-    veryStrong: "قوية جداً"
-}
+    wordMinLength: 'Your password is too short',
+    wordMaxLength: 'Your password is too long',
+    wordInvalidChar: 'Your password contains an invalid character',
+    wordNotEmail: 'Do not use your email as your password',
+    wordSimilarToUsername: 'Your password cannot contain your username',
+    wordTwoCharacterClasses: 'Use different character classes',
+    wordRepetitions: 'Too many repetitions',
+    wordSequences: 'Your password contains sequences',
+    errorList: 'Errors:',
+    veryWeak: 'Very Weak',
+    weak: 'Weak',
+    normal: 'عادي',
+    medium: 'Medium',
+    strong: 'Strong',
+    veryStrong: 'Very Strong'
+};
