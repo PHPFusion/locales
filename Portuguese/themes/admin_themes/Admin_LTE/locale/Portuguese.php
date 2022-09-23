eeb7f124a52d4104597ca4764475976ac1e78452
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:08:15 +0800

    New translations English.php (Portuguese)

---
+++ Portuguese/themes/admin_themes/Admin_LTE/locale/Portuguese.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'Você tem';
+$locale['ALT_002'] = 'Não há nenhuma mensagem';
+$locale['ALT_003'] = 'Nova Mensagem';
+$locale['ALT_004'] = 'Membro desde';
+$locale['ALT_005'] = 'Pesquisar...';
+$locale['ALT_006'] = 'Criado por';
+$locale['ALT_007'] = 'Palavra-passe de Admin';
+$locale['ALT_008'] = 'Opções de Apresentação da Página';
+$locale['ALT_009'] = 'Layout Fixo';
+$locale['ALT_010'] = 'Barra Lateral pequena';
+$locale['ALT_011'] = 'Temas';
+$locale['ALT_012'] = 'Escuro';
+$locale['ALT_013'] = 'Claro';
