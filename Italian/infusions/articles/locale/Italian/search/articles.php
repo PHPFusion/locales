3da36d0317840f3ed08bed7ebbbdcf82b55710f5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:44:07 +0800

    New translations articles.php (Italian)

--- Italian/infusions/articles/locale/Italian/search/articles.php
+++ Italian/infusions/articles/locale/Italian/search/articles.php
@@ -1,8 +1,8 @@
 <?php
 $locale['articles.php'] = "Articoli";
 $locale['a400'] = "Articoli";
-$locale['a401'] = "Articolo";
+$locale['a401'] = "Article";
 $locale['a402'] = "Articoli";
-$locale['a403'] = "Trovato in";
+$locale['a403'] = "Trovata in";
 $locale['a404'] = "oggetto articolo";
 $locale['a405'] = "testo articolo";
