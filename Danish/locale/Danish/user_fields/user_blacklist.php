acc427dfe1e90bbc8dde801aec3ffec225861170
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:11:16 +0800

    New translations user_blacklist.php (Danish)

--- Danish/locale/Danish/user_fields/user_blacklist.php
+++ Danish/locale/Danish/user_fields/user_blacklist.php
@@ -3,12 +3,3 @@ $locale['uf_blacklist'] = "Blacklist";
 $locale['uf_blacklist_desc'] = "Udelukkelse af enkelte brugere";
 $locale['uf_blacklist_error'] = "Fejl i datatypen";
 $locale['uf_blacklist_message'] = "Når du udelukker en eller anden, så kan denne bruger ikke længere se dig og heller ikke se dine debatindlæg eller kontakte dig.";
-
-$locale['uf_blacklist_000'] = "Blokerede og ignorerede brugere";
-$locale['uf_blacklist_001'] = "Ophæv udelukkelse";
-$locale['uf_blacklist_002'] = "Ignorer";
-$locale['uf_blacklist_003'] = "Der er ikke udelukket brugere.";
-$locale['uf_blacklist_004'] = "Brugeren %s er blevet fjernet fra den sorte liste.";
-$locale['uf_blacklist_005'] = "Brugeren blev ikke udelukket.";
-$locale['uf_blacklist_006'] = "Ingen bruger fundet.";
-$locale['uf_blacklist_007'] = "Ikke tilgængelig.";
