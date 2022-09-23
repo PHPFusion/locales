7a87b7a0fe978d834407ec2ca39b37b1539f0bd5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:01:27 +0800

    New translations search.php (Slovak)

--- Slovak/locale/Slovak/bbcodes/search.php
+++ Slovak/locale/Slovak/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Vyhľadanie daného textu na aktuálnej stránke";
 $locale['bb_search_usage'] = "Text, ktorý chcete vyhľadávať";
 $locale['bb_search_prefix'] = "Hľadať ";
-$locale['bb_search_suffix'] = " na ";
+$locale['bb_search_suffix'] = " dňa ";
 $locale['bb_search_where'] = "Kde";
