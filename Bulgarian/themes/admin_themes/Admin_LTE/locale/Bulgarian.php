f431eae27605450d521d3f039f474ee783201940
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:08:08 +0800

    New translations English.php (Bulgarian)

---
+++ Bulgarian/themes/admin_themes/Admin_LTE/locale/Bulgarian.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'You have';
+$locale['ALT_002'] = 'Няма съобщения';
+$locale['ALT_003'] = 'Ново съобщение';
+$locale['ALT_004'] = 'Member since';
+$locale['ALT_005'] = 'Търсене...';
+$locale['ALT_006'] = 'Created by';
+$locale['ALT_007'] = 'Admin Password';
+$locale['ALT_008'] = 'Layout Options';
+$locale['ALT_009'] = 'Fixed layout';
+$locale['ALT_010'] = 'Small Sidebar';
+$locale['ALT_011'] = 'Skins';
+$locale['ALT_012'] = 'Тъмна';
+$locale['ALT_013'] = 'Светла';
