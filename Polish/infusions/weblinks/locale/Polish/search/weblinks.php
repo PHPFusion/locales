8c83b1acc3b3b2c28510f6a9562e7baa96bc2160
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:05:59 +0800

    New translations weblinks.php (Polish)

--- Polish/infusions/weblinks/locale/Polish/search/weblinks.php
+++ Polish/infusions/weblinks/locale/Polish/search/weblinks.php
@@ -1,8 +1,8 @@
 <?php
 $locale['weblinks.php'] = "Linki";
-$locale['w400'] = "Linki";
+$locale['w400'] = "Katalog stron";
 $locale['w401'] = "link";
-$locale['w402'] = "linków";
+$locale['w402'] = "Katalog stron";
 $locale['w403'] = "[NOWY]";
 $locale['w404'] = "Data dodania:";
 $locale['w405'] = "Odwiedzin:";
