68962e08bb7dcb7fb8588b6aa256615d7b727de1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:51:44 +0800

    New translations faqs.php (Turkish)

--- Turkish/infusions/faq/locale/Turkish/search/faqs.php
+++ Turkish/infusions/faq/locale/Turkish/search/faqs.php
@@ -1,8 +1,8 @@
 <?php
 $locale['faqs.php'] = "SSS";
-$locale['fq400'] = "SSS'ler";
+$locale['fq400'] = "SSS";
 $locale['fq401'] = "SSS";
-$locale['fq402'] = "SSS'ler";
+$locale['fq402'] = "SSS";
 $locale['fq403'] = "içinde bulundu";
 $locale['fq404'] = "SSS sorusu";
 $locale['fq405'] = "SSS cevapı";
