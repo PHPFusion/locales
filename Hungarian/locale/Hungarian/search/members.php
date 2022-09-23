963ab63193275c6c459969cbffdc2b588b715402
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:09:31 +0800

    New translations members.php (Hungarian)

--- Hungarian/locale/Hungarian/search/members.php
+++ Hungarian/locale/Hungarian/search/members.php
@@ -1,5 +1,5 @@
 <?php
 $locale['m400'] = "Tagok";
-$locale['m401'] = "tagot";
-$locale['m402'] = "Tagot";
+$locale['m401'] = "Tag";
+$locale['m402'] = "Tagok";
 $locale['m403'] = "A felhasználói profilok csak tagok számára érhetők el!";
