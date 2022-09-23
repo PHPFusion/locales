e53995e8c37103971fd13ce09da8aad4d7e7f9c2
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:11:31 +0800

    New translations user_blacklist.php (Slovak)

--- Slovak/locale/Slovak/user_fields/user_blacklist.php
+++ Slovak/locale/Slovak/user_fields/user_blacklist.php
@@ -3,12 +3,3 @@ $locale['uf_blacklist'] = "Čierna listina";
 $locale['uf_blacklist_desc'] = "Blokovanie užívateľov";
 $locale['uf_blacklist_error'] = "Chyba v dátovom type";
 $locale['uf_blacklist_message'] = "Keď niekoho zablokujete, táto osoba už vás nebude môcť vidieť, vrátane položiek, ktoré uverejníte a ktoré vás kontaktujú.";
-
-$locale['uf_blacklist_000'] = "Blokovaní a ignorovaní užívatelia";
-$locale['uf_blacklist_001'] = "Odblokovať";
-$locale['uf_blacklist_002'] = "Ignorovať";
-$locale['uf_blacklist_003'] = "Žiadny užívateľ nie je na čiernej listine.";
-$locale['uf_blacklist_004'] = "Užívatelia %s boli odstránený z vašej čiernej listiny.";
-$locale['uf_blacklist_005'] = "Užívateľ nebol na čiernej listine.";
-$locale['uf_blacklist_006'] = "Nenašiel sa žiadny užívateľ.";
-$locale['uf_blacklist_007'] = "Nie je k dispozícii.";
