7d1661225c050d73201a107d62e6e85a84e577fb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:59:43 +0800

    New translations olist.php (Danish)

--- Danish/locale/Danish/bbcodes/olist.php
+++ Danish/locale/Danish/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Opretter en ordnet liste";
 $locale['bb_olist_usage'] = "Tekst 1 :enter: Tekst 2 :enter: osv.";
+$locale['bb_olist_0100'] = "Numerisk liste";
+$locale['bb_olist_0101'] = "Alfabeter små";
+$locale['bb_olist_0102'] = "Alfabeter store";
+$locale['bb_olist_0103'] = "Roman små";
+$locale['bb_olist_0104'] = "Roman store";
