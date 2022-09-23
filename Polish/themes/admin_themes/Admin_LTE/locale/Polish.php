30b3f13ba77f21e17455f09680c54ff47e8fc451
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 19:08:24 +0800

    New translations English.php (Polish)

---
+++ Polish/themes/admin_themes/Admin_LTE/locale/Polish.php
@@ -0,0 +1,14 @@
+<?php
+$locale['ALT_001'] = 'Masz';
+$locale['ALT_002'] = 'Nie ma żadnych wiadomości';
+$locale['ALT_003'] = 'Nowa wiadomość';
+$locale['ALT_004'] = 'Członek od';
+$locale['ALT_005'] = 'Szukaj...';
+$locale['ALT_006'] = 'Stworzone przez';
+$locale['ALT_007'] = 'Hasło administratora';
+$locale['ALT_008'] = 'Opcje wyglądu';
+$locale['ALT_009'] = 'Stały wygląd';
+$locale['ALT_010'] = 'Mały panel boczny';
+$locale['ALT_011'] = 'Skórki';
+$locale['ALT_012'] = 'Ciemny';
+$locale['ALT_013'] = 'Jasny';
