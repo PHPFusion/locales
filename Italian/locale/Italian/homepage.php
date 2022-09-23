61c5c099a0f0e5b5f2359fdf967c31c07cabf0c7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:36:20 +0800

    New translations homepage.php (Italian)

--- Italian/locale/Italian/homepage.php
+++ Italian/locale/Italian/homepage.php
@@ -1,9 +1,9 @@
 <?php
 // block titles
 $locale['home_0000'] = "Ultime Notizie";
-$locale['home_0001'] = "Ultimi Articoli";
+$locale['home_0001'] = "Ultimi articoli";
 $locale['home_0002'] = "Ultimi Blog";
-$locale['home_0003'] = "Ultimi Download";
+$locale['home_0003'] = "Download più recenti";
 // no content messages
 $locale['home_0050'] = "Nessuna Notizia da visualizzare";
 $locale['home_0051'] = "Nessun Articolo da visualizzare";
@@ -12,6 +12,6 @@ $locale['home_0053'] = "Nessun Download da visualizzare";
 // common text
 $locale['home_0100'] = "Contenuto moduli non installati";
 $locale['home_0101'] = "Installare almeno un modulo per il contenuto di testo o cambiare la pagina iniziale dalle impostazioni principali.";
-$locale['home_0102'] = "Non categorizzato";
+$locale['home_0102'] = "Non Categorizzato";
 $locale['home_0105'] = "Pubblicato da ";
 $locale['home_0106'] = " in ";
