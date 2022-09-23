829e8cab8dead4c27ca7891ba5381bac59aa7c84
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:59:21 +0800

    New translations olist.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/bbcodes/olist.php
+++ Bulgarian/locale/Bulgarian/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Създава подреден списък от указан тип";
 $locale['bb_olist_usage'] = "Текст 1 :въведи: Текст 2 :въведи: и т.н.";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
