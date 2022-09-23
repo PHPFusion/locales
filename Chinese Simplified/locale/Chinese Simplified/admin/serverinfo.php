b918eedc75ed150790ec59e618614234b91856c1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:47:30 +0800

    New translations serverinfo.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/admin/serverinfo.php
@@ -0,0 +1,32 @@
+<?php
+$locale['SRV_400'] = "服务器信息";
+//General
+$locale['SRV_401'] = "General";
+$locale['SRV_402'] = "Server software";
+$locale['SRV_403'] = "Webserver software";
+$locale['SRV_404'] = "PHP version";
+$locale['SRV_405'] = "PHP interface";
+$locale['SRV_406'] = "MySQL Server version";
+$locale['SRV_406a'] = "Enable PDO?";
+$locale['SRV_407'] = "PHPFusion version";
+$locale['SRV_408'] = "Database prefix";
+$locale['SRV_409'] = "Cookie prefix";
+$locale['SRV_410'] = "Webbrowser and OS";
+$locale['SRV_411'] = "翻译者：Frederick MC Chan"; // If you helped with the translation, you can write your name here. Management can update credits based on recent contributions.
+//PHP settings
+$locale['SRV_420'] = "PHP settings";
+$locale['SRV_423'] = "Safe-mode";
+$locale['SRV_424'] = "RegisterGlobals";
+$locale['SRV_425'] = "Support for";
+$locale['SRV_426'] = "Files upload (max file size)";
+$locale['SRV_428'] = "Display errors";
+$locale['SRV_429'] = "Disabled functions";
+$locale['SRV_430'] = "n/a";
+$locale['SRV_431'] = "version";
+//Write permission
+$locale['SRV_440'] = "Folder Permissions";
+$locale['SRV_441'] = "Writeable";
+$locale['SRV_442'] = "Not writeable";
+//PHPinfo
+$locale['SRV_450'] = "Details";
+$locale['SRV_451'] = "Unable to get data. Check if phpinfo() function is switched off?";
