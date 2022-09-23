68ffb0f6dd981b94ab52eba2b22829b9372e58cb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:44:01 +0800

    New translations articles.php (Czech)

--- Czech/infusions/articles/locale/Czech/search/articles.php
+++ Czech/infusions/articles/locale/Czech/search/articles.php
@@ -2,7 +2,7 @@
 $locale['articles.php'] = "Články";
 $locale['a400'] = "Články";
 $locale['a401'] = "Článek";
-$locale['a402'] = "článků";
+$locale['a402'] = "Články";
 $locale['a403'] = "nalezeno v";
 $locale['a404'] = "předmět článku";
 $locale['a405'] = "text článku";
