08d8b26bf3cf116f7fd0f07130060f11fc80f5e4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:13:16 +0800

    New translations user_delete.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/user_fields/user_delete.php
+++ Lithuanian/locale/Lithuanian/user_fields/user_delete.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_delete'] = "Ištrinti vartotoją";
-$locale['uf_delete_desc'] = "Pašalinti vartotoją";
+$locale['uf_delete_desc'] = "Ištrinti vartotoją";
 $locale['uf_delete_del'] = "Pašalinti mane";
 $locale['uf_delete_exit'] = "Vartotojas ištrintas...";
