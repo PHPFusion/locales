be20660350396607ee302c9a8d8e748371ca9f6a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:53:58 +0800

    New translations forum.php (Ukrainian)

--- Ukrainian/infusions/forum/locale/Ukrainian/search/forum.php
+++ Ukrainian/infusions/forum/locale/Ukrainian/search/forum.php
@@ -1,9 +1,9 @@
 <?php
 $locale['forum.php'] = "Форум";
-$locale['f400'] = "Теми Форуму";
+$locale['f400'] = "Forum Posts";
 $locale['f401'] = "Усі Форуми";
 $locale['f402'] = "Тема Форуму";
-$locale['f403'] = "Теми Форуму";
+$locale['f403'] = "Forum Posts";
 $locale['f404'] = "Прикріплені:";
 $locale['f405'] = "Сповіщення:";
 $locale['f406'] = "Знайдено в";
