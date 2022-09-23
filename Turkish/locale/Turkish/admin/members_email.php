f44d6971250429a7f1556294253e21f0c8fe6ce1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:44:16 +0800

    New translations members_email.php (Turkish)

--- Turkish/locale/Turkish/admin/members_email.php
+++ Turkish/locale/Turkish/admin/members_email.php
@@ -13,11 +13,11 @@ $locale['email_deactivate_subject'] = "[SITENAME] Adresindeki Hesabın Yeniden E
 $locale['email_deactivate_message'] = "Merhaba [USER_NAME],<br/>[SITENAME] Adresinde En Son Giriş Yaptığınız Günden [DEACTIVATION_PERIOD] Gün Geçti. Kullanıcınız Etkin Olmayan Olarak İşaretlendi Ancak Tüm Hesap Ayrıntılarınız Ve İçeriğiniz Bozulmadan Kaldı.<br/><br/>
 Hesabınızı Yeniden Etkinleştirmek İçin Aşağıdaki Bağlantıyı Tıklamanız Yeterlidir: [REACTIVATION_LINK]<br/><br/>Saygılarımızla,<br/>[SITEUSERNAME]";
 
-$locale['email_ban_subject'] = "[SITENAME] Üzerindeki Hesabınız Yasaklandı";
+$locale['email_ban_subject'] = "[SITENAME] hesabınız yasaklandı";
 $locale['email_ban_message'] = "Merhaba [USER_NAME],<br/>[SITENAME] Üzerindeki Hesabınız Şu Nedenden Dolayı, [ADMIN_USERNAME] Tarafından Yasaklandı:<br/>
 [REASON]<br/>Bu Yasak Hakkında Daha Fazla Bilgi Istiyorsanız, Lütfen [SITENAME] Adresindeki Site Yöneticisine Başvurun.<br/><br/>Saygılarımızla,<br/>[SITEUSERNAME]";
 
-$locale['email_secban_subject'] = "[SITENAME] Üzerindeki Hesabınız Yasaklandı";
+$locale['email_secban_subject'] = "[SITENAME] hesabınız yasaklandı";
 $locale['email_secban_message'] = "Merhaba [USER_NAME],<br/>[SITENAME] Üzerindeki Hesabınız [ADMIN_USERNAME] Tarafından Yasaklandı, Çünkü Size Akredite Edilmiş Veya Hesabınıza Bağlı Bazı İşlemler Siteyle İlgili Bir Güvenlik Tehdidi Olarak Değerlendirildi.<br/>
 Bu Güvenlik Yasağı Hakkında Daha Fazla Bilgi Istiyorsanız, Lütfen [SITENAME] Adresinden Site Yöneticisine Başvurun.<br/><br/>Saygılarımızla,<br/>[SITEUSERNAME]";
 
