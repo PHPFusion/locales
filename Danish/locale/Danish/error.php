3ac50a85afdf3afaab62c38cd04e7c0e5790d734
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:34:32 +0800

    New translations error.php (Danish)

--- Danish/locale/Danish/error.php
+++ Danish/locale/Danish/error.php
@@ -2,7 +2,6 @@
 $locale['err401'] = "Uautoriseret adgang til denne side";
 $locale['err403'] = "Adgang til siden forbudt";
 $locale['err404'] = "Siden blev ikke fundet på denne server";
-$locale['err500'] = "Intern serverfejl";
 $locale['errunk'] = "Ukendt fejl";
 $locale['errmsg'] = "Det link, du har forsøgt at følge, er måske udløbet - eller måske har du ikke adgang til siden.";
 $locale['errret'] = "Vend tilbage til forsiden";
