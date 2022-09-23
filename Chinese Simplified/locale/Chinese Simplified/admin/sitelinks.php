d3bda4ebc740511b8ca2273256e9b5e564f04e9c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 17:49:36 +0800

    New translations sitelinks.php (Chinese Simplified)

---
+++ Chinese Simplified/locale/Chinese Simplified/admin/sitelinks.php
@@ -0,0 +1,68 @@
+<?php
+// News titles
+$locale['SL_0001'] = "Site Links";
+$locale['SL_0010'] = "添加站点链接";
+$locale['SL_0011'] = "编辑站点链接";
+$locale['SL_0012'] = "当前站点链接";
+// News messages
+$locale['SL_0015'] = "已添加站点链接";
+$locale['SL_0016'] = "网站链接已更新";
+$locale['SL_0017'] = "网站链接已删除";
+$locale['SL_0018'] = "网站链接设置更新";
+// Site Links form
+$locale['SL_0020'] = "Link Name";
+$locale['SL_0020a'] = "Link Icon";
+$locale['SL_0021'] = "链接网址";
+$locale['SL_0022'] = "Visibility:";
+$locale['SL_0023'] = "Item Ordering";
+$locale['SL_0024'] = "Position:";
+$locale['SL_0025'] = "navigation panel only";
+$locale['SL_0026'] = "navigation panel and sub-header";
+$locale['SL_0027'] = "sub-header only";
+$locale['SL_0028'] = "在新窗口中打开链接";
+$locale['SL_0029'] = "Link Category";
+$locale['SL_0030'] = "Link Icon";
+$locale['SL_0031'] = "Link Status";
+$locale['SL_0032'] = "No Parent";
+$locale['SL_0033'] = "链接语言";
+$locale['SL_0034'] = "Protected";
+$locale['SL_0035'] = "Sub-links";
+$locale['SL_0036'] = "Move Site links to...";
+$locale['SL_0037'] = "Please select the link parent";
+$locale['SL_0038'] = "There are no links available that can be the link parent";
+$locale['SL_0039'] = "Confirm Move";
+
+$locale['SL_0040'] = "Save Link";
+$locale['SL_0041'] = "Link Settings";
+$locale['SL_0042'] = "Configure your site links navigation settings";
+$locale['SL_0043'] = "Links per page";
+$locale['SL_0044'] = "Maximum site links show on the navigation bar";
+$locale['SL_0045'] = "Number of links";
+$locale['SL_0046'] = "Link Grouping";
+$locale['SL_0047'] = "Group links when amount of links exceeds links per page";
+$locale['SL_0048'] = "No, do not group the links";
+$locale['SL_0049'] = "Yes, group the links as \"Show more\"";
+
+// Site Links list
+$locale['SL_0050'] = "Link Name";
+$locale['SL_0051'] = "Visibility";
+$locale['SL_0052'] = "Item Ordering";
+$locale['SL_0053'] = "设置";
+
+$locale['SL_0062'] = "No Site Links have been defined.";
+$locale['SL_0063'] = "Use BBCode in Link Name?";
+$locale['SL_0064'] = "启用此选项将重置与 BB代码 链接名称";
+
+$locale['SL_0070'] = "图标";
+$locale['SL_0071'] = "新窗口";
+$locale['SL_0072'] = "菜单";
+$locale['SL_0073'] = "重新排序";
+$locale['SL_0074'] = "移动";
+$locale['SL_0075'] = "显示选项";
+
+// Delete Site Link category
+$locale['SL_0080'] = "删除此站点链接吗？";
+// Error
+$locale['SL_0085'] = "请输入链接的名称";
+$locale['SL_0086'] = "请输入链接URL";
+$locale['SL_0087'] = "没有要更新的项目，请检查网站链接。";
