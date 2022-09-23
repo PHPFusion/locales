b5b85a0eea5cdc9429509381b720420ccb404dca
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:18:42 +0800

    New translations user_location.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/user_fields/user_location.php
+++ Bulgarian/locale/Bulgarian/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Местонахождение";
+$locale['uf_location'] = "Локация";
 $locale['uf_location_desc'] = "Страна, град";
 $locale['uf_location_error'] = "Полето за местонахождение не може да бъде оставено празно.";
