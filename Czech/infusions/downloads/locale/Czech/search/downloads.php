78d266ef0d4fa7bb69b27cafbd456578e273a542
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:49:17 +0800

    New translations downloads.php (Czech)

--- Czech/infusions/downloads/locale/Czech/search/downloads.php
+++ Czech/infusions/downloads/locale/Czech/search/downloads.php
@@ -2,7 +2,7 @@
 $locale['downloads.php'] = "Ke stažení";
 $locale['d400'] = "Ke stažení";
 $locale['d401'] = "Stáhnout";
-$locale['d402'] = "souborů ke stažení";
+$locale['d402'] = "Ke stažení";
 $locale['d403'] = "[NOVÉ]";
 $locale['d404'] = "Licence:";
 $locale['d405'] = "OS:";
