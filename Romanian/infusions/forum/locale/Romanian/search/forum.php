93e8eb8e1731840cc17eeca94b9c20b52ec4a6c6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:52:44 +0800

    New translations forum.php (Romanian)

--- Romanian/infusions/forum/locale/Romanian/search/forum.php
+++ Romanian/infusions/forum/locale/Romanian/search/forum.php
@@ -1,9 +1,9 @@
 <?php
 $locale['forum.php'] = "Forum";
-$locale['f400'] = "Post&#259;ri forum";
+$locale['f400'] = "Forum Posts";
 $locale['f401'] = "toate forumurile";
 $locale['f402'] = "Postare forum";
-$locale['f403'] = "Post&#259;ri forum";
+$locale['f403'] = "Forum Posts";
 $locale['f404'] = "Fixat&#259;:";
 $locale['f405'] = "Anun&#355;:";
 $locale['f406'] = "g&#259;sit(e) &#238;n";
