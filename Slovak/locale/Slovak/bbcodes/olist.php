1df2ca3054ec28a963fac283980106eb0801189d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:00:25 +0800

    New translations olist.php (Slovak)

--- Slovak/locale/Slovak/bbcodes/olist.php
+++ Slovak/locale/Slovak/bbcodes/olist.php
@@ -1,3 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "Vytvorí usporiadaný zoznam pre zadaný typ";
 $locale['bb_olist_usage'] = "Text 1 :enter: Text 2 :enter: atď.";
+$locale['bb_olist_0100'] = "Numeric List";
+$locale['bb_olist_0101'] = "Alphabets Lowercase";
+$locale['bb_olist_0102'] = "Alphabets Uppercase";
+$locale['bb_olist_0103'] = "Roman Lowercase";
+$locale['bb_olist_0104'] = "Roman Uppercase";
