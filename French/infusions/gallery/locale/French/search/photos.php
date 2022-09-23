cdc38786e828677e35925e5b8e355481fdda0a76
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-23 18:56:30 +0800

    New translations photos.php (French)

--- French/infusions/gallery/locale/French/search/photos.php
+++ French/infusions/gallery/locale/French/search/photos.php
@@ -4,6 +4,6 @@ $locale['p400'] = "Photos";
 $locale['p401'] = "Photo";
 $locale['p402'] = "Photos";
 $locale['p403'] = "[Nouveau]";
-$locale['p404'] = "Album :";
+$locale['p404'] = "Album:";
 $locale['p405'] = "Date d'ajout :";
 $locale['p406'] = "Vues:";
