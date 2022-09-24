8cbddd913150b28117f2d3fc7b8853751613b713
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:57:56 +0800

    New translations photos.php (Czech)

--- Czech/infusions/gallery/locale/Czech/search/photos.php
+++ Czech/infusions/gallery/locale/Czech/search/photos.php
@@ -2,8 +2,8 @@
 $locale['photos.php'] = "Fotogalerie";
 $locale['p400'] = "Fotky";
 $locale['p401'] = "Fotka";
-$locale['p402'] = "Fotky";
+$locale['p402'] = "fotek";
 $locale['p403'] = "[NOVÉ]";
 $locale['p404'] = "Album:";
 $locale['p405'] = "Přidáno:";
-$locale['p406'] = "Zobrazení:";
+$locale['p406'] = "Zobrazeni:";
