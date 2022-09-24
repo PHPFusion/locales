cefcc3b7077bf30ebde860e712e24ef79a0d9e9d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:44:35 +0800

    New translations user_phpfusion.php (Hungarian)

--- Hungarian/locale/Hungarian/user_fields/user_phpfusion.php
+++ Hungarian/locale/Hungarian/user_fields/user_phpfusion.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_phpfusion'] = "PHPFusion";
-$locale['uf_phpfusion_id'] = "PHPFusion felhasználó név";
-$locale['uf_phpfusion_desc'] = "PHPFusion - Támogatási oldal hozzáférés";
-$locale['uf_phpfusion_error'] = "PHPFusion felhasználónév nem lehet üres.";
+$locale['uf_phpfusion_id'] = "PHPFusion Felhasználó Név";
+$locale['uf_phpfusion_desc'] = "PHPFusion Támogatási oldal hozzáférés";
+$locale['uf_phpfusion_error'] = "PHPFusion Felhasználó Név nem lehet üres.";
