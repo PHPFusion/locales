f8c83bbfd48a5b78bbd78b7eebaafb4bb5986b52
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:15 +0800

    New translations user_forum-stat.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/user_fields/user_forum-stat.php
+++ Bulgarian/locale/Bulgarian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Forum Posts";
+$locale['uf_forum-stat'] = "Отоговори във форума";
 $locale['uf_forum-stat_desc'] = "Показва брой отговори във форума.";
 $locale['uf_forum-stat_na'] = "Please install Forum infusion.";
