83396cc40b221b0dfa82fb7c5571905e4f02a959
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:39:56 +0800

    New translations members.php (Czech)

--- Czech/locale/Czech/search/members.php
+++ Czech/locale/Czech/search/members.php
@@ -1,5 +1,5 @@
 <?php
 $locale['m400'] = "Členové";
 $locale['m401'] = "Člen";
-$locale['m402'] = "Členové";
+$locale['m402'] = "členů";
 $locale['m403'] = "Profily jsou viditelné pouze pro členy!";
