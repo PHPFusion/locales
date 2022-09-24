c6f008d5f242157c52b5862e8747982d9fad53f7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:43 +0800

    New translations user_line.php (Malay)

--- Malay/locale/Malay/user_fields/user_line.php
+++ Malay/locale/Malay/user_fields/user_line.php
@@ -1,5 +1,5 @@
 <?php
 $locale['uf_line'] = "Line ID";
-$locale['uf_line_desc'] = "Line Instant Messenger";
-$locale['uf_line_error'] = "Line ID can not be left empty.";
+$locale['uf_line_desc'] = "IM Line";
+$locale['uf_line_error'] = "ID Line tidak boleh dibiarkan kosong.";
 $locale['uf_line_error_1'] = "Line ID is incorrect. Please use a valid Line ID";
