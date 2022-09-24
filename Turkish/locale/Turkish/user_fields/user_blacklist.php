d65c08f8f9b97f003b1af401cb2ea703157c6509
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:41:27 +0800

    New translations user_blacklist.php (Turkish)

--- Turkish/locale/Turkish/user_fields/user_blacklist.php
+++ Turkish/locale/Turkish/user_fields/user_blacklist.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_blacklist'] = "Kara Liste";
+$locale['uf_blacklist'] = "Karaliste";
 $locale['uf_blacklist_desc'] = "Üyelere Özel Gizli Kara liste";
 $locale['uf_blacklist_error'] = "Veri türü hatası";
 $locale['uf_blacklist_message'] = "Birisini engellediğinizde o kişi gönderdiğiniz şeyler de dahil olmak üzere artık sizi göremez ve sizinle iletişim kuramaz.";
