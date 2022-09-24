f97b78730b6743f7f132fac9719ce3bfffbb43bc
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:44:08 +0800

    New translations user_name_middle.php (Norwegian)

--- Norwegian/locale/Norwegian/user_fields/user_name_middle.php
+++ Norwegian/locale/Norwegian/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Mellomnavn";
+$locale['uf_name_middle'] = "Andre (mellom) navn";
 $locale['uf_name_middle_desc'] = "Brukers faktiske mellom navn.";
 $locale['uf_name_middle_error'] = "Andre (mellom) navn må skrives inn.";
