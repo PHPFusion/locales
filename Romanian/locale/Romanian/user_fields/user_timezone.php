eb05f19351485ee0379b3f73960b8a61bf83e847
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:46:06 +0800

    New translations user_timezone.php (Romanian)

--- Romanian/locale/Romanian/user_fields/user_timezone.php
+++ Romanian/locale/Romanian/user_fields/user_timezone.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_timezone'] = "Fus orar";
+$locale['uf_timezone'] = "Fus oras";
 $locale['uf_timezone_desc'] = "Fusul orar definit de utilizator.";
 $locale['uf_timezone_error'] = "Fusul orar nu a fost setat corect.";
