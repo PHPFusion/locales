2f9b0f173667e62dacd98c173447816d5195d725
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:34:50 +0800

    New translations code.php (Spanish)

--- Spanish/locale/Spanish/bbcodes/code.php
+++ Spanish/locale/Spanish/bbcodes/code.php
@@ -1,5 +1,5 @@
 <?php
 $locale['bb_code_description'] = "Muestra texto en código.";
 $locale['bb_code_usage'] = "Algo de Código.";
-$locale['bb_code_code'] = "Código";
+$locale['bb_code_code'] = "Código.";
 $locale['bb_code_save'] = "Descargar código fuente.";
