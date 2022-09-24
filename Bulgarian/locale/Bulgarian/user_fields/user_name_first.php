8803f7b732a938777ede05f0618a906dab1f9d3e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:22 +0800

    New translations user_name_first.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/user_fields/user_name_first.php
+++ Bulgarian/locale/Bulgarian/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_first'] = "Име";
+$locale['uf_name_first'] = "Собсвено име";
 $locale['uf_name_first_desc'] = "Истинско име на потребителя.";
 $locale['uf_name_first_error'] = "Полето за истинско име на потребителя не може да бъде оставено празно.";
