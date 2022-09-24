cced79a838c9cc2fecc61dea20535b17ada28387
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:57:08 +0800

    New translations photos.php (Romanian)

--- Romanian/infusions/gallery/locale/Romanian/search/photos.php
+++ Romanian/infusions/gallery/locale/Romanian/search/photos.php
@@ -1,9 +1,9 @@
 <?php
 $locale['photos.php'] = "Photo Gallery";
-$locale['p400'] = "Imagini &#238;n galerie";
+$locale['p400'] = "Imagini";
 $locale['p401'] = "Imagine";
-$locale['p402'] = "Imagini &#238;n galerie";
+$locale['p402'] = "Imagini";
 $locale['p403'] = "[NOU]";
 $locale['p404'] = "Album:";
 $locale['p405'] = "Data ad&#259;ug&#259;rii:";
-$locale['p406'] = "Views:";
+$locale['p406'] = "Vizualiz&#259;ri:";
