f9f2fb38b4690349094ce7fa4affecceb659a65e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:39 +0800

    New translations user_location.php (Russian)

--- Russian/locale/Russian/user_fields/user_location.php
+++ Russian/locale/Russian/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Расположение";
+$locale['uf_location'] = "Местонахождение пользователя";
 $locale['uf_location_desc'] = "Страна и населённый пункт";
 $locale['uf_location_error'] = "Местонахождение не может быть пустым.";
