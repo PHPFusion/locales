7bad39d3f5066a8c1d8b51348035c92e0a1d72d7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:32:39 +0800

    New translations sitelinks.php (Norwegian)

--- Norwegian/locale/Norwegian/admin/sitelinks.php
+++ Norwegian/locale/Norwegian/admin/sitelinks.php
@@ -1,6 +1,6 @@
 <?php
 // News titles
-$locale['SL_0001'] = "Interne lenker";
+$locale['SL_0001'] = "Sidens Lenker";
 $locale['SL_0010'] = "Legg Til Nettsteds Lenke";
 $locale['SL_0011'] = "Rediger Nettsteds Lenke";
 $locale['SL_0012'] = "Nåværende Nettsteds Lenker";
@@ -10,18 +10,18 @@ $locale['SL_0016'] = "Nettsteds Lenke er Oppdatert";
 $locale['SL_0017'] = "Nettsteds Lenke er Slettet";
 $locale['SL_0018'] = "Site Link Settings updated";
 // Site Links form
-$locale['SL_0020'] = "Link Name";
+$locale['SL_0020'] = "Lenkens Navn:";
 $locale['SL_0020a'] = "Link Icon";
 $locale['SL_0021'] = "Lenkens Nettadresse:";
 $locale['SL_0022'] = "Kan sees av:";
-$locale['SL_0023'] = "Order";
+$locale['SL_0023'] = "Rekkefølge:";
 $locale['SL_0024'] = "Posisjon:";
 $locale['SL_0025'] = "Kun i navigasjonspanel";
 $locale['SL_0026'] = "Både i Navigasjonspanel og menylinjen øverst";
 $locale['SL_0027'] = "Kun i menylinjen øverst";
 $locale['SL_0028'] = "Åpne lenken i nytt vindu";
-$locale['SL_0029'] = "Link Category";
-$locale['SL_0030'] = "Link Icon";
+$locale['SL_0029'] = "Lenke Kategori";
+$locale['SL_0030'] = "Lenke Ikon";
 $locale['SL_0031'] = "Link Status";
 $locale['SL_0032'] = "No Parent";
 $locale['SL_0033'] = "Link Language";
@@ -32,7 +32,7 @@ $locale['SL_0037'] = "Please select the link parent";
 $locale['SL_0038'] = "There are no links available that can be the link parent";
 $locale['SL_0039'] = "Confirm Move";
 
-$locale['SL_0040'] = "Save Link";
+$locale['SL_0040'] = "Lagre Lenke";
 $locale['SL_0041'] = "Link Settings";
 $locale['SL_0042'] = "Configure your site links navigation settings";
 $locale['SL_0043'] = "Links per page";
@@ -44,16 +44,16 @@ $locale['SL_0048'] = "No, do not group the links";
 $locale['SL_0049'] = "Yes, group the links as \"Show more\"";
 
 // Site Links list
-$locale['SL_0050'] = "Link Name";
-$locale['SL_0051'] = "Visibility";
-$locale['SL_0052'] = "Order";
-$locale['SL_0053'] = "Valg";
+$locale['SL_0050'] = "Lenkens Navn";
+$locale['SL_0051'] = "Kan sees av";
+$locale['SL_0052'] = "Rekkefølge";
+$locale['SL_0053'] = "Alternativer";
 
 $locale['SL_0062'] = "Ingen Nettsteds Lenker har blitt definert.";
 $locale['SL_0063'] = "Use BBCode in Link Name?";
 $locale['SL_0064'] = "Enabling this will reformat link name with BBcodes";
 
-$locale['SL_0070'] = "Icon";
+$locale['SL_0070'] = "Ikon";
 $locale['SL_0071'] = "Nytt Vindu?";
 $locale['SL_0072'] = "Meny";
 $locale['SL_0073'] = "Endre Rekkefølge";
