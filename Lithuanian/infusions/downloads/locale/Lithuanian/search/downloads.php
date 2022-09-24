85c2008375b37ef5abac3238d1b2bbb4a7175f94
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:54:43 +0800

    New translations downloads.php (Lithuanian)

--- Lithuanian/infusions/downloads/locale/Lithuanian/search/downloads.php
+++ Lithuanian/infusions/downloads/locale/Lithuanian/search/downloads.php
@@ -7,5 +7,5 @@ $locale['d403'] = "[NAUJAS]";
 $locale['d404'] = "Licenzija:";
 $locale['d405'] = "O/S:";
 $locale['d406'] = "Versija:";
-$locale['d407'] = "Date Added:";
+$locale['d407'] = "Įdėtas:";
 $locale['d408'] = "Atsisiuntimai:";
