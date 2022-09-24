a86231d0e68946fb9c2dcecca8a58a6c23483b10
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:59:24 +0800

    New translations news.php (Czech)

--- Czech/infusions/news/locale/Czech/news.php
+++ Czech/infusions/news/locale/Czech/news.php
@@ -1,16 +1,16 @@
 <?php
 $locale['news_0001'] = "Pokračovat ve čtení";
 $locale['news_0002'] = "Tisk";
-$locale['news_0004'] = "Zprávy";
-$locale['news_0005'] = "Žádné zprávy zatím nebyly zveřejněny";
-$locale['news_0006'] = "Nekategorizováno";
+$locale['news_0004'] = "Novinky";
+$locale['news_0005'] = "Žádné novinky zatím nebyly zveřejněny.";
+$locale['news_0006'] = "Nezařazeno";
 $locale['news_0008'] = "Naposledy aktualizováno";
 $locale['news_0009'] = "Kategorie novinek";
 $locale['news_0010'] = "Všechny ostatní kategorie";
-$locale['news_0011'] = "Nejnovější zprávy";
-$locale['news_0012'] = "Nejvíce komentované zprávy";
+$locale['news_0011'] = "Nejnovější";
+$locale['news_0012'] = "Nejvíce komentované";
 $locale['news_0013'] = "Nejlépe hodnocené";
-$locale['news_0014'] = "Náhled";
+$locale['news_0014'] = "Dlaždice";
 $locale['news_0015'] = "Seznam";
 $locale['news_0016'] = "Žádné kategorie novinek nebyly definovány.";
 $locale['news_0017'] = "Filtrovat podle";
