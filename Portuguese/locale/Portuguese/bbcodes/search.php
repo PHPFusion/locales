b04b7a69544418ee7a507c81aeec14fc4a32a070
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:37:23 +0800

    New translations search.php (Portuguese)

--- Portuguese/locale/Portuguese/bbcodes/search.php
+++ Portuguese/locale/Portuguese/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "A pesquisar por um dado texto no site actual";
 $locale['bb_search_usage'] = "Texto a pesquisar";
 $locale['bb_search_prefix'] = "Pesquisar por ";
-$locale['bb_search_suffix'] = " ligado ";
+$locale['bb_search_suffix'] = " em ";
 $locale['bb_search_where'] = "Onde";
