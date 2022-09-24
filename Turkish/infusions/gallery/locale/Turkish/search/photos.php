f70ed623166a1a171d007b3398b457327177533e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:58:19 +0800

    New translations photos.php (Turkish)

--- Turkish/infusions/gallery/locale/Turkish/search/photos.php
+++ Turkish/infusions/gallery/locale/Turkish/search/photos.php
@@ -1,9 +1,9 @@
 <?php
 $locale['photos.php'] = "Fotoğraf Albümü";
-$locale['p400'] = "Fotoğraflar";
+$locale['p400'] = "Resimler";
 $locale['p401'] = "Resim";
-$locale['p402'] = "Fotoğraflar";
+$locale['p402'] = "Resim";
 $locale['p403'] = "[YENİ]";
 $locale['p404'] = "Albüm:";
 $locale['p405'] = "Eklenme Tarihi:";
-$locale['p406'] = "Görüntülenme:";
+$locale['p406'] = "Gösterim:";
