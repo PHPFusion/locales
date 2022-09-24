dba5a6292c224c89a57db4488cc6914da4b246fa
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:56:07 +0800

    New translations faqs.php (Slovak)

--- Slovak/infusions/faq/locale/Slovak/search/faqs.php
+++ Slovak/infusions/faq/locale/Slovak/search/faqs.php
@@ -2,7 +2,7 @@
 $locale['faqs.php'] = "FAQ";
 $locale['fq400'] = "FAQ";
 $locale['fq401'] = "FAQ";
-$locale['fq402'] = "FAQ";
+$locale['fq402'] = "otázok";
 $locale['fq403'] = "nájdené v";
 $locale['fq404'] = "FAQ otázka";
 $locale['fq405'] = "FAQ odpoveď";
