1cac000c816c53d3d6c52e98b74efe713e99f255
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:53:09 +0800

    New translations blog.php (Belarusian)

--- Belarusian/infusions/blog/locale/Belarusian/search/blog.php
+++ Belarusian/infusions/blog/locale/Belarusian/search/blog.php
@@ -1,9 +1,9 @@
 <?php
 $locale['blog.php'] = "Blog";
 $locale['b400'] = "Blog";
-$locale['b401'] = "Blog item";
-$locale['b402'] = "Blog items";
+$locale['b401'] = "запіс блога";
+$locale['b402'] = "запіс(-ы, -аў) блога";
 $locale['b403'] = "found in";
-$locale['b404'] = "Blog subject";
-$locale['b405'] = "Blog text";
-$locale['b406'] = "extended Blog text";
+$locale['b404'] = "тэме запісу";
+$locale['b405'] = "тэксце запісу";
+$locale['b406'] = "поўным тэксце запісу";
