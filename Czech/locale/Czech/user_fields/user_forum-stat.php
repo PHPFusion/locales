6743519784ec101c844207a068c38014c427779a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:16 +0800

    New translations user_forum-stat.php (Czech)

--- Czech/locale/Czech/user_fields/user_forum-stat.php
+++ Czech/locale/Czech/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Příspěvky fóra";
+$locale['uf_forum-stat'] = "Příspěvky ve fóru";
 $locale['uf_forum-stat_desc'] = "Zobrazí počet příspěvků ve fóru.";
 $locale['uf_forum-stat_na'] = "Prosím, nainstalujte fórum.";
