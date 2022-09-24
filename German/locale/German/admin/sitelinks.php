50b19126714474a67883634852e862c12c0d07b9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:32:29 +0800

    New translations sitelinks.php (German)

--- German/locale/German/admin/sitelinks.php
+++ German/locale/German/admin/sitelinks.php
@@ -1,40 +1,40 @@
 <?php
 // News titles
-$locale['SL_0001'] = "Navigationslinks";
-$locale['SL_0010'] = "Navigationslink hinzufügen";
-$locale['SL_0011'] = "Navigationslink bearbeiten";
-$locale['SL_0012'] = "Aktuell vorhandene Navigationslinks";
+$locale['SL_0001'] = "Seiten links";
+$locale['SL_0010'] = "Seiten link hinzufügen";
+$locale['SL_0011'] = "Seiten link bearbeiten";
+$locale['SL_0012'] = "Aktuelle Seiten-Links";
 // News messages
-$locale['SL_0015'] = "Der Navigationslink wurde erfolgreich hinzugefügt.";
-$locale['SL_0016'] = "Der Navigationslink wurde erfolgreich aktualisiert.";
-$locale['SL_0017'] = "Der Navigationslink wurde erfolgreich entfernt.";
-$locale['SL_0018'] = "Der Navigationslink wurde erfolgreich bearbeitet.";
+$locale['SL_0015'] = "Seiten link hinzugefügt";
+$locale['SL_0016'] = "Seiten-Links aktualisiert";
+$locale['SL_0017'] = "Seiten link gelöscht";
+$locale['SL_0018'] = "Seiten link-Einstellungen aktualisiert";
 // Site Links form
 $locale['SL_0020'] = "Link Name:";
 $locale['SL_0020a'] = "Link-Symbol";
 $locale['SL_0021'] = "Link URL:";
-$locale['SL_0022'] = "Sichtbarkeit:";
+$locale['SL_0022'] = "Link Sichtbarkeit";
 $locale['SL_0023'] = "Reihenfolge:";
 $locale['SL_0024'] = "Position:";
 $locale['SL_0025'] = "Nur im Navigationspanel";
 $locale['SL_0026'] = "Navigationspanel und in der Headernavigation";
 $locale['SL_0027'] = "Nur in der Headernavigation";
-$locale['SL_0028'] = "Link in einem neuen Fenster &ouml;ffnen.";
-$locale['SL_0029'] = "Linkkategorie";
-$locale['SL_0030'] = "Link-Icon";
+$locale['SL_0028'] = "Link in neuem Fenster öffnen";
+$locale['SL_0029'] = "Link Kategorie";
+$locale['SL_0030'] = "Link Symbol";
 $locale['SL_0031'] = "Link Status";
 $locale['SL_0032'] = "Keine Hauptkategorie";
 $locale['SL_0033'] = "Link Sprache";
-$locale['SL_0034'] = "Gesichert";
+$locale['SL_0034'] = "Geschützt";
 $locale['SL_0035'] = "Unterlink";
-$locale['SL_0036'] = "Verschiebe Link zu...";
-$locale['SL_0037'] = "Bitte wähle eine Kategorie";
-$locale['SL_0038'] = "Es sind keine Links vorhanden, zu denen dieser eingeordnet werden kann";
-$locale['SL_0039'] = "Bestätige das Verschieben";
+$locale['SL_0036'] = "Seiten links verschieben nach...";
+$locale['SL_0037'] = "Bitte wählen Sie den übergeordneten Link aus";
+$locale['SL_0038'] = "Es sind keine Links verfügbar, die der übergeordnete Link sein können";
+$locale['SL_0039'] = "Verschieben bestätigen";
 
 $locale['SL_0040'] = "Link speichern";
 $locale['SL_0041'] = "Link Einstellungen";
-$locale['SL_0042'] = "Konfiguriere deine Navigationseinstellungen";
+$locale['SL_0042'] = "Konfigurieren Sie die Navigation Einstellungen für Seiten-Links";
 $locale['SL_0043'] = "Links pro Seite";
 $locale['SL_0044'] = "Maximale Anzahl der Links in der Navigationsbar";
 $locale['SL_0045'] = "Anzahl der Links";
@@ -47,22 +47,22 @@ $locale['SL_0049'] = "Ja, gruppiere die Links als \"Zeige mehr\"";
 $locale['SL_0050'] = "Link Name";
 $locale['SL_0051'] = "Sichtbarkeit";
 $locale['SL_0052'] = "Reihenfolge";
-$locale['SL_0053'] = "Optionen";
+$locale['SL_0053'] = "Einstellungen";
 
-$locale['SL_0062'] = "Derzeit wurden noch keine Navigationslinks hinzugef&uuml;gt.";
+$locale['SL_0062'] = "Es wurden keine Seiten-Links angelegt.";
 $locale['SL_0063'] = "BBCode im Linknamen verwenden?";
 $locale['SL_0064'] = "Wenn Sie dies aktivieren, wird der Linkname mit BBcodes neu formatiert";
 
 $locale['SL_0070'] = "Symbol";
 $locale['SL_0071'] = "Neues Fenster?";
-$locale['SL_0072'] = "Men&uuml;";
+$locale['SL_0072'] = "Menü";
 $locale['SL_0073'] = "Sortierung erneuern";
 $locale['SL_0074'] = "Verschiebe";
-$locale['SL_0075'] = "Zeige Optionen";
+$locale['SL_0075'] = "Zeige Einstellungen";
 
 // Delete Site Link category
-$locale['SL_0080'] = "Bist du dir sicher, dass du den Navigationslink entfernen m&ouml;chtest?";
+$locale['SL_0080'] = "Diesen Seiten Link löschen?";
 // Error
-$locale['SL_0085'] = "Du musst einen Namen f&uuml;r den Navigationslink angeben.";
-$locale['SL_0086'] = "Du musst eine Adresse f&uuml;r den Navigationslink angeben.";
-$locale['SL_0087'] = "Es ist nichts ausgewählt. Bitte wähle einen Link aus.";
+$locale['SL_0085'] = "Bitte geben Sie einen Link namen ein";
+$locale['SL_0086'] = "Bitte geben Sie eine Link-URL ein";
+$locale['SL_0087'] = "Es gibt kein zu aktualisierendes Element. Bitte überprüfen Sie einen Seiten-Link.";
