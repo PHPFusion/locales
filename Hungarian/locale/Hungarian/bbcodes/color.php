ba6dae9ebd5ea733da7a216359350fe231ceab4a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:34:59 +0800

    New translations color.php (Hungarian)

--- Hungarian/locale/Hungarian/bbcodes/color.php
+++ Hungarian/locale/Hungarian/bbcodes/color.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_color_description'] = "Kijelölt szöveg színezése";
-$locale['bb_color_usage'] = "Kijelölt szöveg színezése";
+$locale['bb_color_usage'] = "Szöveg";
 $locale['bb_color_hex'] = "123456";
