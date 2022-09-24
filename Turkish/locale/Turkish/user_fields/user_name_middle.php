32ab250c098b0d4e1824444562341c342c41c369
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:44:19 +0800

    New translations user_name_middle.php (Turkish)

--- Turkish/locale/Turkish/user_fields/user_name_middle.php
+++ Turkish/locale/Turkish/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Göbek Adınız";
+$locale['uf_name_middle'] = "Göbek Adı";
 $locale['uf_name_middle_desc'] = "Kullanıcı gerçek göbek adı veya ikinci adı.";
 $locale['uf_name_middle_error'] = "İkinci (göbek) ad boş bırakılamaz.";
