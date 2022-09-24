4fe78f9fbac32309e691bb45a747fde2df325acc
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:00:45 +0800

    New translations weblinks.php (Czech)

--- Czech/infusions/weblinks/locale/Czech/search/weblinks.php
+++ Czech/infusions/weblinks/locale/Czech/search/weblinks.php
@@ -1,8 +1,8 @@
 <?php
 $locale['weblinks.php'] = "Webové odkazy";
-$locale['w400'] = "Webové odkazy";
+$locale['w400'] = "Odkazy";
 $locale['w401'] = "Odkaz";
-$locale['w402'] = "Webové odkazy";
+$locale['w402'] = "webových odkazů";
 $locale['w403'] = "[NOVÉ]";
 $locale['w404'] = "Přidáno:";
 $locale['w405'] = "Zobrazeni:";
