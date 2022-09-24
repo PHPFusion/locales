649bef81ad8dede6372b5dec7fcb30e92f642fc4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:58:59 +0800

    New translations poll.php (Turkish)

--- Turkish/infusions/member_poll_panel/locale/Turkish/permalinks/poll.php
+++ Turkish/infusions/member_poll_panel/locale/Turkish/permalinks/poll.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_poll_title'] = "Üyeler Anketi";
+$locale['pl_poll_title'] = "Kullanıcı Anketleri";
 $locale['pl_poll_desc'] = "Kullanıcı Anketleri İçin Kalıcı Bağlantıları Etkinleştir.";
