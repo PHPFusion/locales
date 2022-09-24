d417bed821626864e528dba8673780bf12a317b4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:08 +0800

    New translations lostpassword.php (Norwegian)

--- Norwegian/locale/Norwegian/lostpassword.php
+++ Norwegian/locale/Norwegian/lostpassword.php
@@ -5,7 +5,7 @@ $locale['402'] = "Ditt nye passord er sendt til din e-postadresse.";
 $locale['403'] = "Tilbake til hovedsiden";
 $locale['404'] = "E-post adressen du oppga ble ikke funnet.";
 $locale['405'] = "E-post adressen du oppga har ugyldig format.";
-$locale['406'] = "Please try again.";
+$locale['406'] = "Vennligst forsøk igjen.";
 $locale['407'] = "Vennligst oppgi e-post adressen for brukerkontoen din i feltet under.
 Et nytt passord vil bli opprettet og sendt til deg via denne e-post adressen.";
 $locale['408'] = "Send Passord";
