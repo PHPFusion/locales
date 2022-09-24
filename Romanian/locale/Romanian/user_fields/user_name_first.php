601568e996f2971815ab96e87cd0a825ea9cc517
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:13 +0800

    New translations user_name_first.php (Romanian)

--- Romanian/locale/Romanian/user_fields/user_name_first.php
+++ Romanian/locale/Romanian/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_first'] = "Prenume";
+$locale['uf_name_first'] = "Prenumele utilizatorului";
 $locale['uf_name_first_desc'] = "Prenumele utilizatorului.";
 $locale['uf_name_first_error'] = "Prenumele utilizatorului nu poate fi l&#259;sat gol.";
