2e5b75f4e8559b0577ad2f6fae7bd6e715b908b0
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:25 +0800

    New translations user_location.php (Czech)

--- Czech/locale/Czech/user_fields/user_location.php
+++ Czech/locale/Czech/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Umístění";
+$locale['uf_location'] = "Bydliště";
 $locale['uf_location_desc'] = "Kraj, město a nebo vesnice";
 $locale['uf_location_error'] = "Bydliště nesmí být prázdné.";
