a23a5f410181392c59d7b897d574fc2f52f88c2e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:55:12 +0800

    New translations faqs.php (Czech)

--- Czech/infusions/faq/locale/Czech/search/faqs.php
+++ Czech/infusions/faq/locale/Czech/search/faqs.php
@@ -2,7 +2,7 @@
 $locale['faqs.php'] = "FAQ";
 $locale['fq400'] = "FAQ";
 $locale['fq401'] = "FAQ";
-$locale['fq402'] = "FAQ";
+$locale['fq402'] = "otázek";
 $locale['fq403'] = "nalezeno v";
 $locale['fq404'] = "FAQ otázka";
 $locale['fq405'] = "FAQ odpověď";
