2ce1eda011d2af4f44be70e49ff2fa7a3750e5b7
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:56:08 +0800

    New translations forums.php (Slovak)

--- Slovak/infusions/forum/locale/Slovak/permalinks/forums.php
+++ Slovak/infusions/forum/locale/Slovak/permalinks/forums.php
@@ -1,5 +1,5 @@
 <?php
-$locale['pl_forums_title'] = "Fórum";
+$locale['pl_forums_title'] = "Fóra";
 $locale['pl_forums_desc'] = "Povoliť trvalé odkazy pre fórum.";
 $locale['pl_tags_001'] = "ID fóra pre konkrétne fórum.";
 $locale['pl_tags_002'] = "Názov konkrétneho fóra.";
