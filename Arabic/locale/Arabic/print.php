a419bbe6b37a1545176ebce88101eb3fb099d02e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:32 +0800

    New translations print.php (Arabic)

--- Arabic/locale/Arabic/print.php
+++ Arabic/locale/Arabic/print.php
@@ -1,12 +1,12 @@
 <?php
 //news, articles, blog
-$locale['400'] = "أرسل بواسطة ";
+$locale['400'] = "نشر من قبل ";
 $locale['401'] = " في ";
 $locale['402'] = "التفاصيل";
 $locale['403'] = "التفاصيل";
 //forums
 $locale['500'] = "عنوان الموضوع:";
-$locale['501'] = "أرسل بواسطة ";
+$locale['501'] = "نشر من قبل ";
 $locale['502'] = " في ";
 $locale['503'] = "تم التعديل بواسطة ";
 //License Agreement
