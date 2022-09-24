50c2c67ae72ee2d09585f05ad2787359eb113915
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:55:48 +0800

    New translations forum.php (Spanish)

--- Spanish/infusions/forum/locale/Spanish/search/forum.php
+++ Spanish/infusions/forum/locale/Spanish/search/forum.php
@@ -1,9 +1,9 @@
 <?php
 $locale['forum.php'] = "Foros";
-$locale['f400'] = "Forum Posts";
+$locale['f400'] = "Mensajes del Foro";
 $locale['f401'] = "Todos los Foros";
 $locale['f402'] = "Mensaje del Foro";
-$locale['f403'] = "Forum Posts";
+$locale['f403'] = "Mensajes del Foro";
 $locale['f404'] = "Fijado:";
 $locale['f405'] = "Anuncio:";
 $locale['f406'] = "encontrado(s) en el";
