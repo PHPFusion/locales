01d1cb918f474ebd477ea9dcee524b19e105ab60
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:32:30 +0800

    New translations smileys.php (German)

--- German/locale/German/admin/smileys.php
+++ German/locale/German/admin/smileys.php
@@ -1,38 +1,38 @@
 <?php
 // Smiley titles
-$locale['SMLY_400'] = "Aktuell vorhandene Smileys";
-$locale['SMLY_401'] = "Smiley hinzuf&uuml;gen";
+$locale['SMLY_400'] = "Aktuelle Smileys";
+$locale['SMLY_401'] = "Smiley hinzufügen";
 $locale['SMLY_402'] = "Smiley bearbeiten";
 $locale['SMLY_403'] = "Smileys";
 $locale['SMLY_404'] = "Aktive Smileys";
-$locale['SMLY_405'] = "Inaktive Smileys";
+$locale['SMLY_405'] = "Deaktivierte Smileys";
 // Smiley messages
-$locale['SMLY_410'] = "Das Smiley wurde erfolgreich hinzugef&uuml;gt.";
-$locale['SMLY_411'] = "Das Smiley wurde erfolgreich aktualisiert.";
-$locale['SMLY_412'] = "Das Smiley wurde erfolgreich entfernt.";
-$locale['SMLY_413'] = "Das Smiley konnte nicht aktualisiert werden.";
+$locale['SMLY_410'] = "Smiley hinzugefügt";
+$locale['SMLY_411'] = "Smiley aktualisiert";
+$locale['SMLY_412'] = "Smiley gelöscht";
+$locale['SMLY_413'] = "Smiley deaktiviert";
 
-$locale['SMLY_414'] = "Das Smiley konnte nicht hinzugef&uuml;gt werden.";
-$locale['SMLY_415'] = "Das Smileyk&uuml;rzel ist bereits in Verwendung.";
-$locale['SMLY_416'] = "Bist du dir sicher, dass du das Smiley entfernen m&ouml;chtest?";
+$locale['SMLY_414'] = "Der Smiley-Text wird verwendet.";
+$locale['SMLY_415'] = "Der Smiley-Code wird verwendet.";
+$locale['SMLY_416'] = "Möchten Sie diesen Smiley wirklich löschen?";
 $locale['SMLY_417'] = "Möchten Sie diesen Smiley wirklich deaktivieren?";
 $locale['SMLY_418'] = "Fehler beim Hochladen des Smiley";
 // Smiley form
-$locale['SMLY_420'] = "K&uuml;rzel:";
-$locale['SMLY_421'] = "Bild:";
-$locale['SMLY_422'] = "Text:";
+$locale['SMLY_420'] = "Smiley kürzel code:";
+$locale['SMLY_421'] = "Smiley Bild:";
+$locale['SMLY_422'] = "Smiley Text:";
 $locale['SMLY_423'] = "Smiley speichern";
-$locale['SMLY_424'] = "Vorschau:";
+$locale['SMLY_424'] = "Smiley aktualisieren";
 // Current Smileys
-$locale['SMLY_430'] = "K&uuml;rzel";
+$locale['SMLY_430'] = "Code kürzel";
 $locale['SMLY_431'] = "Bild";
 $locale['SMLY_432'] = "Text";
-$locale['SMLY_433'] = "Optionen";
-$locale['SMLY_435'] = "L&ouml;schen";
-$locale['SMLY_436'] = "Derzeit wurden noch keine Smileys hinzugef&uuml;gt.";
+$locale['SMLY_433'] = "Einstellungen";
+$locale['SMLY_435'] = "Inaktiv";
+$locale['SMLY_436'] = "Es sind keine Smileys angelegt.";
 // Error Message
-$locale['SMLY_437'] = "Du musst ein K&uuml;rzel f&uuml;r das Smiley angeben.";
-$locale['SMLY_438'] = "Du musst ein Bild f&uuml;r das Smiley ausw&auml;hlen.";
-$locale['SMLY_439'] = "Du musst einen Text f&uuml;r das Smiley angeben.";
+$locale['SMLY_437'] = "Bitte geben Sie einen Smiley-Code ein";
+$locale['SMLY_438'] = "Bitte wählen Sie einen Smiley";
+$locale['SMLY_439'] = "Bitte geben Sie einen Smiley-Text ein";
 $locale['SMLY_440'] = "Keine aktiven Smileys";
 $locale['SMLY_441'] = "Keine inaktiven Smileys";
