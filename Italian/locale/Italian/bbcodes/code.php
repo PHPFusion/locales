c4eb87f4071b965d997fbec6dd357e2f60c7ef21
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:35:03 +0800

    New translations code.php (Italian)

--- Italian/locale/Italian/bbcodes/code.php
+++ Italian/locale/Italian/bbcodes/code.php
@@ -1,5 +1,5 @@
 <?php
 $locale['bb_code_description'] = "Mostra testo come codice";
 $locale['bb_code_usage'] = "Codice";
-$locale['bb_code_code'] = "Code";
+$locale['bb_code_code'] = "Codice";
 $locale['bb_code_save'] = "Scarica sorgente";
