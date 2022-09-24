43c644bc4e497949e1391e5eb90c0eb2beedf635
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:09 +0800

    New translations homepage.php (Norwegian)

--- Norwegian/locale/Norwegian/homepage.php
+++ Norwegian/locale/Norwegian/homepage.php
@@ -1,9 +1,9 @@
 <?php
 // block titles
 $locale['home_0000'] = "Siste Nytt";
-$locale['home_0001'] = "Siste artikler";
+$locale['home_0001'] = "Siste Artikler";
 $locale['home_0002'] = "Siste Blogg";
-$locale['home_0003'] = "Siste Nedlastinger";
+$locale['home_0003'] = "Siste Nedlasting";
 // no content messages
 $locale['home_0050'] = "Ingen Nyheter";
 $locale['home_0051'] = "Ingen Artikler";
@@ -12,6 +12,6 @@ $locale['home_0053'] = "Ingen Nedlastinger";
 // common text
 $locale['home_0100'] = "Innholdsmoduler er ikke innstallert";
 $locale['home_0101'] = "Vennligst innstaller minst en modul for tekstinnhold eller endre startside i Hovedinnstillinger.";
-$locale['home_0102'] = "Ukategoriserte";
+$locale['home_0102'] = "Ikke kategorisert";
 $locale['home_0105'] = "Publisert av";
 $locale['home_0106'] = " i ";
