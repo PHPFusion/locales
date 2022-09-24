b7d13dd7dcc56e24dc7d216d80dc6bb96aa6e212
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:37:06 +0800

    New translations search.php (Bulgarian)

--- Bulgarian/locale/Bulgarian/bbcodes/search.php
+++ Bulgarian/locale/Bulgarian/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Търсене за даден текст в текущия сайт";
 $locale['bb_search_usage'] = "Текст за търсене";
 $locale['bb_search_prefix'] = "Търсене за ";
-$locale['bb_search_suffix'] = " на ";
+$locale['bb_search_suffix'] = " в ";
 $locale['bb_search_where'] = "Къде";
