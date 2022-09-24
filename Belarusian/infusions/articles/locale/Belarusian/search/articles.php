20533a387a1157fcf79469e6aa88a2a3974af282
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:52:34 +0800

    New translations articles.php (Belarusian)

--- Belarusian/infusions/articles/locale/Belarusian/search/articles.php
+++ Belarusian/infusions/articles/locale/Belarusian/search/articles.php
@@ -1,8 +1,8 @@
 <?php
 $locale['articles.php'] = "Articles";
 $locale['a400'] = "Articles";
-$locale['a401'] = "Article";
+$locale['a401'] = "Артыкул";
 $locale['a402'] = "Articles";
 $locale['a403'] = "found in";
-$locale['a404'] = "article subject";
-$locale['a405'] = "article text";
+$locale['a404'] = "у тэме артыкула";
+$locale['a405'] = "у тэксце артыкула";
