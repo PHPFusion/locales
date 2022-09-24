e30ee8f39c83a8c2824952ee956e98b7dab189fc
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:55:56 +0800

    New translations forum.php (Italian)

--- Italian/infusions/forum/locale/Italian/search/forum.php
+++ Italian/infusions/forum/locale/Italian/search/forum.php
@@ -1,11 +1,11 @@
 <?php
 $locale['forum.php'] = "Forums";
-$locale['f400'] = "Forum Posts";
+$locale['f400'] = "Messaggi forum";
 $locale['f401'] = "tutti i forum";
 $locale['f402'] = "Messaggio forum";
-$locale['f403'] = "Forum Posts";
+$locale['f403'] = "Messaggi forum";
 $locale['f404'] = "Importante:";
 $locale['f405'] = "Annuncio:";
-$locale['f406'] = "Trovata in";
+$locale['f406'] = "Trovato in";
 $locale['f407'] = "oggetto messaggio";
 $locale['f408'] = "contenuto messaggio";
