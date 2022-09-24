7f545a53243a9322c120afe5a7199427f3361293
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:30 +0800

    New translations user_name_first.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/user_fields/user_name_first.php
+++ Lithuanian/locale/Lithuanian/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_first'] = "Vardas";
+$locale['uf_name_first'] = "Nario vardas";
 $locale['uf_name_first_desc'] = "Tikrasis nario vardas (Jonas, Marytė ir t.t.).";
 $locale['uf_name_first_error'] = "Nario vardo laukelis negali būti tuščias.";
