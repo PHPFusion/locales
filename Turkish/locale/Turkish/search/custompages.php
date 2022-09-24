e696889e9b58a5c5d55eb91ed3cd3d7e839dfd8e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:40:18 +0800

    New translations custompages.php (Turkish)

--- Turkish/locale/Turkish/search/custompages.php
+++ Turkish/locale/Turkish/search/custompages.php
@@ -1,7 +1,7 @@
 <?php
 $locale['c400'] = "Özel Sayfalar";
 $locale['c401'] = "Özel Sayfa";
-$locale['c402'] = "Özel Sayfalar";
+$locale['c402'] = "Özel Sayfa";
 $locale['c403'] = "içinde bulundu";
 $locale['c404'] = "Sayfa Başlığında Bulundu";
 $locale['c405'] = "Sayfa Içeriğinde Bulundu";
