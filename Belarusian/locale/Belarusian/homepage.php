26cfe78170b4467d108f295a0079f7c53decaa2d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:49:50 +0800

    New translations homepage.php (Belarusian)

--- Belarusian/locale/Belarusian/homepage.php
+++ Belarusian/locale/Belarusian/homepage.php
@@ -1,17 +1,17 @@
 <?php
 // block titles
-$locale['home_0000'] = "Latest News";
-$locale['home_0001'] = "Latest Articles";
-$locale['home_0002'] = "Latest Blogs";
-$locale['home_0003'] = "Latest Downloads";
+$locale['home_0000'] = "Апошнія навіны";
+$locale['home_0001'] = "Апошнія артыкулы";
+$locale['home_0002'] = "Апошнія запісы блога";
+$locale['home_0003'] = "Апошнія загрузкі";
 // no content messages
-$locale['home_0050'] = "No News to display";
-$locale['home_0051'] = "No Articles to display";
-$locale['home_0052'] = "No Blogs to display";
-$locale['home_0053'] = "No Downloads to display";
+$locale['home_0050'] = "Навін няма";
+$locale['home_0051'] = "Артыкулаў няма";
+$locale['home_0052'] = "Запісаў няма";
+$locale['home_0053'] = "Загрузак няма";
 // common text
-$locale['home_0100'] = "Content modules not installed";
-$locale['home_0101'] = "Please install at least one module for text content or change start page in Main Settings.";
+$locale['home_0100'] = "Модулі змесціва не ўсталяваны";
+$locale['home_0101'] = "Усталюйце хаця б адзін модуль для тэкставага змесціва ці змяніце стартавую старонку ў асноўных наладах.";
 $locale['home_0102'] = "Uncategorized";
-$locale['home_0105'] = "Published by ";
-$locale['home_0106'] = " in ";
+$locale['home_0105'] = "Апублікаваў(-ла) ";
+$locale['home_0106'] = " у катэгорыю ";
