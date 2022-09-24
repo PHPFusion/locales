4ee3304f06780bda74f79f837c3349b9b2e2c586
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:48:41 +0800

    New translations error.php (German)

--- German/locale/German/error.php
+++ German/locale/German/error.php
@@ -1,7 +1,7 @@
 <?php
-$locale['err401'] = "Der Zugriff auf diese Seite ist nicht authentifiziert!";
-$locale['err403'] = "Der Zugriff auf diese Seite ist nicht erlaubt!";
-$locale['err404'] = "Die gew&uuml;schte Seite wurde auf dem Server nicht gefunden!";
-$locale['errunk'] = "Es ist ein unbekannter Fehler aufgetreten!";
-$locale['errmsg'] = "Dem Link, dem du gefolgt bist, ist entweder nicht mehr vorhanden oder du hast kein Recht diese Seite zu sehen.";
-$locale['errret'] = "Zur&uuml;ck zur Startseite";
+$locale['err401'] = "Nicht autorisierter Zugriff auf diese Seite";
+$locale['err403'] = "Zugriff auf diese Seite verboten";
+$locale['err404'] = "Diese Seite ist auf diesem Server leider nicht verfügbar";
+$locale['errunk'] = "Unbekannter Fehler";
+$locale['errmsg'] = "Der Link, dem Sie gefolgt sind, ist möglicherweise abgelaufen oder die Seite ist für Sie möglicherweise nicht zugänglich.";
+$locale['errret'] = "Zurück zur Hauptseite";
