122855a56df3811f3d062c15990ed43cd93b058d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:49:57 +0800

    New translations lostpassword.php (German)

--- German/locale/German/lostpassword.php
+++ German/locale/German/lostpassword.php
@@ -1,20 +1,17 @@
 <?php
 $locale['400'] = "Passwort vergessen";
-$locale['401'] = "Eine Nachricht mit einem Best&auml;tigungslink wurde an deine E-Mail Adresse gesendet.";
-$locale['402'] = "Eine Nachricht mit deinem neuen Passwort wurde an deine E-Mail Adresse gesendet.";
-$locale['403'] = "Zur&uuml;ck zur Startseite";
+$locale['401'] = "Eine Bestätigungs-E-Mail wurde an Ihre E-Mail-Adresse gesendet.";
+$locale['402'] = "Ihr neues Passwort wurde an Ihre E-Mail-Adresse gesendet.";
+$locale['403'] = "Zurück zur Startseite";
 $locale['404'] = "Die von dir angegebene E-Mail Adresse konnte nicht gefunden werden.";
-$locale['405'] = "Die von dir angegebene E-Mail Adresse ist nicht g&uuml;ltig.";
-$locale['406'] = "Bitte versuche es nochmals.";
-$locale['407'] = "Du musst im nachfolgenden Formular die E-Mail Adresse angeben, mit welcher der Account erstellt wurde.
-";
-$locale['408'] = "Passwort zusenden";
-$locale['409'] = "Neues Passwort auf ";
-$locale['410'] = "Hallo [USER_NAME],
-
-";
-$locale['411'] = "Hallo [USER_NAME],
-
-";
-$locale['412'] = "Leider ist ein Fehler aufgetreten. Bitte versuche es nochmals.";
+$locale['405'] = "Die von Ihnen angegebene E-Mail-Adresse ist ungültig.";
+$locale['406'] = "Bitte versuche es erneut.";
+$locale['407'] = "Bitte geben Sie unten die E-Mail-Adresse ein, mit der Ihr Konto registriert ist.<br/>Ein neues Passwort wird erstellt und an diese Adresse gesendet.";
+$locale['408'] = "Passwort senden";
+$locale['409'] = "Neues Passwort für ";
+$locale['410'] = "Hallo [USER_NAME],<br/>Sie haben oder jemand hat ein neues Passwort angefordert, um auf Ihr [SITENAME]-Konto zuzugreifen.<br/>
+Um Ihr Passwort zu ändern, klicken Sie bitte auf den folgenden Link:<br/>[NEW_PASS_LINK]<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
+$locale['411'] = "Hallo [USER_NAME],<br/>Ihr neues Passwort für den Zugriff auf Ihr [SITENAME]-Konto lautet:<br/>
+[NEW_PASS]<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
+$locale['412'] = "Es ist ein Fehler aufgetreten. Bitte versuchen Sie es erneut.";
 $locale['413'] = "Bitte geben Sie Ihre E-mail Adresse ein:";
