52571c72733e5d3e612b1430304e545b27eef0d9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:39 +0800

    New translations user_forum-stat.php (Ukrainian)

--- Ukrainian/locale/Ukrainian/user_fields/user_forum-stat.php
+++ Ukrainian/locale/Ukrainian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Forum Posts";
+$locale['uf_forum-stat'] = "Повідомлення на форумі";
 $locale['uf_forum-stat_desc'] = "Кількість повідомлень на форумі";
 $locale['uf_forum-stat_na'] = "Please install Forum infusion.";
