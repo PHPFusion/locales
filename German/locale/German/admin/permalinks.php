ac22ffe7d243cbf88099efdda95e1638d5e1e240
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:31:44 +0800

    New translations permalinks.php (German)

--- German/locale/German/admin/permalinks.php
+++ German/locale/German/admin/permalinks.php
@@ -5,16 +5,16 @@ $locale['PL_401'] = "Deaktivierte Permalinks";
 $locale['PL_401a'] = "Permalink Einstellungen";
 
 // Common
-$locale['PL_402'] = "Umschreibmodus";
+$locale['PL_402'] = "Name umschreiben";
 $locale['PL_403'] = "Beschreibung";
-$locale['PL_404'] = "Optionen";
+$locale['PL_404'] = "Einstellungen";
 $locale['PL_404a'] = "Aktivieren";
 $locale['PL_404b'] = "Deaktivieren";
 $locale['PL_404d'] = "Neu installieren";
 
 // Editing Permalinks
 $locale['PL_405'] = "Permalinks bearbeiten für %s";
-$locale['PL_406'] = "Stichwörter";
+$locale['PL_406'] = "Stichworte";
 $locale['PL_407'] = "Beschreibung";
 $locale['PL_408'] = "Typen";
 $locale['PL_409'] = "Muster bearbeiten";
@@ -22,10 +22,10 @@ $locale['PL_410'] = "Permalink type %u:";
 $locale['PL_411'] = "Fehler";
 $locale['PL_412'] = "Überprüfen Sie die Dateien von Permalink %s!";
 $locale['PL_413'] = "Speichern";
-$locale['PL_414'] = "WARNUNG: Permalinks &amp; Benutzerdefinierte Alias-Daten werden dauerhaft gelöscht!<br/>Sind Sie sicher, dass Sie diese Permalink deaktivieren möchten?";
-$locale['PL_415'] = "Permalink Regeln transformieren verschiedene URLs in Suchmaschinen-freundliche URLs mit Optionen, um die URL-Struktur für Ihre Website anzupassen.<br/>
-                  Sie können die Permalinks in der Registerkarte Einstellungen aktivieren, wenn Ihr Server mod_rewrite unterstützt. Du aktivierst die verschiedenen Permalinks im Reiter \"Deaktiviert\". Sie können die Regeln bearbeiten, um eine URL direkt in jedem Permalink-Modul Ihrer Präferenz anzupassen.<br/><br/>
-                  Bitte beachten Sie, dass mod_rewrite oder sein Äquivalent benötigt wird, damit Permalinks funktioniert.";
+$locale['PL_414'] = "ACHTUNG: Permalinks &amp; Benutzerdefinierte Alias-Daten werden dauerhaft gelöscht!<br/>Möchten Sie diesen Permalink wirklich deaktivieren?";
+$locale['PL_415'] = "Permalink-Regeln verwandeln verschiedene Website-URLs in suchmaschinenfreundliche URLs mit Optionen zum Anpassen der URL-Struktur für Ihre Website.<br/>
+                  Sie können die Permalinks auf der Registerkarte \"Einstellungen\" aktivieren, wenn Ihr Server mod_rewrite unterstützt. Sie aktivieren die verschiedenen Permalinks, die Sie auf der Registerkarte Deaktiviert finden. Sie können die Regeln bearbeiten, um eine URL direkt in jedem Permalink-Modul weiter an Ihre Vorlieben anzupassen.<br/><br/>
+                  Bitte beachten Sie, dass mod_rewrite oder ein Äquivalent erforderlich ist, damit Permalinks funktionieren.";
 
 // Admin Messages
 $locale['PL_420'] = "Ein Fehler ist aufgetreten.";
@@ -40,5 +40,5 @@ $locale['PL_429'] = "Permalink-Treiber für %s";
 // Other
 $locale['PL_427'] = "Keine Permalinks sind aktiviert.";
 $locale['PL_428'] = "Permalinks";
-$locale['seo_htc_warning'] = "Bitte beachte, wenn du etwas an den Einstellungen &auml;nderst, wird die \".htaccess\"-Datei &uuml;berschrieben und alle bisherigen &Auml;nderungen an der Datei sind verloren!";
-$locale['rewrite_disabled'] = "Es sieht so aus, als ob `mod_rewrite` auf deinem Rechner nicht aktiviert ist. Das Aktivieren von SEF-URLs könnte deine Website zerstören. Bitte kontaktiere deinen Hosting-Provider, um `mod_rewrite` auf deinem Host zu aktivieren.";
+$locale['seo_htc_warning'] = "Bitte beachten Sie, dass, wenn Sie eine dieser Einstellungen ändern, der Inhalt von „.htaccess“ überschrieben wird und alle zuvor an dieser Datei vorgenommenen Änderungen verloren gehen.";
+$locale['rewrite_disabled'] = "Es sieht so aus, als ob `mod_rewrite` auf Ihrem Host nicht aktiviert ist. Das Aktivieren von SEF-URLs kann Ihre Website beschädigen. Bitte wenden Sie sich an Ihren Hosting-Provider, um „mod_rewrite“ auf Ihrem Host zu aktivieren.";
