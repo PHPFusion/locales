01bd00723e8f6fdb1ba0c063ef3feb706b799ef1
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:51:22 +0800

    New translations search.php (German)

--- German/locale/German/search.php
+++ German/locale/German/search.php
@@ -1,40 +1,40 @@
 <?php
-$locale['400'] = "Suche auf [SITENAME]";
+$locale['400'] = "Suche [SITENAME]";
 $locale['401'] = "Suchbegriff";
-$locale['402'] = "Suche starten";
-$locale['403'] = "Ergebnis muss mindestens ein Wort enthalten";
-$locale['404'] = "Ergebnis muss alle W&ouml;rter enthalten";
-$locale['405'] = "Suchbereich";
-$locale['406'] = "Optionen";
-$locale['407'] = "Komplette Seite";
-$locale['408'] = "Suchergebnise";
+$locale['402'] = "Suche";
+$locale['403'] = "suche eines dieser Wörter";
+$locale['404'] = "das Ergebnis muss all diese Wörter enthalten";
+$locale['405'] = "Wo:";
+$locale['406'] = "Einstellungen:";
+$locale['407'] = "Ganze Seite";
+$locale['408'] = "Suchergebnisse";
 //addition date
-$locale['420'] = "Zeitraum";
+$locale['420'] = "Suche:";
 $locale['421'] = "Alles";
 $locale['422'] = "letzter Tag";
 $locale['423'] = "letzte Woche";
 $locale['424'] = "letzten zwei Wochen";
-$locale['425'] = "letztes Monat";
+$locale['425'] = "letzter Monat";
 $locale['426'] = "letzten drei Monate";
 $locale['427'] = "letzten sechs Monate";
 //where
-$locale['430'] = "Titel und Inhalt";
-$locale['431'] = "nur Inhalt";
-$locale['432'] = "nur Titel";
+$locale['430'] = "name und Nachricht";
+$locale['431'] = "nur Nachricht";
+$locale['432'] = "nur Name";
 //sort by
 $locale['440'] = "Sortieren nach";
-$locale['441'] = "Datum";
-$locale['442'] = "Titel";
+$locale['441'] = "hinzufügen Datum";
+$locale['442'] = "name";
 $locale['443'] = "Autor";
 //
-$locale['450'] = "Absteigend (Z-A)";
-$locale['451'] = "Aufsteigend (A-Z)";
+$locale['450'] = "absteigend";
+$locale['451'] = "aufsteigend";
 //characters in results
 $locale['460'] = "Anzeigen";
-$locale['461'] = "Zeichenanzahl im Ergebnis";
-$locale['462'] = "kompletten Inhalt";
+$locale['461'] = "zeichen im Ergebnis";
+$locale['462'] = "gesamter Text";
 //No results
-$locale['500'] = "Es wurden keine Ergebnise deinen Suchkriterien entsprechend gefunden.";
+$locale['500'] = "Keine Treffer gefunden.";
 $locale['501'] = "Der Suchbegriff muss mindestens 3 Zeichen lang sein.";
 $locale['502'] = "Suchbegriffe mit weniger als 3 Zeichen (%s) werden in der Suche ignoriert.";
 //Global search
