a9c10c6578cabc72f985aff734c9d0ec51f3c084
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:29:16 +0800

    New translations bbcodes.php (Chinese Simplified)

--- Chinese Simplified/locale/Chinese Simplified/admin/bbcodes.php
+++ Chinese Simplified/locale/Chinese Simplified/admin/bbcodes.php
@@ -1,5 +1,5 @@
 <?php
-$locale['BBCA_400'] = "BB Codes";
+$locale['BBCA_400'] = "BB 代码";
 $locale['BBCA_400a'] = "BB 代码管理";
 $locale['BBCA_401'] = "测试BB代码";
 // Enabled BB Codes
@@ -11,11 +11,11 @@ $locale['BBCA_406'] = "用途";
 $locale['BBCA_407'] = "Item Ordering";
 $locale['BBCA_408'] = "下移";
 $locale['BBCA_409'] = "上移";
-$locale['BBCA_410'] = "Disable";
+$locale['BBCA_410'] = "禁用";
 $locale['BBCA_411'] = "没有启用 BB 代码。";
 // Disabled BB Codes
 $locale['BBCA_413'] = "禁用的 BB 代码";
-$locale['BBCA_414'] = "Enable";
+$locale['BBCA_414'] = "启用";
 $locale['BBCA_416'] = "没有禁用的 BB 代码。";
 // Test BB Codes
 $locale['BBCA_417'] = "BB 代码测试输出";
@@ -27,9 +27,9 @@ $locale['BBCA_430'] = "BB 代码已向上移动。";
 $locale['BBCA_431'] = "BB 代码已向下移动。";
 $locale['BBCA_432'] = "BB 代码已启用。";
 $locale['BBCA_433'] = "BB 代码已被禁用。";
-$locale['BBCA_444'] = "BB Code has been updated";
-$locale['BBCA_445'] = "BB Code failed to be updated";
+$locale['BBCA_444'] = "BB 代码已更新。";
+$locale['BBCA_445'] = "BB 代码更新失败";
 
-$locale['BBCA_500'] = "Add Separator";
-$locale['BBCA_501'] = "Separator";
-$locale['BBCA_502'] = "Separator for grouping BBcode buttons";
+$locale['BBCA_500'] = "添加分隔符";
+$locale['BBCA_501'] = "分隔符";
+$locale['BBCA_502'] = "BB 代码按键分组的分隔符";
