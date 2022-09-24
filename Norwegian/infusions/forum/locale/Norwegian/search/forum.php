e95baa3257eaedbb55ebd17a5b722e742119d7b9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:56:48 +0800

    New translations forum.php (Norwegian)

--- Norwegian/infusions/forum/locale/Norwegian/search/forum.php
+++ Norwegian/infusions/forum/locale/Norwegian/search/forum.php
@@ -1,9 +1,9 @@
 <?php
 $locale['forum.php'] = "Debatter";
-$locale['f400'] = "Forum Posts";
+$locale['f400'] = "Forum innlegg";
 $locale['f401'] = "alle forumer";
 $locale['f402'] = "Forum innlegg";
-$locale['f403'] = "Forum Posts";
+$locale['f403'] = "Forum innlegg";
 $locale['f404'] = "Låst til toppen:";
 $locale['f405'] = "Offentliggjørelse:";
 $locale['f406'] = "funnet i";
