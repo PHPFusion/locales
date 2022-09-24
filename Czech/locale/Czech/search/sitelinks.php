92f78e4296184e4b0f5caa45e7a535c325c7271f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:40:30 +0800

    New translations sitelinks.php (Czech)

--- Czech/locale/Czech/search/sitelinks.php
+++ Czech/locale/Czech/search/sitelinks.php
@@ -1,6 +1,6 @@
 <?php
-$locale['s400'] = "Odkazy na stránku";
-$locale['s401'] = "Odkazy na stránku";
+$locale['s400'] = "Odkazy stránky";
+$locale['s401'] = "Odkazy stránky";
 $locale['s402'] = "odkazů";
 $locale['s403'] = "nalezeno v";
 $locale['s404'] = "Název odkazu stránky";
