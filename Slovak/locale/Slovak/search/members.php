7c233e965c22ab42d92a5b74d4604fa7a26cca8c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:40:51 +0800

    New translations members.php (Slovak)

--- Slovak/locale/Slovak/search/members.php
+++ Slovak/locale/Slovak/search/members.php
@@ -1,5 +1,5 @@
 <?php
-$locale['m400'] = "Užívatelia";
+$locale['m400'] = "Členovia";
 $locale['m401'] = "Člen";
-$locale['m402'] = "Užívatelia";
+$locale['m402'] = "členov";
 $locale['m403'] = "Profily sú viditeľné len pre členov!";
