7272bec6429fb06d78e3b725e22084eb73ffb605
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:59:39 +0800

    New translations news.php (Portuguese)

--- Portuguese/infusions/news/locale/Portuguese/news.php
+++ Portuguese/infusions/news/locale/Portuguese/news.php
@@ -2,7 +2,7 @@
 $locale['news_0001'] = "Continuar a ler";
 $locale['news_0002'] = "Imprimir";
 $locale['news_0004'] = "Notícias";
-$locale['news_0005'] = "Nenhuma notícia foi publicada ainda";
+$locale['news_0005'] = "Nenhuma notíciafoi publicada ainda";
 $locale['news_0006'] = "Sem Categoria";
 $locale['news_0008'] = "Última Atualização";
 $locale['news_0009'] = "Categorias de Notícias";
@@ -16,4 +16,4 @@ $locale['news_0016'] = "Não há categorias definidas.";
 $locale['news_0017'] = "Filtrar por";
 $locale['news_0018'] = "Mostrar todas as notícias";
 $locale['news_0019'] = "Galeria de Notícias";
-$locale['news_0020'] = "Most Read News";
+$locale['news_0020'] = "Notícias mais lidas";
