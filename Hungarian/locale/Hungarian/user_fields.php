c35fe58f44d6c5b2f8db93c37e499fa1ab40e3bd
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:51:57 +0800

    New translations user_fields.php (Hungarian)

--- Hungarian/locale/Hungarian/user_fields.php
+++ Hungarian/locale/Hungarian/user_fields.php
@@ -1,12 +1,12 @@
 <?php
 // Items shown in profile
-$locale['u040'] = "Regisztráció dátuma";
+$locale['u040'] = "Regisztráció";
 $locale['u041'] = "Utolsó látogatás";
-$locale['u042'] = "Még nem járt itt";
-$locale['u043'] = "Új üzenet írása";
+$locale['u042'] = "Soha";
+$locale['u043'] = "Privát üzenet küldése";
 $locale['u044'] = "Elérhetőségek";
 $locale['u045'] = "Információk";
-$locale['u046'] = "Lehetőségek";
+$locale['u046'] = "Beállítások";
 $locale['u047'] = "Statisztika";
 $locale['u048'] = "Admin Információ";
 $locale['u049'] = "IP cím";
@@ -14,42 +14,42 @@ $locale['u050'] = "-";
 $locale['u051'] = "E-mail elrejtése";
 $locale['u052'] = " Igen ";
 $locale['u053'] = " Nem";
-$locale['u054'] = "Felfüggesztési napló megtekintése";
-$locale['u055'] = "Felhasználó státusza:";
-$locale['u056'] = "Indok";
+$locale['u054'] = "Kitiltási napló";
+$locale['u055'] = "Státusz:";
+$locale['u056'] = "Indoklás";
 $locale['u057'] = "Felhasználói csoportok";
 $locale['u058'] = "Admin lehetőségek";
 $locale['u059'] = "Hozzáad";
 $locale['u060'] = "Biztosan hozzá akarod adni ezt a felhasználót a csoporthoz?";
 $locale['u061'] = "Hozzáadás a csoporthoz";
-$locale['u062'] = "Felhasználói avatar";
-$locale['u063'] = "Felhasználó rangja";
-$locale['u064'] = "Email";
+$locale['u062'] = "Avatar";
+$locale['u063'] = "Felhasználói Szint";
+$locale['u064'] = "E-mail";
 $locale['u066'] = "Regisztráció dátuma";
-$locale['u067'] = "Utolsó látogatás";
+$locale['u067'] = "Legutóbbi látogatás";
 $locale['u068'] = "Felhasználónév";
 $locale['u070'] = "Kitilt";
 $locale['u071'] = "Felfüggeszt";
 $locale['u073'] = "Valóban törölni szereznéd ezt a felhasználót?";
 $locale['u074'] = "Kitiltás visszavonva";
 // Profile and register
-$locale['u100'] = "Ha meg szeretnéd változtatni jelszavad vagy e-mail címed, kérjük add meg a jelenlegi jelszavadat is.";
+$locale['u100'] = "Ha meg szeretnéd változtatni jelszavad vagy e-mail címed,<br /> kérjük add meg a jelenlegi jelszavadat is.";
 $locale['u101'] = "Regisztráció";
 $locale['u102'] = "Profil szerkesztése";
 $locale['u103'] = "Profil";
-$locale['u104'] = "Felhasználói profil";
+$locale['u104'] = "Profil";
 $locale['u105'] = "Profil módosítása";
 
 // New UF 1.20
-$locale['uf_100'] = "Általános fiókbeállítások";
-$locale['uf_101'] = "Felhasználói információk beállításai";
-$locale['uf_102'] = "Felhasználói avatar beállításai";
+$locale['uf_100'] = "Általános account beállítások";
+$locale['uf_101'] = "User Infó beállítások";
+$locale['uf_102'] = "User Avatar beállítások";
 $locale['uf_103'] = "Általános";
-$locale['uf_104'] = "Felhasználói menü";
+$locale['uf_104'] = "User Infó";
 $locale['uf_105'] = "Profilkép";
 $locale['uf_106'] = "Alap információk";
 $locale['uf_107'] = "%s nem rendelkezik felhasználói adattal.";
-$locale['uf_108'] = "Nincsenek meghatározva felhasználói mezők";
+$locale['uf_108'] = "User mezők nincsenek.";
 
 // View User Groups
 $locale['u110'] = "Felhasználói csoportok";
@@ -58,10 +58,10 @@ $locale['u112'] = "%u tag";
 $locale['u113'] = "Felhasználónév";
 $locale['u114'] = "Rang";
 $locale['u115'] = "Nyelv";
-$locale['u116'] = "Nincsenek csoporttagok.";
-$locale['u117'] = "Nincs felhasználói csoport.";
+$locale['u116'] = "Nincs Csoportnak Tagja.";
+$locale['u117'] = "Nincs Csoport.";
 // User name and email
-$locale['u119'] = "A felhasználó neve tilos.";
+$locale['u119'] = "A felhasználónév tartalmazza.";
 $locale['u120'] = "A felhasználónév nem engedélyezett karaktereket tartalmaz.";
 $locale['u121'] = "A megadott felhasználónév már foglalt.";
 $locale['u122'] = "Meg kell adnod a kívánt felhasználóneved.";
@@ -71,21 +71,21 @@ $locale['u125'] = "A megadott e-mail címmel már regisztrált egy felhasználó
 $locale['u126'] = "Meg kell adnod az e-mail címed.";
 $locale['u127'] = "Felhasználónév";
 $locale['u128'] = "E-mail cím";
-$locale['u129'] = "Alap felhasználói információk";
+$locale['u129'] = "Profil infó";
 // Passwords
 $locale['u130'] = "Admin jelszó";
 $locale['u131'] = "Admin jelszó";
 $locale['u132'] = "Bejelentkezési jelszó";
 $locale['u133'] = "Bejelentkezési jelszó";
 $locale['u134'] = "Új jelszó";
-$locale['u134a'] = "Jelszó beállítása";
+$locale['u134a'] = "Jelszó";
 $locale['u134b'] = "Jelszó újra";
-$locale['u135a'] = "Jelenlegi jelszó";
-$locale['u135b'] = "Új jelszó beállítása";
-$locale['u135c'] = "Új jelszó újra";
-$locale['u136'] = "A jelszó mező nem lehet üres.";
-$locale['u137'] = "A jelenlegi rendszergazdai jelszó nem hagyható üresen.";
-$locale['u138'] = "A jelenlegi bejelentkezési jelszavát nem hagyhatja üresen.";
+$locale['u135a'] = "Bejelentkezési jelszó";
+$locale['u135b'] = "Új jelszó";
+$locale['u135c'] = "Jelszó megerősítése ";
+$locale['u136'] = "Meg kell adnod a jelszót.";
+$locale['u137'] = "Meg kell adnod a jelenlegi admin jelszavad.";
+$locale['u138'] = "Meg kell adnod a jelenlegi jelszavad.";
 $locale['u139'] = "A megadott jelszó nem egyezik a jelenlegi jelszavaddal.";
 $locale['u140'] = "A megadott admin jelszó nem egyezik a jelenlegi admin jelszavaddal.";
 $locale['u141'] = " nem lehet ugyan az, mint a ";
@@ -96,21 +96,33 @@ $locale['u144'] = "Új admin jelszó";
 $locale['u144a'] = "Admin jelszó";
 $locale['u145'] = "Admin jelszó megerősítése";
 $locale['u146'] = " nem lehet ugyan az, mint a jelenlegi ";
-$locale['u147'] = "A jelszónak 8-64 karakter hosszúnak kell lennie.<br/>Engedélyezett karakterek: a-z, 0-9 és @!#$%&\\/()=-_?+*.,:;";
-$locale['u148'] = "Az új bejelentkezési jelszavak nem azonosak.";
-$locale['u148a'] = "Az új admin jelszavak nem azonosak.";
+$locale['u147'] = "A jelszónak 8-64 karakter hosszúnak kell lennie.<br />
+Engedélyezett karakterek: a-z, 0-9 és @!#$%&\\/()=-_?+*.,:;";
+$locale['u148'] = "Az új jelszavak nem egyeznek meg.";
+$locale['u148a'] = "Az új admin jelszavak nem egyeznek meg.";
 $locale['u149'] = "A jelenlegi jelszavad nem lett megadva vagy érvénytelen.";
 $locale['u149a'] = "A jelenlegi admin jelszavad nem lett megadva vagy érvénytelen.";
-$locale['u149b'] = "A megadott belépési jelszó hibás vagy nincs megadva.<br/>A megfelelő belépési jelszó nélkül nem tudod módosítani az admin jelszavad.";
+$locale['u149b'] = "A megadott belépési jelszó hibás vagy nincs megadva.<br />
+A megfelelő belépési jelszó nélkül nem tudod módosítani az admin jelszavad.";
 // Email activation
-$locale['u150'] = "A regisztráció már majdnem kész. Hamarosan kapsz egy aktiváló e-mailt a felhasználóneveddel, jelszavaddal és egy linkkel, amin aktiválhatod hozzáférésed.";
-$locale['u151'] = "Üdvözlünk a(z) [SITENAME] oldalon";
-$locale['u152'] = "Szia USER_NAME,<br/>
-Üdvözlünk oldalunkon - SITENAME. Az alábbi adatokkal regisztráltál:<br/>
-Felhasználónév: USER_NAME<br/>
-Jelszó: USER_PASSWORD<br/>
-Az alábbi linken aktiválhatod hozzáférésed: ACTIVATION_LINK<br/>
-Üdvözlettel,<br/>SITENAME";
+$locale['u150'] = "A regisztráció már majdnem kész.<br />
+Hamarosan kapsz egy aktiváló e-mailt a felhasználóneveddel, jelszavaddal és egy linkkel, amin aktiválhatod hozzáférésed.";
+$locale['u151'] = "Üdvözlünk - [SITENAME]";
+$locale['u152'] = "Szia USER_NAME,<br />
+
+Üdvözlünk oldalunkon - SITENAME.<br />
+Az alábbi adatokkal regisztráltál:<br />
+
+Felhasználónév: USER_NAME<br />
+
+Jelszó: USER_PASSWORD<br />
+
+Az alábbi linken aktiválhatod hozzáférésed:<br />
+ACTIVATION_LINK<br />
+
+Üdvözlettel,<br />
+
+SITENAME";
 
 $locale['u153'] = "Nem sikerült elküldeni az ellenőrző levelet.";
 $locale['u154'] = "Kérjük [LINK]jelezd[/LINK] a problémát az oldal adminisztrátorának.";
@@ -120,9 +132,9 @@ $locale['u156'] = "Az e-mail címed megváltoztatásához kérjük add meg a jel
 $locale['u160'] = "Regisztráció befejezve";
 $locale['u161'] = "Most már be tudsz jelentkezni.";
 $locale['u162'] = "Egy adminisztrátor hamarosan aktiválja hozzáférésed.";
-$locale['u163'] = "Profil sikeresen módosítva.";
+$locale['u163'] = "Profil módosítva.";
 $locale['u164'] = "Profil módosítása nem sikerült";
-$locale['u165'] = "Aktiváló e-mail újraküldve.";
+$locale['u165'] = "Aktíváló E-mail újraküldve.";
 
 $locale['u167'] = "az alábbi okok miatt:";
 $locale['u168'] = "Kérjük próbáld meg újra.";
@@ -141,9 +153,9 @@ $locale['u186'] = "Kép feltöltéséhez kattints a 'Tallózás' gombra";
 // Captcha and terms
 $locale['u190'] = "Ellenőrzőkód";
 $locale['u191'] = "Add meg az ellenőrzőkódot";
-$locale['u192'] = "Felhasználási feltételek";
-$locale['u193'] = "Elolvastam és elfogadom a [LINK]Felhasználási feltételeket[/LINK].";
-$locale['u193a'] = "Frissítve";
+$locale['u192'] = "Szabályzat";
+$locale['u193'] = "Elolvastam és elfogadom a [LINK]szabályzatot[/LINK].";
+$locale['u193a'] = "Módosítva";
 $locale['u193b'] = "Elfogadom";
 $locale['u194'] = "A megadott ellenőrzőkód hibás.";
 $locale['u195'] = "Kérjük add meg az ellenőrzőkódot.";
@@ -151,13 +163,21 @@ $locale['u195'] = "Kérjük add meg az ellenőrzőkódot.";
 $locale['u200'] = "Egy ellenőrző levelet küldtünk az új e-mail címedre (%s).";
 $locale['u201'] = "E-mail címed lecseréléséhez kattints az e-mailben található linkre.";
 $locale['u202'] = "E-mail cím ellenőrzése - [SITENAME]";
-$locale['u203'] = "Szia [USER_NAME],<br/>
-Valaki ezt az e-mail címet adta meg oldalunkon a profiljában.<br/>
-Ha valóban te szeretnéd lecserélni az e-mail címedet erre, kérjük kattints az alábbi linkre:<br/>
-[EMAIL_VERIFY_LINK]<br/>
-Megjegyzés: a link használata előtt jelentkezz be!<br/>
-Üdvözlettel,<br/>
-[SITEUSERNAME]<br/>[SITENAME]";
+$locale['u203'] = "Szia [USER_NAME],<br />
+
+Valaki ezt az e-mail címet adta meg oldalunkon a profiljában.<br />
+
+Ha valóban te szeretnéd lecserélni az e-mail címedet erre, kérjük kattints az alábbi linkre:<br />
+
+[EMAIL_VERIFY_LINK]<br />
+
+Megjegyzés: a link használata előtt jelentkezz be!<br />
+
+Üdvözlettel,<br />
+
+[SITEUSERNAME]<br />
+
+[SITENAME]";
 // Checker
 $locale['u300'] = "A jelszónak legalább 1 speciális karaktert kell tartalmaznia";
 $locale['u301'] = "A jelszónak tartalmaznia kell legalább 1 nagybetűs és 1 kisbetűs karaktert";
