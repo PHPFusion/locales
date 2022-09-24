3a9350923f8169b989d2ce17dc336440b951a15e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:01:09 +0800

    New translations weblinks.php (Romanian)

--- Romanian/infusions/weblinks/locale/Romanian/weblinks.php
+++ Romanian/infusions/weblinks/locale/Romanian/weblinks.php
@@ -4,11 +4,11 @@ $locale['web_0000'] = "Leg&#259;turi pe internet";
 $locale['web_0001'] = "Toate leg&#259;turile pe internet";
 $locale['fmt_open'] = "deschide|deschide";
 // Sorting
-$locale['web_0030'] = "Cel mai recent";
+$locale['web_0030'] = "Cele mai recente";
 $locale['web_0031'] = "Top vizite";
 $locale['web_0032'] = "Cele mai vechi post&#259;ri";
 // Views
-$locale['web_0040'] = "Vizualizare ca pictograme";
+$locale['web_0040'] = "Vizualizare ca galerie";
 $locale['web_0041'] = "Vizualizare ca list&#259;";
 // Messages
 $locale['web_0060'] = "Nicio categorie de leg&#259;turi pe internet definit&#259;.";
