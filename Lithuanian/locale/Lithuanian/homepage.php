e1cbf3884cd75fb21ae8f9aa6276e20aae0448ed
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:06 +0800

    New translations homepage.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/homepage.php
+++ Lithuanian/locale/Lithuanian/homepage.php
@@ -3,7 +3,7 @@
 $locale['home_0000'] = "Paskutinės naujienos";
 $locale['home_0001'] = "Naujausi straipsniai";
 $locale['home_0002'] = "Naujausi tinklaraščiai";
-$locale['home_0003'] = "Naujausi siuntiniai";
+$locale['home_0003'] = "Naujausi atsisiuntimai";
 // no content messages
 $locale['home_0050'] = "Nėra naujienų rodymui";
 $locale['home_0051'] = "Nėra straipsnių rodymui";
@@ -12,6 +12,6 @@ $locale['home_0053'] = "Nėra siuntinių rodymui";
 // common text
 $locale['home_0100'] = "Turinio moduliai neįdiegti";
 $locale['home_0101'] = "Prašome įdiegti bent vieną modulį teksto turiniui arba pakeisti pradžios langą Pagrindiniuose nustatymuose.";
-$locale['home_0102'] = "Nekategorizuota";
+$locale['home_0102'] = "Be kategorijos";
 $locale['home_0105'] = "Paskelbė ";
 $locale['home_0106'] = " ";
