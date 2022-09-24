3777b155ad42c468eb2d09f3801b8c1703c3875b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:52:55 +0800

    New translations articles.php (Slovak)

--- Slovak/infusions/articles/locale/Slovak/search/articles.php
+++ Slovak/infusions/articles/locale/Slovak/search/articles.php
@@ -2,7 +2,7 @@
 $locale['articles.php'] = "Články";
 $locale['a400'] = "Články";
 $locale['a401'] = "Článok";
-$locale['a402'] = "Články";
+$locale['a402'] = "článkov";
 $locale['a403'] = "nájdené v";
 $locale['a404'] = "predmet článku";
 $locale['a405'] = "text článku";
