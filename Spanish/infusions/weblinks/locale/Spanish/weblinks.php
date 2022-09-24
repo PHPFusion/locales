10118f7e82a1818aa8487d3fff4e9ac98d7823d4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:01:18 +0800

    New translations weblinks.php (Spanish)

--- Spanish/infusions/weblinks/locale/Spanish/weblinks.php
+++ Spanish/infusions/weblinks/locale/Spanish/weblinks.php
@@ -1,6 +1,6 @@
 <?php
 // Overview
-$locale['web_0000'] = "Enlaces Web";
+$locale['web_0000'] = "Enlaces web";
 $locale['web_0001'] = "Todos los enlaces web";
 $locale['fmt_open'] = "abrir|abrir";
 // Sorting
@@ -8,8 +8,8 @@ $locale['web_0030'] = "Más reciente";
 $locale['web_0031'] = "Arriba abierto";
 $locale['web_0032'] = "Más antiguo";
 // Views
-$locale['web_0040'] = "vista del pulgar";
-$locale['web_0041'] = "Vista de lista";
+$locale['web_0040'] = "Vista galería";
+$locale['web_0041'] = "Vista lista";
 // Messages
 $locale['web_0060'] = "No se definieron categorías de enlace web.";
 $locale['web_0062'] = "No hay un enlace web en esta categoría.";
