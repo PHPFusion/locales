5cdc7196bfc076be6b6dbb1a92e2eb7a07be56ab
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:48:01 +0800

    New translations contact.php (Belarusian)

--- Belarusian/locale/Belarusian/contact.php
+++ Belarusian/locale/Belarusian/contact.php
@@ -1,25 +1,25 @@
 <?php
 // Contact Form
-$locale['CT_400'] = "Contact";
-$locale['CT_401'] = "There are several ways you can get in contact. You can email directly at
-[SITE_EMAIL]. If you are a Member you can send a [PM_LINK].
-Alternatively, you can fill in the form on this page which sends your message via Email.";
-$locale['CT_402'] = "Name:";
-$locale['CT_403'] = "Email Address:";
-$locale['CT_404'] = "Subject:";
-$locale['CT_405'] = "Message:";
-$locale['CT_406'] = "Send Message";
-$locale['CT_407'] = "Validation Code:";
-$locale['CT_408'] = "Enter Validation Code:";
+$locale['CT_400'] = "Зваротная сувязь";
+$locale['CT_401'] = "Звязацца з вамі можна некалькімі спосабамі. Вы можаце напісаць наўпрост на адрас электроннай пошты
+[SITE_EMAIL]. Калі вы зарэгістраваны, вы можаце адправіць [PM_LINK].
+Ці ж вы можаце запоўніць форму на гэтай старонцы, якая адправіць ваша паведамленне па электроннай пошце.";
+$locale['CT_402'] = "Імя:";
+$locale['CT_403'] = "Адрас электроннай пошты:";
+$locale['CT_404'] = "Тэма:";
+$locale['CT_405'] = "Паведамленне:";
+$locale['CT_406'] = "Адправіць паведамленне";
+$locale['CT_407'] = "Код праверкі:";
+$locale['CT_408'] = "Увядзіце код праверкі:";
 // Contact Errors
-$locale['CT_420'] = "You must specify a Name";
-$locale['CT_421'] = "You must specify an Email Address";
-$locale['CT_422'] = "You must specify a Subject";
-$locale['CT_423'] = "You must specify a Message";
-$locale['CT_424'] = "You must enter correct Validation Code";
-$locale['CT_425'] = "Internal error: cannot send your message.";
+$locale['CT_420'] = "Патрэбна пазначыць імя";
+$locale['CT_421'] = "Патрэбна пазначыць адрас электроннай пошты";
+$locale['CT_422'] = "Патрэбна пазначыць тэму";
+$locale['CT_423'] = "Патрэбна ўвесці паведамленне";
+$locale['CT_424'] = "Патрэбна ўвесці правільны код праверкі";
+$locale['CT_425'] = "Унутраная памылка: немагчыма адправіць ваша паведамленне.";
 // Message Sent
-$locale['CT_440'] = "Your Message has been sent";
-$locale['CT_441'] = "Thank You";
-$locale['CT_442'] = "Your message was not sent for the following reason(s):";
-$locale['CT_443'] = "Please try again.";
+$locale['CT_440'] = "Ваша паведамленне адпраўлена";
+$locale['CT_441'] = "Дзякуй";
+$locale['CT_442'] = "Ваша паведамленне не было адпраўлена па наступнай(-ых) прычыне(-ах):";
+$locale['CT_443'] = "Паспрабуйце яшчэ раз.";
