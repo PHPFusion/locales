94bb799ae58da3965a360ab1cbb566697a2b38e6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:44:04 +0800

    New translations user_name_middle.php (Italian)

--- Italian/locale/Italian/user_fields/user_name_middle.php
+++ Italian/locale/Italian/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "Secondo Nome";
+$locale['uf_name_middle'] = "Secondo nome (centrale)";
 $locale['uf_name_middle_desc'] = "utente reale secondo nome.";
 $locale['uf_name_middle_error'] = "secondo nome (centrale) non può essere lasciato vuoto...";
