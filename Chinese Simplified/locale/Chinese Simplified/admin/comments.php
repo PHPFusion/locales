edb7b8171078ae4b2fc093959879e71d181de2aa
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:29:14 +0800

    New translations comments.php (Chinese Simplified)

--- Chinese Simplified/locale/Chinese Simplified/admin/comments.php
+++ Chinese Simplified/locale/Chinese Simplified/admin/comments.php
@@ -1,26 +1,26 @@
 <?php
 // Comments titles
 $locale['C_400'] = "编辑评论";
-$locale['C_401'] = "Comments";
+$locale['C_401'] = "评论";
 // Comments messages
 $locale['C_410'] = "评论已更新";
 $locale['C_411'] = "评论已删除";
-$locale['C_412'] = "The comment has been removed and user added to blacklist.";
+$locale['C_412'] = "该评论已被删除，用户已添加到黑名单。";
 // Edit Comment form
 $locale['C_420'] = "在这个评论中禁用微笑";
 $locale['C_421'] = "保存评论";
 // Comments
-$locale['C_431'] = "Delete & Ban";
+$locale['C_431'] = "删除和禁止";
 $locale['C_432'] = "用户 IP：";
 $locale['C_433'] = "删除此评论？";
-$locale['C_434'] = "There are no comments for this item.";
-$locale['C_435'] = "Do you really want to add this user to a blacklist?";
-$locale['C_436'] = "Blacklisted comment.";
+$locale['C_434'] = "没有对这品目的评论。";
+$locale['C_435'] = "你确定要将该用户加入黑名单吗？";
+$locale['C_436'] = "黑名单评论。";
 // Settings
-$locale['C_440'] = "Enable comment system?";
-$locale['C_441'] = "Allow Guests to post?";
-$locale['C_442'] = "Enable avatar comments?";
+$locale['C_440'] = "启用评论系统？";
+$locale['C_441'] = "允许匿名用户发帖？";
+$locale['C_442'] = "启用头像评论？";
 $locale['C_443'] = "Comments per page";
-$locale['C_444'] = "Comments Sorting";
-$locale['C_445'] = "Ascending";
-$locale['C_446'] = "Descending";
+$locale['C_444'] = "评论排序";
+$locale['C_445'] = "递增";
+$locale['C_446'] = "降序";
