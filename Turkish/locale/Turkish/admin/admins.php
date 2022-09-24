128991a10d6e56cbcb0df7feb1ad271c18a0f2ec
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:28:33 +0800

    New translations admins.php (Turkish)

--- Turkish/locale/Turkish/admin/admins.php
+++ Turkish/locale/Turkish/admin/admins.php
@@ -6,14 +6,14 @@ $locale['ADM_402'] = "Yönetici Silindi";
 // Search/Add Administrator
 $locale['ADM_410'] = "Yönetici Ekle";
 $locale['ADM_411'] = "Aşağıdaki Formu Kullanarak Eklenmesini İstediğiniz Kullanıcıları Arayın.";
-$locale['ADM_413'] = "Kullanıcı Adı";
+$locale['ADM_413'] = "Üye İsmi";
 $locale['ADM_415'] = "Tüm Ayrıcalıkları Uygula";
 $locale['ADM_416'] = "Süper Yönetici Yap";
 $locale['ADM_418'] = "Kullanıcı Bulunamadı.";
 $locale['ADM_419'] = "Tekrar Ara";
 // Current Administrators
 $locale['ADM_420'] = "Yöneticiler";
-$locale['ADM_421'] = "Kullanıcı Adı";
+$locale['ADM_421'] = "Üye İsmi";
 $locale['ADM_422'] = "Yönetici Seviyesi";
 $locale['ADM_423'] = "Seçenekler";
 $locale['ADM_425'] = "Haklar Atanmadı";
@@ -22,8 +22,8 @@ $locale['ADM_440'] = "Yönetici Haklarını Düzenle";
 $locale['ADM_441'] = "İçerik Yöneticisi";
 $locale['ADM_442'] = "Kullanıcı Yöneticisi";
 $locale['ADM_443'] = "Sistem Yöneticisi";
-$locale['ADM_444'] = "Uygulamalar";
-$locale['ADM_445'] = "Hepsini işaretle";
+$locale['ADM_444'] = "Modüller";
+$locale['ADM_445'] = "Hepsini Seç";
 $locale['ADM_448'] = "Yönetici Haklarını Kaydet";
 $locale['ADM_449'] = "Ayarlar";
 $locale['ADM_450'] = "Güvenliğini Kontrol Et";
