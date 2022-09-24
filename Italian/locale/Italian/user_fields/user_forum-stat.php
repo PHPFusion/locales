f5d57c3bd3e474e0bfbb4ccceff0bbed19c53d79
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:20 +0800

    New translations user_forum-stat.php (Italian)

--- Italian/locale/Italian/user_fields/user_forum-stat.php
+++ Italian/locale/Italian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Forum Posts";
+$locale['uf_forum-stat'] = "Posts Forum";
 $locale['uf_forum-stat_desc'] = "Mostra il numero di posts nel forum.";
 $locale['uf_forum-stat_na'] = "Please install Forum infusion.";
