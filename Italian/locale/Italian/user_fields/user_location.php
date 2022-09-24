47c1a01f3cfc0ddaf793bb325adcc80b30606584
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:24 +0800

    New translations user_location.php (Italian)

--- Italian/locale/Italian/user_fields/user_location.php
+++ Italian/locale/Italian/user_fields/user_location.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_location'] = "Posizione";
+$locale['uf_location'] = "Provenienza";
 $locale['uf_location_desc'] = "Paese, citta o contrada";
 $locale['uf_location_error'] = "La provenienza non puo essere lasciata vuota.";
