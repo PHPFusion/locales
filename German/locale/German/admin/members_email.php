fa27749d17c1459fa0e08ac78ed6344736a5b6d9
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:31:02 +0800

    New translations members_email.php (German)

--- German/locale/German/admin/members_email.php
+++ German/locale/German/admin/members_email.php
@@ -1,32 +1,32 @@
 <?php
 $locale['email_create_subject'] = "Account erstellt auf [SITENAME]";
 $locale['email_create_message'] = "Hallo [USER_NAME],<br/>
-Ihr Konto bei [SITENAME] wurde erstellt.<br/>Sie können sich jetzt mit folgenden Details anmelden:<br/>
-Benutzername: [USER_NAME]<br/>Passwort: [PASSWORD]<br/>Grüße,<br/>[SITEUSERNAME]";
+Ihr Konto bei [SITENAME] wurde erstellt.<br/>Sie können sich jetzt mit den folgenden Daten anmelden:<br/>
+Benutzername: [USER_NAME]<br/>Passwort: [PASSWORD]<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_activate_subject'] = "Account aktiviert auf [SITENAME]";
 $locale['email_activate_message'] = "Hallo [USER_NAME],<br/>Ihr Konto bei [SITENAME] wurde aktiviert.<br/>
-Sie können sich jetzt mit Ihrem gewählten Benutzernamen und Passwort anmelden.<br/>Mit freundlichen Grüßen,<br/>[SITEUSERNAME]";
+Sie können sich jetzt mit Ihrem gewählten Benutzernamen und Passwort anmelden.<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_deactivate_subject'] = "Account-Reaktivierung erforderlich auf [SITENAME]";
-$locale['email_deactivate_message'] = "Hallo [USER_NAME],<br/>Es ist [DEACTIVATION_PERIOD] Tag(e) seit Sie sich zuletzt bei [SITENAME] angemeldet haben. Ihr Benutzer wurde als inaktiv markiert, aber alle Ihre Kontodaten und Inhalte bleiben intakt.<br/>
-Um Ihr Konto zu reaktivieren, klicken Sie einfach auf den folgenden Link: [REACTIVATION_LINK]<br/>Mit freundlichen Grüßen,<br/>[SITEUSERNAME]";
+$locale['email_deactivate_message'] = "Hallo [USER_NAME],<br/>seit Ihrer letzten Anmeldung bei [SITENAME] sind [DEACTIVATION_PERIOD] Tag(e) vergangen. Ihr Benutzer wurde als inaktiv markiert, aber alle Ihre Kontodetails und Inhalte bleiben erhalten.<br/>
+Um Ihr Konto wieder zu aktivieren, klicken Sie einfach auf den folgenden Link: [REACTIVATION_LINK]<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_ban_subject'] = "Dein Account auf [SITENAME] wurde gesperrt";
-$locale['email_ban_message'] = "Hallo [USER_NAME],<br/>Ihr Konto bei [SITENAME] wurde von [ADMIN_USERNAME] gesperrt, aus folgendem Grund:<br/>
-[REASON]<br/>Wenn Sie weitere Informationen über dieses Verbot wünschen, bitte kontaktieren Sie den Website-Administrator unter [SITENAME].<br/>Grüße,<br/>[SITEUSERNAME]";
+$locale['email_ban_message'] = "Hallo [USER_NAME],<br/>Ihr Konto auf [SITENAME] wurde von [ADMIN_USERNAME] aus folgendem Grund gesperrt:<br/>
+[GRUND]<br/>Wenn Sie weitere Informationen zu diesem Verbot wünschen, wenden Sie sich bitte an den Site-Administrator unter [SITENAME].<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_secban_subject'] = "Dein Account auf [SITENAME] wurde gesperrt";
-$locale['email_secban_message'] = "Hallo [USER_NAME],<br/>Ihr Konto auf [SITENAME] wurde von [ADMIN_USERNAME] wegen einiger Aktionen, die Sie akkreditiert oder mit Ihrem Konto verbunden sind, als Sicherheitsbedrohung für die Seite gesperrt.<br/>
-Wenn Sie weitere Informationen über dieses Sicherheitsverbot wünschen, wenden Sie sich bitte an den Website-Administrator unter [SITENAME].<br/>Grüße,<br/>[SITEUSERNAME]";
+$locale['email_secban_message'] = "Hallo [USER_NAME],<br/>Ihr Konto auf [SITENAME] wurde von [ADMIN_USERNAME] gesperrt, da einige Ihnen zugeschriebene oder mit Ihrem Konto verknüpfte Aktionen als Sicherheitsbedrohung für die Website angesehen wurden.<br/>
+Wenn Sie weitere Informationen zu diesem Sicherheitsverbot wünschen, wenden Sie sich bitte an den Site-Administrator unter [SITENAME].<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_suspend_subject'] = "Dein Account auf [SITENAME] wurde temporär gesperrt";
 $locale['email_suspend_message'] = "Hallo [USER_NAME],<br/>
-Ihr Konto auf [SITENAME] wurde von [ADMIN_USERNAME] bis [DATE] (Site-Zeit) gesperrt wegen folgendem Grund:<br/>
-[REASON]<br/>Wenn Sie weitere Informationen über diese Suspendierung wünschen, bitte kontaktieren Sie den Website-Administrator unter [SITENAME].<br/>Grüße,<br/>[SITEUSERNAME]";
+Ihr Konto auf [SITENAME] wurde von [ADMIN_USERNAME] bis zum [DATE] (Site-Zeit) aus folgendem Grund gesperrt:<br/>
+[GRUND]<br/>Wenn Sie weitere Informationen zu dieser Sperrung wünschen, wenden Sie sich bitte an den Site-Administrator unter [SITENAME].<br/>Mit freundlichen Grüßen<br/>[SITEUSERNAME]";
 
 $locale['email_resend_subject'] = "Aktivierungslink erneut senden - [SITENAME]";
 $locale['email_resend_message'] = "Hallo [USER_NAME],<br/>
-Sie haben diese E-Mail erhalten, weil Sie die E-Mail nicht auf unserer Seite aktiviert haben - [SITENAME].<br/>Wenn Sie innerhalb eines Tages keine E-Mail aktivieren, wird Ihre Registrierungsanfrage abgebrochen.<br/>
-Sie haben sich mit den folgenden Informationen registriert:<br/>Benutzername: [USER_NAME]<br/>
-Sie können das Konto mit folgendem Link aktivieren:<br/>[ACTIVATION_LINK]<br/>Mit freundlichen Grüßen,<br/>[SITENAME]";
+Sie haben diese E-Mail erhalten, weil Sie die E-Mail auf unserer Website nicht aktiviert haben – [SITENAME].<br/>Wenn Sie eine E-Mail nicht innerhalb eines Tages aktivieren, wird Ihre Registrierungsanfrage storniert.<br/>
+Sie haben sich mit folgenden Informationen registriert:<br/>Benutzername: [USER_NAME]<br/>
+Sie können das Konto über den folgenden Link aktivieren:<br/>[ACTIVATION_LINK]<br/>Mit freundlichen Grüßen<br/>[SITENAME]";
