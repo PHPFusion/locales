c25b6ce306960a8ae417cf38fd3c0e3ac63a944d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:44:11 +0800

    New translations user_name_last.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/user_fields/user_name_last.php
+++ Lithuanian/locale/Lithuanian/user_fields/user_name_last.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_last'] = "Pavardė";
+$locale['uf_name_last'] = "Nario pavardė";
 $locale['uf_name_last_desc'] = "Tikroji nario pavardė.";
 $locale['uf_name_last_error'] = "Nario pavardės laukelis negali būti tuščias.";
