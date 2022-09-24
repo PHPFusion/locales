d6e1833054181f079f66098807317757c1d47db4
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:55:08 +0800

    New translations faqs.php (Spanish)

--- Spanish/infusions/faq/locale/Spanish/search/faqs.php
+++ Spanish/infusions/faq/locale/Spanish/search/faqs.php
@@ -1,8 +1,8 @@
 <?php
 $locale['faqs.php'] = "FAQ";
 $locale['fq400'] = "Preguntas Frecuentes";
-$locale['fq401'] = "FAQ";
+$locale['fq401'] = "Pregunta Frecuente";
 $locale['fq402'] = "Preguntas Frecuentes";
-$locale['fq403'] = "encontrado(s) en el";
+$locale['fq403'] = "encontrado en el";
 $locale['fq404'] = "texto de la pregunta";
 $locale['fq405'] = "texto de la respuesta";
