16c2bf2a480e03a859d1df351f18ccbdb0b77122
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:52:36 +0800

    New translations articles.php (Arabic)

--- Arabic/infusions/articles/locale/Arabic/articles.php
+++ Arabic/infusions/articles/locale/Arabic/articles.php
@@ -1,16 +1,16 @@
 <?php
 $locale['article_0000'] = "المقالات";
 $locale['article_0001'] = "كل المقالات";
-$locale['article_0002'] = "عرض التصنيفات";
+$locale['article_0002'] = "إظهار التصنيفات";
 $locale['article_0003'] = "التصنيفات";
-$locale['article_0004'] = "آخر تحديث";
+$locale['article_0004'] = "أخر تحديث";
 $locale['article_0005'] = "التصنيف:";
-$locale['article_0030'] = "الأحدث ";
-$locale['article_0031'] = "الأكثر تعليقاً";
-$locale['article_0032'] = "الأعلى تقييماً";
-$locale['article_0040'] = "العرض بطريقة المعرض";
+$locale['article_0030'] = "أحدث المقالات";
+$locale['article_0031'] = "أكثر تعليقات";
+$locale['article_0032'] = "أعلى تقييم";
+$locale['article_0040'] = "عرض الصور";
 $locale['article_0041'] = "عرض القائمة";
-$locale['article_0060'] = "لا يوجد تصنيفات مقالات معرّفة.";
-$locale['article_0061'] = "لا توجد مقالات معرّفة.";
-$locale['article_0062'] = "لا يوجد أي مقالات في هذا التصنيف.";
+$locale['article_0060'] = "لا توجد مقالات معرّفة في التصانيف.";
+$locale['article_0061'] = "لا توجد مقالات معرّفه.";
+$locale['article_0062'] = "لا توجد مقالات في هذا التصنيف.";
 $locale['article_0063'] = "الأكثر قراءة";
