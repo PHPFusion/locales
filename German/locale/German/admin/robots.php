b4b5f034b0d0e74f9a29867f5fbeb77cd3391c2e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:32:33 +0800

    New translations robots.php (German)

--- German/locale/German/admin/robots.php
+++ German/locale/German/admin/robots.php
@@ -1,12 +1,12 @@
 <?php
-$locale['ROBOT_400'] = "robots.txt-Datei bearbeiten";
-$locale['ROBOT_410'] = "Willst du die robots.txt-Datei wirklich auf den Standard zur&uuml;cksetzen?";
-$locale['ROBOT_411'] = "Es existiert keine Datei namens robots.txt. ";
+$locale['ROBOT_400'] = "robots.txt";
+$locale['ROBOT_410'] = "Bist du sicher? Die Datei robots.txt wird auf den Standardwert gesetzt.";
+$locale['ROBOT_411'] = "robots.txt-Datei existiert nicht. Sie können diese Datei im Stammverzeichnis erstellen.";
 $locale['ROBOT_412'] = "Die robots.txt-Datei wurde aktualisiert.";
-$locale['ROBOT_414'] = "Die robots.txt-Datei ist schreibgesch&uuml;tzt.";
-$locale['ROBOT_416'] = "robots.txt Datei wurde ertellt.";
-$locale['ROBOT_417'] = "Die robots.txt-Datei ist nicht schreibbar.";
-$locale['ROBOT_420'] = "robots.txt-Datei bearbeiten";
-$locale['ROBOT_421'] = "Hilfe und Informationen erh&auml;ltst du hier.";
+$locale['ROBOT_414'] = "die robots.txt-Datei ist nicht beschreibbar.";
+$locale['ROBOT_416'] = "robots.txt Datei wurde erstellt.";
+$locale['ROBOT_417'] = "robots.txt Inhalt falsch. Bitte verwenden Sie einen gültigen Inhalt.";
+$locale['ROBOT_420'] = "Bearbeiten Sie Ihre robots.txt-Datei";
+$locale['ROBOT_421'] = "[LINK]Weitere Informationen und Hilfe zur robots.txt-Datei finden Sie hier.[/LINK]";
 $locale['ROBOT_422'] = "Neue robots.txt Datei erstellt";
-$locale['ROBOT_423'] = "Standard wiederherstellen";
+$locale['ROBOT_423'] = "Zurücksetzen auf Standard";
