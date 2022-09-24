905873d5bebd5635fa90a53a50bc1f2aac733eae
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:56:57 +0800

    New translations forum.php (Slovak)

--- Slovak/infusions/forum/locale/Slovak/search/forum.php
+++ Slovak/infusions/forum/locale/Slovak/search/forum.php
@@ -1,9 +1,9 @@
 <?php
-$locale['forum.php'] = "Fórum";
+$locale['forum.php'] = "Fóra";
 $locale['f400'] = "Príspevky fóra";
 $locale['f401'] = "všetky fóra";
 $locale['f402'] = "Príspevok fóra";
-$locale['f403'] = "Príspevky fóra";
+$locale['f403'] = "príspevkov fóra";
 $locale['f404'] = "Pripnuté:";
 $locale['f405'] = "Oznámenie:";
 $locale['f406'] = "nájdené v";
