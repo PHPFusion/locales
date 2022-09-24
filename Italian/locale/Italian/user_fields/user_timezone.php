c3116bb562615b57c7b128d8d0851317da4f1668
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:46:14 +0800

    New translations user_timezone.php (Italian)

--- Italian/locale/Italian/user_fields/user_timezone.php
+++ Italian/locale/Italian/user_fields/user_timezone.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_timezone'] = "Timezone";
+$locale['uf_timezone'] = "Fuso orario";
 $locale['uf_timezone_desc'] = "Fuso orario definito dall'utente.";
 $locale['uf_timezone_error'] = "Fuso orario non impostato correttamente.";
