9aa790a2522bcd254171d2f44f8d5ebe23eb0b5f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:47:20 +0800

    New translations comments.php (German)

--- German/locale/German/comments.php
+++ German/locale/German/comments.php
@@ -1,14 +1,14 @@
 <?php
 $locale['c100'] = "Kommentare";
 $locale['c101'] = "Es wurden keine Kommentare geschrieben.";
-$locale['c102'] = "Kommentar hinzuf&uuml;gen";
+$locale['c102'] = "Kommentar abschicken";
 $locale['c103'] = "Kommentar aktualisieren.";
 $locale['c104'] = "Name:";
 $locale['c105'] = "Bitte anmelden, um einen Kommentar zu schreiben.";
 $locale['c106'] = "Kommentare verwalten";
 $locale['c107'] = " Deaktivieren von Smileys in diesem Kommentar";
-$locale['c110'] = "Diesen Kommentar l&ouml;schen?";
+$locale['c110'] = "Diesen Kommentar löschen?";
 $locale['c111'] = "Einen Kommentar hinterlassen";
 $locale['c112'] = "Antworten";
-$locale['c113'] = "Titel";
-$locale['c114'] = "Kommentar wurde aktualisiert";
+$locale['c113'] = "Name";
+$locale['c114'] = "Kommentare wurden aktualisiert";
