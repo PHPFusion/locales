86a3458a7039a4edf85d4964d45b48572d9b417b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:37:30 +0800

    New translations search.php (Turkish)

--- Turkish/locale/Turkish/bbcodes/search.php
+++ Turkish/locale/Turkish/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Sitede Yazılan Metni Arar";
 $locale['bb_search_usage'] = "Aranacak Metin";
 $locale['bb_search_prefix'] = "Aranacak Kelime: ";
-$locale['bb_search_suffix'] = " Tarih: ";
+$locale['bb_search_suffix'] = " Aranacak Yer: ";
 $locale['bb_search_where'] = "arama_bolumu";
