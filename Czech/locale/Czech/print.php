357831bfcaf9710bf3e374b3d11c0f880104d7d2
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:33 +0800

    New translations print.php (Czech)

--- Czech/locale/Czech/print.php
+++ Czech/locale/Czech/print.php
@@ -1,12 +1,12 @@
 <?php
 //news, articles, blog
-$locale['400'] = "Přidal/a ";
+$locale['400'] = "Přidal ";
 $locale['401'] = " dne ";
 $locale['402'] = "Rozšířené novinky";
 $locale['403'] = "Rozšířený blog";
 //forums
 $locale['500'] = "Předmět vlákna:";
-$locale['501'] = "Přidal/a ";
+$locale['501'] = "Odeslánoy ";
 $locale['502'] = " dne ";
 $locale['503'] = "Upraveno ";
 //License Agreement
