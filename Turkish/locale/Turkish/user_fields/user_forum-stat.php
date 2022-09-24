5ba0aba43a9735f46e929f001329214174b3db6c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:33 +0800

    New translations user_forum-stat.php (Turkish)

--- Turkish/locale/Turkish/user_fields/user_forum-stat.php
+++ Turkish/locale/Turkish/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Forum Mesajlar";
+$locale['uf_forum-stat'] = "Forum Mesajları";
 $locale['uf_forum-stat_desc'] = "Forum mesajlarının sayısını görüntüler.";
 $locale['uf_forum-stat_na'] = "Lütfen Forum uygulamasını yükleyin.";
