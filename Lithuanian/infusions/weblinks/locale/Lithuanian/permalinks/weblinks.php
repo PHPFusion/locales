d52f7cfbba8d6f92f63fcd4eb01605e3687dd091
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:00:51 +0800

    New translations weblinks.php (Lithuanian)

--- Lithuanian/infusions/weblinks/locale/Lithuanian/permalinks/weblinks.php
+++ Lithuanian/infusions/weblinks/locale/Lithuanian/permalinks/weblinks.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_weblinks_title'] = "Nuorodos";
+$locale['pl_weblinks_title'] = "Tinklapio nuorodos";
 $locale['pl_weblinks_desc'] = "Įjungti pastovias nuorodas tinklapio nuorodoms.";
