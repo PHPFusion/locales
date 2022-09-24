85d816cf5599199664113d563334654a1b7b983c
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:53:11 +0800

    New translations blog.php (Belarusian)

--- Belarusian/infusions/blog/locale/Belarusian/permalinks/blog.php
+++ Belarusian/infusions/blog/locale/Belarusian/permalinks/blog.php
@@ -1,5 +1,5 @@
 <?php
 $locale['pl_blog_title'] = "Blog";
-$locale['pl_blog_desc'] = "Enable Permalinks for the Blog on the site.";
-$locale['pl_tags_001'] = "ID of the Blog Item.";
-$locale['pl_tags_002'] = "Title or Subject of the Blog Item.";
+$locale['pl_blog_desc'] = "Уключэнне нязменных спасылак для блога на сайце.";
+$locale['pl_tags_001'] = "Ідэнтыфікатар запісу блога.";
+$locale['pl_tags_002'] = "Загаловак ці тэма запісу блога.";
