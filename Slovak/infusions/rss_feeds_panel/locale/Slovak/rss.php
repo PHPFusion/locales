f4190647e3e003f719ea8d6e399636eae6f6096e
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:00:14 +0800

    New translations rss.php (Slovak)

--- Slovak/infusions/rss_feeds_panel/locale/Slovak/rss.php
+++ Slovak/infusions/rss_feeds_panel/locale/Slovak/rss.php
@@ -2,7 +2,7 @@
 $locale['rss_title'] = "RSS kanály";
 $locale['rss_articles'] = "Články";
 $locale['rss_blog'] = "Blog";
-$locale['rss_downloads'] = "Na stiahnutie";
+$locale['rss_downloads'] = "Súbory na stiahnutie";
 $locale['rss_forums'] = "Fórum";
 $locale['rss_news'] = "Novinky";
 $locale['rss_weblinks'] = "Odkazy";
