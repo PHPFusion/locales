9fc0985aa21268927638d45db9c90495903ccc6f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:41:56 +0800

    New translations user_delete.php (Turkish)

--- Turkish/locale/Turkish/user_fields/user_delete.php
+++ Turkish/locale/Turkish/user_fields/user_delete.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_delete'] = "Kullanıcıyı Sil";
-$locale['uf_delete_desc'] = "Kullanıcıyı Sil";
+$locale['uf_delete'] = "Kullanıcı Sil";
+$locale['uf_delete_desc'] = "Kullanıcı Sil";
 $locale['uf_delete_del'] = "Beni Sil";
 $locale['uf_delete_exit'] = "Hesap Silindi...";
