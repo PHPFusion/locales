22bf568ef507b723a1c9c154aad662c23f30bf79
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:49:43 +0800

    New translations lostpassword.php (Spanish)

--- Spanish/locale/Spanish/lostpassword.php
+++ Spanish/locale/Spanish/lostpassword.php
@@ -5,7 +5,7 @@ $locale['402'] = "Tu nueva contraseña ha sido enviada a tu dirección de email.
 $locale['403'] = "Volver al Sitio";
 $locale['404'] = "No se ha podido encontrar la dirección de email que has indicado.";
 $locale['405'] = "La dirección de email que has indicado no es válida.";
-$locale['406'] = "Please try again.";
+$locale['406'] = "Vuelve a intentarlo.";
 $locale['407'] = "Indica la dirección de email con la que registraste tu cuenta.
 Se creará una nueva contraseña y se enviará a esa dirección.";
 $locale['408'] = "Enviar Contraseña";
