bee0c8eb6db9743c9c22e4aaa2b346dd5a44ea92
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:57 +0800

    New translations user_name_middle.php (Arabic)

--- Arabic/locale/Arabic/user_fields/user_name_middle.php
+++ Arabic/locale/Arabic/user_fields/user_name_middle.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_middle'] = "الاسم الأوسط";
+$locale['uf_name_middle'] = "الاسم الثاني";
 $locale['uf_name_middle_desc'] = "الإسم الثاني، اسم الأب.";
 $locale['uf_name_middle_error'] = "الرجاء إدخال جميع البيانات المطلوبة..";
