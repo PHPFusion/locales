c7f23b4a2725bbb66ccf3bfb20425995cb68c039
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:52:04 +0800

    New translations submissions.php (Polish)

--- Polish/locale/Polish/submissions.php
+++ Polish/locale/Polish/submissions.php
@@ -1,6 +1,6 @@
 <?php
-$locale['article_submit'] = "%s artykuł";
-$locale['blog_submit'] = "%s wpis";
-$locale['download_submit'] = "%s plik";
-$locale['gallery_submit'] = "%s zdjęcie";
+$locale['article_submit'] = "%s artykuły";
+$locale['blog_submit'] = "%s wpisów";
+$locale['download_submit'] = "%s plików";
+$locale['gallery_submit'] = "%s zdjęć";
 $locale['news_submit'] = "%s aktualności";
