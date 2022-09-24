b353acff369c2c3e5feb2e0b6ddf0ac9e60b8830
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:29:19 +0800

    New translations admins.php (Chinese Simplified)

--- Chinese Simplified/locale/Chinese Simplified/admin/admins.php
+++ Chinese Simplified/locale/Chinese Simplified/admin/admins.php
@@ -6,14 +6,14 @@ $locale['ADM_402'] = "管理员已删除";
 // Search/Add Administrator
 $locale['ADM_410'] = "添加管理员";
 $locale['ADM_411'] = "请搜索要添加的用户";
-$locale['ADM_413'] = "User Name";
+$locale['ADM_413'] = "用户名";
 $locale['ADM_415'] = "应用所有权限";
 $locale['ADM_416'] = "创建超级管理员";
 $locale['ADM_418'] = "找不到用户";
 $locale['ADM_419'] = "再次搜索";
 // Current Administrators
-$locale['ADM_420'] = "Administrators";
-$locale['ADM_421'] = "User Name";
+$locale['ADM_420'] = "管理員";
+$locale['ADM_421'] = "用户名";
 $locale['ADM_422'] = "管理级别";
 $locale['ADM_423'] = "设置";
 $locale['ADM_425'] = "没有权利";
@@ -22,10 +22,10 @@ $locale['ADM_440'] = "编辑管理员权利";
 $locale['ADM_441'] = "内容管理";
 $locale['ADM_442'] = "用户管理";
 $locale['ADM_443'] = "系统管理";
-$locale['ADM_444'] = "Infusions";
-$locale['ADM_445'] = "Check all";
+$locale['ADM_444'] = "聚合";
+$locale['ADM_445'] = "选择全部";
 $locale['ADM_448'] = "保存管理员权利";
-$locale['ADM_449'] = "Settings";
+$locale['ADM_449'] = "设置";
 $locale['ADM_450'] = "勾选安全";
 // Popup Error/Warning Messages
 $locale['ADM_460'] = "删除此管理员？";
