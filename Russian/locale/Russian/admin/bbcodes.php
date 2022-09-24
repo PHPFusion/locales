960f554a65902de92efaa214a246c01e450b030f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:29:11 +0800

    New translations bbcodes.php (Russian)

--- Russian/locale/Russian/admin/bbcodes.php
+++ Russian/locale/Russian/admin/bbcodes.php
@@ -1,5 +1,5 @@
 <?php
-$locale['BBCA_400'] = "Коды BB";
+$locale['BBCA_400'] = "Управление BB кодами";
 $locale['BBCA_400a'] = "BB коды админа";
 $locale['BBCA_401'] = "Проверка BB кодов";
 // Enabled BB Codes
@@ -11,11 +11,11 @@ $locale['BBCA_406'] = "Использование";
 $locale['BBCA_407'] = "Порядок";
 $locale['BBCA_408'] = "Вниз";
 $locale['BBCA_409'] = "Вверх";
-$locale['BBCA_410'] = "Запретить";
+$locale['BBCA_410'] = "Выключить";
 $locale['BBCA_411'] = "Нет разрешенных BB кодов.";
 // Disabled BB Codes
 $locale['BBCA_413'] = "Отключенные";
-$locale['BBCA_414'] = "Разрешить";
+$locale['BBCA_414'] = "Включить";
 $locale['BBCA_416'] = "Все доступные BB коды включены.";
 // Test BB Codes
 $locale['BBCA_417'] = "Проверка отображения BB кодов";
