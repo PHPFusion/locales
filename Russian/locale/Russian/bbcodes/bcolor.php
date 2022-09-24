ea03acef3f612b18ccae13277963f296f37a9925
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:34:43 +0800

    New translations bcolor.php (Russian)

--- Russian/locale/Russian/bbcodes/bcolor.php
+++ Russian/locale/Russian/bbcodes/bcolor.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_bcolor_description'] = "Цвет фона для выделенного текста";
-$locale['bb_bcolor_usage'] = "Цвет фона для выделенного текста";
+$locale['bb_bcolor_usage'] = "Окрашивание фона для выделенного текста";
 $locale['bb_bcolor_hex'] = "HEX-значение для цвета фона";
