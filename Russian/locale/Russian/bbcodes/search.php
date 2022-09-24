914dfe36d391c503d0d855e5d46f453dc22d1230
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:37:27 +0800

    New translations search.php (Russian)

--- Russian/locale/Russian/bbcodes/search.php
+++ Russian/locale/Russian/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Поиск заданного текста по этому сайту";
 $locale['bb_search_usage'] = "Текст для поиска";
 $locale['bb_search_prefix'] = "Поиск ";
-$locale['bb_search_suffix'] = " в ";
+$locale['bb_search_suffix'] = " на ";
 $locale['bb_search_where'] = "Где";
