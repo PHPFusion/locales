aae0aed10c4ac060cbf592474eae93a1d028ebcb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:58:06 +0800

    New translations photos.php (Norwegian)

--- Norwegian/infusions/gallery/locale/Norwegian/search/photos.php
+++ Norwegian/infusions/gallery/locale/Norwegian/search/photos.php
@@ -1,9 +1,9 @@
 <?php
 $locale['photos.php'] = "Photo Gallery";
-$locale['p400'] = "Bilder:";
+$locale['p400'] = "Bilder";
 $locale['p401'] = "Bilde";
-$locale['p402'] = "Bilder:";
+$locale['p402'] = "Bilder";
 $locale['p403'] = "[NY]";
 $locale['p404'] = "Album:";
 $locale['p405'] = "Lagt til dato:";
-$locale['p406'] = "Views:";
+$locale['p406'] = "Visninger:";
