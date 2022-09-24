08d671fdda0d40752aed10bad3037123b27bbe08
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:58:55 +0800

    New translations poll.php (Portuguese)

--- Portuguese/infusions/member_poll_panel/locale/Portuguese/permalinks/poll.php
+++ Portuguese/infusions/member_poll_panel/locale/Portuguese/permalinks/poll.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_poll_title'] = "Votações de Membros";
+$locale['pl_poll_title'] = "Votação dos membros";
 $locale['pl_poll_desc'] = "Habilitar Permalinks para as Votações de Membros no site.";
