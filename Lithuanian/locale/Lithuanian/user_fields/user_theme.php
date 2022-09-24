e20a6c0cf2ab0f84490776c3c87950bbf661d7e5
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:45:48 +0800

    New translations user_theme.php (Lithuanian)

--- Lithuanian/locale/Lithuanian/user_fields/user_theme.php
+++ Lithuanian/locale/Lithuanian/user_fields/user_theme.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_theme'] = "Dizainas";
+$locale['uf_theme'] = "Tema";
 $locale['uf_theme_desc'] = "Leid�ia nariams keisti temas.";
 $locale['uf_theme_error'] = "Tema negali b�ti tu��ia.";
