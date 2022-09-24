84fd837edbb4b1ae9c9946fcf6814ae89abc7ff8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:34:15 +0800

    New translations bcolor.php (Romanian)

--- Romanian/locale/Romanian/bbcodes/bcolor.php
+++ Romanian/locale/Romanian/bbcodes/bcolor.php
@@ -1,4 +1,4 @@
 <?php
 $locale['bb_bcolor_description'] = "Culoarea de fundal pentru textul selectat";
-$locale['bb_bcolor_usage'] = "Culoarea de fundal pentru textul selectat";
+$locale['bb_bcolor_usage'] = "Culoarea de fundal pentrul textul selectat";
 $locale['bb_bcolor_hex'] = "Valoarea hexa pentru culoarea de fundal";
