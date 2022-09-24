2421c53191b646926c64935fe2b3ef6d2ab221f8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:42:31 +0800

    New translations user_forum-stat.php (Russian)

--- Russian/locale/Russian/user_fields/user_forum-stat.php
+++ Russian/locale/Russian/user_fields/user_forum-stat.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_forum-stat'] = "Сообщения форума";
+$locale['uf_forum-stat'] = "Сообщений на форуме";
 $locale['uf_forum-stat_desc'] = "Отображает количество сообщений на форуме.";
 $locale['uf_forum-stat_na'] = "Пожалуйста, установите инфузию форума.";
