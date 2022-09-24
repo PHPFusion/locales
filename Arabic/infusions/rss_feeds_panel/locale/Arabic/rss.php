ae272e8c83add12ffcf17a9d929eb0b6d6341290
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:59:52 +0800

    New translations rss.php (Arabic)

--- Arabic/infusions/rss_feeds_panel/locale/Arabic/rss.php
+++ Arabic/infusions/rss_feeds_panel/locale/Arabic/rss.php
@@ -1,10 +1,10 @@
 <?php
-$locale['rss_title'] = "تغذيات RSS";
+$locale['rss_title'] = "بمقتطفات الأخبار أر أس أس";
 $locale['rss_articles'] = "المقالات";
 $locale['rss_blog'] = "المدونة";
 $locale['rss_downloads'] = "ملفات التحميل";
 $locale['rss_forums'] = "المنتديات";
-$locale['rss_news'] = "الأخبار";
+$locale['rss_news'] = "أخبار";
 $locale['rss_weblinks'] = "الروابط";
 $locale['rss_in'] = " في ";
-$locale['rss_nodata'] = "لا تتوفر بيانات RSS";
+$locale['rss_nodata'] = "لا توجد بيانات متاحة لمقتطفات الأخبار أر أس أس";
