5174cd2f7851572b380618599ae9179727fef7bb
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:41:01 +0800

    New translations user_blacklist.php (Spanish)

--- Spanish/locale/Spanish/user_fields/user_blacklist.php
+++ Spanish/locale/Spanish/user_fields/user_blacklist.php
@@ -1,5 +1,5 @@
 <?php
-$locale['uf_blacklist'] = "Expulsiones";
+$locale['uf_blacklist'] = "Lista negra";
 $locale['uf_blacklist_desc'] = "Privacidad usuario listas negras";
 $locale['uf_blacklist_error'] = "Error en tipo de datos";
 $locale['uf_blacklist_message'] = "Una vez que bloquee a alguien, esa persona ya no puede ver incluyendo cosas que publica y en contacto con usted.";
