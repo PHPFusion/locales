defdd0c684176f063abe4d0fe298be8d69afb239
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:43:37 +0800

    New translations user_name_first.php (Portuguese)

--- Portuguese/locale/Portuguese/user_fields/user_name_first.php
+++ Portuguese/locale/Portuguese/user_fields/user_name_first.php
@@ -1,4 +1,4 @@
 <?php
-$locale['uf_name_first'] = "Primeiro nome";
+$locale['uf_name_first'] = "Nome";
 $locale['uf_name_first_desc'] = "Nome de usuário real (John, Mary etc.).";
 $locale['uf_name_first_error'] = "O Nome real do utilizador não pode ser deixado em branco.";
