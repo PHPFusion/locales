09918eea4a0fb315db0a260affd9f50a92c891fd
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:36:58 +0800

    New translations search.php (Spanish)

--- Spanish/locale/Spanish/bbcodes/search.php
+++ Spanish/locale/Spanish/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Buscador interno del Texto indicado";
 $locale['bb_search_usage'] = "Texto a buscar";
 $locale['bb_search_prefix'] = "Buscar ";
-$locale['bb_search_suffix'] = " el ";
+$locale['bb_search_suffix'] = " en ";
 $locale['bb_search_where'] = "Donde";
