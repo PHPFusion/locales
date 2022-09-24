2088497a33099210902c05cfcd6d205490e50e69
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:35:34 +0800

    New translations hide.php (German)

--- German/locale/German/bbcodes/hide.php
+++ German/locale/German/bbcodes/hide.php
@@ -1,4 +1,4 @@
 <?php
-$locale['bb_hide'] = "Nachricht f&uuml;r Administratoren";
-$locale['bb_hide_description'] = "Zeigt die Nachricht nur f&uuml;r Administratoren an.";
-$locale['bb_hide_usage'] = "Text";
+$locale['bb_hide'] = "Nachricht für Admins";
+$locale['bb_hide_description'] = "Nachricht nur für Admins sichtbar";
+$locale['bb_hide_usage'] = "Text nur für Admins sichtbar";
