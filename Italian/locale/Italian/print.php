ed6de2b6051c2812e69399b7d987b9ccb46074ac
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:50:39 +0800

    New translations print.php (Italian)

--- Italian/locale/Italian/print.php
+++ Italian/locale/Italian/print.php
@@ -1,12 +1,12 @@
 <?php
 //news, articles, blog
-$locale['400'] = "Inviato da ";
+$locale['400'] = "Scritto da ";
 $locale['401'] = " il ";
 $locale['402'] = "Notizia estesa";
 $locale['403'] = "Esteso Blog";
 //forums
 $locale['500'] = "Oggetto dell' argomento:";
-$locale['501'] = "Inviato da ";
+$locale['501'] = "Scritta da ";
 $locale['502'] = " il ";
 $locale['503'] = "Modificata da ";
 //License Agreement
