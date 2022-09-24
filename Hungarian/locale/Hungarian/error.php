6d7a18b07e9da5a545c3b9ea746f70a7cfdd2c2a
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:48:41 +0800

    New translations error.php (Hungarian)

--- Hungarian/locale/Hungarian/error.php
+++ Hungarian/locale/Hungarian/error.php
@@ -4,4 +4,4 @@ $locale['err403'] = "Hozzáférés megtagadva!";
 $locale['err404'] = "Ez az oldal nem található ezen a szerveren!";
 $locale['errunk'] = "Ismeretlen hiba";
 $locale['errmsg'] = "A link lejárt, az oldal nem lesz elérhető az Ön számára.";
-$locale['errret'] = "Vissza a főoldalra";
+$locale['errret'] = "Vissza a Főoldalra";
