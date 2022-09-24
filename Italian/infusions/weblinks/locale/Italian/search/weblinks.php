c93ec07d1fb4505afa15d6ae6444411ff6865f62
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 00:00:43 +0800

    New translations weblinks.php (Italian)

--- Italian/infusions/weblinks/locale/Italian/search/weblinks.php
+++ Italian/infusions/weblinks/locale/Italian/search/weblinks.php
@@ -1,8 +1,8 @@
 <?php
 $locale['weblinks.php'] = "Link del Web";
-$locale['w400'] = "WebLinks";
+$locale['w400'] = "Web link";
 $locale['w401'] = "Web link";
-$locale['w402'] = "WebLinks";
+$locale['w402'] = "Web link";
 $locale['w403'] = "[NUOVO]";
 $locale['w404'] = "Data inserimento:";
 $locale['w405'] = "Visite:";
