0f018e24cda8fc5cc9cc1e6213d5658d398038f8
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:38:20 +0800

    New translations ulist.php (Italian)

--- Italian/locale/Italian/bbcodes/ulist.php
+++ Italian/locale/Italian/bbcodes/ulist.php
@@ -1,6 +1,6 @@
 <?php
 $locale['bb_ulist_description'] = "Crea un elenco puntato non ordinato";
-$locale['bb_ulist_usage'] = "Elemento 1 :enter: Elemento 2 :enter: [..]";
+$locale['bb_ulist_usage'] = "Primo elemento :enter: Secondo elemento :enter: [..]";
 $locale['bb_ulist_1'] = "disco";
 $locale['bb_ulist_2'] = "cerchio";
 $locale['bb_ulist_3'] = "quadrato";
