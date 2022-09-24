1d916cde7b2487d9bd6ddeb9cfac6b4b75dd0d21
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:37:22 +0800

    New translations search.php (Polish)

--- Polish/locale/Polish/bbcodes/search.php
+++ Polish/locale/Polish/bbcodes/search.php
@@ -2,5 +2,5 @@
 $locale['bb_search_description'] = "Wyszukiwanie tekstu w bieżącej witrynie";
 $locale['bb_search_usage'] = "Tekst do wyszukania";
 $locale['bb_search_prefix'] = "Szukaj ";
-$locale['bb_search_suffix'] = " dnia ";
+$locale['bb_search_suffix'] = " w: ";
 $locale['bb_search_where'] = "Gdzie";
