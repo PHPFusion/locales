e98d27b7225621cc15282b170fdadef55a09f209
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:59:30 +0800

    New translations news.php (Norwegian)

--- Norwegian/infusions/news/locale/Norwegian/news.php
+++ Norwegian/infusions/news/locale/Norwegian/news.php
@@ -1,17 +1,17 @@
 <?php
-$locale['news_0001'] = "Forsett å Lese";
+$locale['news_0001'] = "Fortsett å lese";
 $locale['news_0002'] = "Skriv ut";
 $locale['news_0004'] = "Nyheter";
-$locale['news_0005'] = "Det er ikke skrevet noen nyheter enda";
-$locale['news_0006'] = "Ukategoriserte";
-$locale['news_0008'] = "Sis Oppdaterte";
+$locale['news_0005'] = "Ingen Nyheter har blitt skrevet ennå";
+$locale['news_0006'] = "Ukategorisert";
+$locale['news_0008'] = "Sist Oppdatert";
 $locale['news_0009'] = "Nyhets Kategorier";
 $locale['news_0010'] = "Alle Andre Kategorier";
-$locale['news_0011'] = "Siste Nytt";
+$locale['news_0011'] = "Aller Siste Nyhet";
 $locale['news_0012'] = "Mest Kommenterte Nyhet";
 $locale['news_0013'] = "Topp Avstemt Nyhet";
-$locale['news_0014'] = "Miniatyr utseende";
-$locale['news_0015'] = "Liste utseende";
+$locale['news_0014'] = "Miniatyr visning";
+$locale['news_0015'] = "Listevisning";
 $locale['news_0016'] = "No categories defined.";
 $locale['news_0017'] = "Filtrer etter:";
 $locale['news_0018'] = "Show all news";
