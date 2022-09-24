6a053f8f7a3ba265540ccb3c7002880a70f952e6
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:39:51 +0800

    New translations search.php (Romanian)

--- Romanian/locale/Romanian/permalinks/search.php
+++ Romanian/locale/Romanian/permalinks/search.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_search_title'] = "Search";
+$locale['pl_search_title'] = "C&#259;utare";
 $locale['pl_search_desc'] = "Activeaz&#259; leg&#259;turile permanente pentru c&#259;utare.";
