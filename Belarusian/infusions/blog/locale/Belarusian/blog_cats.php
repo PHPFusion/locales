19a315af5e2ff4aaa94fb3357f273cf2314adc0d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-24 23:53:45 +0800

    New translations blog_cats.php (Belarusian)

--- Belarusian/infusions/blog/locale/Belarusian/blog_cats.php
+++ Belarusian/infusions/blog/locale/Belarusian/blog_cats.php
@@ -1,9 +1,9 @@
 <?php
 $locale['400'] = "Blog Categories";
-$locale['401'] = "Category:";
-$locale['402'] = "Number of Items:";
-$locale['403'] = "None-Categorised Blogs";
-$locale['404'] = "There are no Blog items in this category.";
+$locale['401'] = "Катэгорыя:";
+$locale['402'] = "Колькасць запісаў:";
+$locale['403'] = "Некатэгарызаваныя запісы";
+$locale['404'] = "У гэтай катэгорыі запісаў блогаў няма.";
 $locale['405'] = "More";
-$locale['406'] = "Show All Categories";
-$locale['407'] = "No Blog Categories defined";
+$locale['406'] = "Паказаць усе катэгорыі";
+$locale['407'] = "Створаных катэгорый запісаў няма";
