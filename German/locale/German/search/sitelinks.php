d2e79506072e0d33e218a2f67a4a99600feb2503
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 23:26:11 +0800

    New translations sitelinks.php (German)

--- German/locale/German/search/sitelinks.php
+++ German/locale/German/search/sitelinks.php
@@ -1,7 +1,7 @@
 <?php
-$locale['s400'] = "Navigationslinks";
-$locale['s401'] = "Navigationslinks";
-$locale['s402'] = "Seitenlink";
+$locale['s400'] = "Seiten links";
+$locale['s401'] = "Seiten links";
+$locale['s402'] = "Seiten link";
 $locale['s403'] = "gefunden in";
-$locale['s404'] = "Seiten-Linktitel";
+$locale['s404'] = "Seiten-Link Name";
 $locale['s405'] = "Seiten-Link-URL";
