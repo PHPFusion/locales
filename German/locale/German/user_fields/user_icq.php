b89ed8a15c9e58af5d07d9c1853b2ef7b69fa390
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-25 23:26:02 +0800

    New translations user_icq.php (German)

--- German/locale/German/user_fields/user_icq.php
+++ German/locale/German/user_fields/user_icq.php
@@ -3,4 +3,4 @@ $locale['uf_icq'] = "ICQ#";
 // ICQ is UIN not ID. - https://en.wikipedia.org/wiki/ICQ#UIN
 $locale['uf_icq_desc'] = "ICQ UIN";
 $locale['uf_icq_error'] = "ICQ darf nicht leer gelassen werden.";
-$locale['uf_icq_error_1'] = "Ungültige ICQ UIN. Bitte verwenden Sie eine gültige ICQ Benutzeridentifikationsnummer.";
+$locale['uf_icq_error_1'] = "Ungültige ICQ-UIN. Bitte verwenden Sie eine gültige ICQ-Benutzer Identifikationsnummer.";
