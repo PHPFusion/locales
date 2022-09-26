d3cda8054b6a2546ec479ede98c5806ac6842a53
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-26 17:25:47 +0800

    New translations user_timezone.php (German)

--- German/locale/German/user_fields/user_timezone.php
+++ German/locale/German/user_fields/user_timezone.php
@@ -1,4 +1,4 @@
 <?php
 $locale['uf_timezone'] = "Zeitzone";
-$locale['uf_timezone_desc'] = "Zeitzone deines Wohnortes.";
-$locale['uf_timezone_error'] = "Die Zeitzone wurde nicht korrekt eingetragen.";
+$locale['uf_timezone_desc'] = "Benutzerdefinierte Zeitzone.";
+$locale['uf_timezone_error'] = "Die Zeitzone wurde nicht richtig eingestellt.";
