15c7b571628518d889cc9c0c301a8eace540e10f
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-27 16:39:21 +0800

    New translations faq.php (German)

--- German/infusions/faq/locale/German/permalinks/faq.php
+++ German/infusions/faq/locale/German/permalinks/faq.php
@@ -1,3 +1,3 @@
 <?php
-$locale['pl_faq_cat_title'] = "FAQs - H&auml;ufig gestellte Fragen";
-$locale['pl_faq_cat_desc'] = "Aktiviert die Permalinks f&uuml;r die FAQ's auf der Homepage.";
+$locale['pl_faq_cat_title'] = "FAQs";
+$locale['pl_faq_cat_desc'] = "Aktivieren Sie den FAQ-Kategorie-Permalink auf der Seite.";
