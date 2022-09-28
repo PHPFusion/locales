18501e744b1ba9f0428b7bb751103f8e33f29f06
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-28 17:09:15 +0800

    New translations !autolink.php (Chinese Simplified)

--- Chinese Simplified/locale/Chinese Simplified/bbcodes/!autolink.php
+++ Chinese Simplified/locale/Chinese Simplified/bbcodes/!autolink.php
@@ -1,3 +1,3 @@
 <?php
-$locale['bb_autolink_description'] = "Turns links or email addresses into clickable hyperlinks.";
-$locale['bb_autolink_usage'] = "This bbcode has no button. MUST be above URL and Mail bbcode.";
+$locale['bb_autolink_description'] = "将链接或电子邮件地址转换为可点击的链接。";
+$locale['bb_autolink_usage'] = "这个BB码没有按钮。必须将高于URL和邮件BB码";
