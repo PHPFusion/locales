1ccaa6518c905bdf0cab608d5615c2d5f9dd6995
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-28 17:09:19 +0800

    New translations forum.php (German)

--- German/infusions/forum/locale/German/search/forum.php
+++ German/infusions/forum/locale/German/search/forum.php
@@ -1,11 +1,11 @@
 <?php
 $locale['forum.php'] = "Foren";
-$locale['f400'] = "Foren";
+$locale['f400'] = "Forum Beiträge";
 $locale['f401'] = "Alle Foren";
-$locale['f402'] = "Forenbeitrag";
-$locale['f403'] = "Forenbeitr&auml;ge";
+$locale['f402'] = "Forum Beitrag";
+$locale['f403'] = "Forum Beiträge";
 $locale['f404'] = "Gepinnt:";
-$locale['f405'] = "Ank&uuml;ndigung:";
+$locale['f405'] = "Ankündigung:";
 $locale['f406'] = "gefunden in";
-$locale['f407'] = "Betreff";
-$locale['f408'] = "Nachricht";
+$locale['f407'] = "betreff angeben";
+$locale['f408'] = "nachricht senden";
