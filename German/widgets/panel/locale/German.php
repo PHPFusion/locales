b7745aeb711aaecb235e2c3681401e81eae5be3d
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-28 17:09:03 +0800

    New translations English.php (German)

--- German/widgets/panel/locale/German.php
+++ German/widgets/panel/locale/German.php
@@ -1,8 +1,8 @@
 <?php
-$locale['PW_0100'] = "Panel";
-$locale['PW_0101'] = "PHPFusion-Panels";
-$locale['PW_0200'] = "Installiertes Panel auswählen";
-$locale['PW_0201'] = "Bitte überprüfen Sie die installierten Panel Zugriffseinstellungen in [LINK]Panel Administration[/LINK]";
-$locale['PW_0220'] = "Panel speichern";
-$locale['PW_0221'] = "Panel speichern und schließen";
-$locale['PW_0222'] = "Panel-Dateiname";
+$locale['PW_0100'] = "Paneel";
+$locale['PW_0101'] = "PHPFusion-Paneele";
+$locale['PW_0200'] = "Wählen Sie Installiertes Paneel";
+$locale['PW_0201'] = "Bitte überprüfen Sie die installierten Paneel Zugriffseinstellungen in [LINK]Paneel Administration[/LINK]";
+$locale['PW_0220'] = "Paneel speichern";
+$locale['PW_0221'] = "Paneel speichern und schließen";
+$locale['PW_0222'] = "Paneel-Dateiname";
