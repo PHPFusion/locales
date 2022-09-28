1b600d0e3485c173edc1d9918bd2fd51ebfb3a02
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-28 17:09:02 +0800

    New translations English.php (German)

--- German/widgets/file/locale/German.php
+++ German/widgets/file/locale/German.php
@@ -1,8 +1,8 @@
 <?php
-$locale['f0100'] = "Titel";
+$locale['f0100'] = "Name";
 $locale['f0102'] = "Dateipfad vom Root-Verzeichnis";
 $locale['f0103'] = "Datei speichern";
-$locale['f0104'] = "Speichern und schliessen";
+$locale['f0104'] = "Datei speichern und schließen";
 $locale['f0105'] = "WARNUNG: Datei-Inklusion nicht gefunden";
-$locale['f0106'] = "Server Hosted Datei verwenden";
+$locale['f0106'] = "Server gehostete Datei verwenden";
 $locale['f0107'] = "Datei-Widget";
