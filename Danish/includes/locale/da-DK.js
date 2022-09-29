25a6f8eac85ef24e9faf9bb0d3c6b0dd6eaa419b
Frederick MC Chan <meangczac.chan@gmail.com>
2022-09-29 17:17:43 +0800

    New translations en-gb.js (Danish)

--- Danish/includes/locale/da-DK.js
+++ Danish/includes/locale/da-DK.js
@@ -5,45 +5,45 @@
 (function (global, factory) {
    typeof exports === 'object' && typeof module !== 'undefined' && typeof require === 'function' ? factory(require('../moment')) : typeof define === 'function' && define.amd ? define(['../moment'], factory) : factory(global.moment);
 }(this, function (moment) {
-   'use strict';
+   'brug streng';
    //! moment.js locale configuration
    var enGb = moment.defineLocale('en-gb', {
-      months: 'January_February_March_April_May_June_July_August_September_October_November_December'.split('_'),
-      monthsShort: 'Jan_Feb_Mar_Apr_May_Jun_Jul_Aug_Sep_Oct_Nov_Dec'.split('_'),
-      weekdays: 'Sunday_Monday_Tuesday_Wednesday_Thursday_Friday_Saturday'.split('_'),
-      weekdaysShort: 'Sun_Mon_Tue_Wed_Thu_Fri_Sat'.split('_'),
-      weekdaysMin: 'Su_Mo_Tu_We_Th_Fr_Sa'.split('_'),
+      months: 'Januar_Februar_Marts_April_Maj_Juni_Juli_August_September_Oktober_November_December'.split('_'),
+      monthsShort: 'Jan_Feb_Mar_Apr_Maj_Jun_Jul_Aug_Sep_Okt_Nov_Dec'.split('_'),
+      weekdays: 'Søndag_Mandag_Tirsdag_Onsdag_Torsdag_Fredag_Lørdag'.split('_'),
+      weekdaysShort: 'Søn_Man_Tir_Ons_Tor_Fre_Lør'.split('_'),
+      weekdaysMin: 'Sø_Ma_Ti_On_To_Fr_Lø'.split('_'),
       longDateFormat: {
-         LT: 'HH:mm',
-         LTS: 'HH:mm:ss',
-         L: 'DD/MM/YYYY',
-         LL: 'D MMMM YYYY',
-         LLL: 'D MMMM YYYY HH:mm',
-         LLLL: 'dddd, D MMMM YYYY HH:mm'
+         LT: 'TT:mm',
+         LTS: 'TT:mm:ss',
+         L: 'DD/MM/ÅÅÅÅ',
+         LL: 'D MMMM ÅÅÅÅ',
+         LLL: 'D MMMM ÅÅÅÅ TT:mm',
+         LLLL: 'dddd, D MMMM ÅÅÅÅ TT:mm'
       },
       calendar: {
-         sameDay: '[Today at] LT',
-         nextDay: '[Tomorrow at] LT',
-         nextWeek: 'dddd [at] LT',
-         lastDay: '[Yesterday at] LT',
-         lastWeek: '[Last] dddd [at] LT',
+         sameDay: '[I dag kl.] LT',
+         nextDay: '[I morgen kl.] LT',
+         nextWeek: 'dddd [kl.] LT',
+         lastDay: '[I går kl.] LT',
+         lastWeek: '[Sidste] dddd [kl.] LT',
          sameElse: 'L'
       },
       relativeTime: {
-         future: 'in %s',
-         past: '%s ago',
-         s: 'a few seconds',
-         ss: '%d seconds',
-         m: 'a minute',
-         mm: '%d minutes',
-         h: 'an hour',
-         hh: '%d hours',
-         d: 'a day',
-         dd: '%d days',
-         M: 'a month',
-         MM: '%d months',
-         y: 'a year',
-         yy: '%d years'
+         future: 'i %s',
+         past: '%s siden',
+         s: 'nogle få sekunder',
+         ss: '%d sekunder',
+         m: 'et minut',
+         mm: '%d minutter',
+         h: 'en time',
+         hh: '%d timer',
+         d: 'en dag',
+         dd: '%d dage',
+         M: 'en måned',
+         MM: '%d måneder',
+         y: 'et år',
+         yy: '%d år'
       },
       dayOfMonthOrdinalParse: /\d{1,2}(st|nd|rd|th)/,
       ordinal: function (number) {
