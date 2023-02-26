ba51c0fc8741c27e31240d948e1059b013d8e9bf
Frederick MC Chan <meangczac.chan@gmail.com>
2023-02-26 16:10:17 +0800

    New translations en.js (Polish)

--- Polish/includes/dynamics/assets/password/lang/pl.js
+++ Polish/includes/dynamics/assets/password/lang/pl.js
@@ -1,9 +1,9 @@
 var pwstrength_locale = {
     wordMinLength: 'Hasło jest zbyt krótkie',
     wordMaxLength: 'Hasło jest za długie',
-    wordInvalidChar: 'Hasło zawiera nieprawidłowy znak',
-    wordNotEmail: 'Hasło nie może być Twoim emailem',
-    wordSimilarToUsername: 'Hasło nie może zawierać nazwy użytkownika',
+    wordInvalidChar: 'Hasło zawiera niedozwolone znaki',
+    wordNotEmail: 'Nie używaj adresu e-mail jako hasła',
+    wordSimilarToUsername: 'Twoje hasło nie może zawierać nazwy użytkownika',
     wordTwoCharacterClasses: 'Użyj innych klas znaków',
     wordRepetitions: 'Zbyt wiele powtórzeń',
     wordSequences: 'Hasło zawiera sekwencje',
