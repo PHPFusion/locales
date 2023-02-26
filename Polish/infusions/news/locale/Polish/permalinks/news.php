08198cc77b8d50f7ccb73b0a8d8713e4b5eaa884
Frederick MC Chan <meangczac.chan@gmail.com>
2023-02-26 16:10:26 +0800

    New translations news.php (Polish)

--- Polish/infusions/news/locale/Polish/permalinks/news.php
+++ Polish/infusions/news/locale/Polish/permalinks/news.php
@@ -2,4 +2,4 @@
 $locale['pl_news_title'] = "Aktualności";
 $locale['pl_news_desc'] = "Włącza bezpośrednie odnośniki dla aktualności na stronie.";
 $locale['pl_tags_001'] = "Identyfikator elementu aktualności.";
-$locale['pl_tags_002'] = "Tytuł lub temat elementu aktualności.";
+$locale['pl_tags_002'] = "Tytuł lub temat wpisu w aktualnościach.";
