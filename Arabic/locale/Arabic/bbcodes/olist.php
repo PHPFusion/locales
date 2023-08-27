5a07bf1e6bdb1bb1be6ca414f987c45653ee8e72
Frederick MC Chan <meangczac.chan@gmail.com>
2023-08-27 20:26:02 +0800

    New translations olist.php (Arabic)

--- Arabic/locale/Arabic/bbcodes/olist.php
+++ Arabic/locale/Arabic/bbcodes/olist.php
@@ -1,8 +1,8 @@
 <?php
 $locale['bb_olist_description'] = "إنشاء قائمة تبدأ بأرقام تسلسلية";
 $locale['bb_olist_usage'] = "القيمة :سطرجديد: القيمة :سطرجديد: القيمة ... الخ";
-$locale['bb_olist_0100'] = "Numeric List";
-$locale['bb_olist_0101'] = "Alphabets Lowercase";
-$locale['bb_olist_0102'] = "Alphabets Uppercase";
-$locale['bb_olist_0103'] = "Roman Lowercase";
-$locale['bb_olist_0104'] = "Roman Uppercase";
+$locale['bb_olist_0100'] = "قائمة رقمية";
+$locale['bb_olist_0101'] = "أبجدي بحروف صغيرة";
+$locale['bb_olist_0102'] = "أبجدي بحروف كبيرة";
+$locale['bb_olist_0103'] = "حروف رومانية صغيرة";
+$locale['bb_olist_0104'] = "حروف رومانية كبيرة";
