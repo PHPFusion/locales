2badb3330b131393f466e3be96a4924bc9067f3a
Frederick MC Chan <meangczac.chan@gmail.com>
2023-08-27 20:25:56 +0800

    New translations image_uploads.php (Arabic)

--- Arabic/locale/Arabic/admin/image_uploads.php
+++ Arabic/locale/Arabic/admin/image_uploads.php
@@ -11,7 +11,7 @@ $locale['IMGUP_421'] = "اسم الملف:";
 $locale['IMGUP_422'] = "الصور الأساسية";
 $locale['IMGUP_423'] = "صور المقال";
 $locale['IMGUP_424'] = "صور الأخبار";
-$locale['IMGUP_425'] = "Max. file size %s / Allowed file types .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
+$locale['IMGUP_425'] = "الحد الأقصى لحجم الملف %s / أنواع الملفات المسموح بها .jpg,.jpeg,.png,.gif,.bmp,.svg,.tiff,.webp";
 $locale['IMGUP_426'] = "تم رفع الصورة";
 $locale['IMGUP_427'] = "صور تصنيفات الأخبار";
 $locale['IMGUP_428'] = "صور المدونة";
@@ -25,6 +25,6 @@ $locale['IMGUP_460'] = "الصور";
 $locale['IMGUP_461'] = "عرض";
 $locale['IMGUP_463'] = "هذا المجلد فارغ";
 $locale['IMGUP_464'] = "تحديث قائمة صور محرر TinyMCE ";
-$locale['IMGUP_465'] = "Image list updated";
+$locale['IMGUP_465'] = "تم تحديث قائمة الصور";
 // Popup Error/Warning Messages
 $locale['IMGUP_470'] = "حذف هذه الصورة؟";
